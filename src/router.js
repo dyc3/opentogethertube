e121f5b41c703a83290c0490ee9c03f31a767891
Carson McManus <carson.mcmanus1@gmail.com>
2019-07-21 11:22:02 -0400

    add semicolons

--- src/router.js
+++ src/router.js
@@ -1,8 +1,8 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   mode: 'history',
@@ -22,4 +22,4 @@ export default new Router({
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     }
   ]
-})
+});
