891ab485caaed856444c7a97edf413e39724397f
Carson McManus <carson.mcmanus1@gmail.com>
2019-07-31 12:08:56 -0400

    update router

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-// import Home from './views/Home.vue';
+import Home from './views/Home.vue';
 
 Vue.use(Router);
 
@@ -8,13 +8,13 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: Home
-    // },
     {
       path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/room/:name',
       name: 'room',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
