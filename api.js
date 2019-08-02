92dace8ed441b244f594d408d7646c36f30508ec
Carson McManus <carson.mcmanus1@gmail.com>
2019-08-02 01:13:44 -0400

    fix queue endpoint

--- api.js
+++ api.js
@@ -76,19 +76,18 @@ module.exports = function(_roommanager) {
 	});
 
 	router.post("/room/:name/queue", (req, res) => {
-		if (req.params.name === "test") {
-			roommanager.rooms[req.params.name].queue.push(req.body.url);
-			res.json({
-				success: true
-			});
-			roommanager.updateRoom(roommanager.rooms[req.params.name]);
-		}
-		else {
+		if (!roommanager.rooms.hasOwnProperty(req.params.name)) {
 			res.status(404);
 			res.json({
 				error: "Room does not exist"
 			});
+			return;
 		}
+		roommanager.rooms[req.params.name].queue.push(req.body.url);
+		res.json({
+			success: true
+		});
+		roommanager.updateRoom(roommanager.rooms[req.params.name]);
 	});
 
 	return router;
