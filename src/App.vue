987a85cb83aaf2981c4b09e6ba87e6d0d0c50079
Carson McManus <carson.mcmanus1@gmail.com>
2019-08-02 00:37:22 -0400

    add app bar

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
 <template>
   <v-app id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <v-content>
+      <v-app-bar>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-toolbar-title>
+          <router-link class="link-invis" to="/">
+            OpenTogetherTube
+          </router-link>
+        </v-toolbar-title>
+        <v-toolbar-items>
+
+        </v-toolbar-items>
+      </v-app-bar>
       <router-view/>
     </v-content>
   </v-app>
 </template>
 
 <style lang="scss">
-
+.link-invis {
+  text-decoration: none;
+  color: inherit !important;
+}
 </style>
