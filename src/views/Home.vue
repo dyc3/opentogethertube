0acf44e563b6ab6d6276cc14991214fda36d6e23
Carson McManus <carson.mcmanus1@gmail.com>
2019-08-02 22:26:00 -0400

    fix so more content can be added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,26 @@
 <template>
-	<v-container class="hero" fluid bg fill-height grid-list-md text-xs-center>
-		<v-layout row align-center>
-			<v-layout column align-center>
+	<div class="home">
+		<v-container class="hero" fluid bg fill-height grid-list-md text-xs-center>
+			<v-layout row align-center>
+				<v-layout column align-center>
+					<v-flex>
+						<h1>Enjoy Together.</h1>
+						<span>
+							Real-time syncronized playback. Optional voting system. <br>
+							No sign up required. All Open Source.
+						</span>
+					</v-flex>
+				</v-layout>
+			</v-layout>
+		</v-container>
+		<v-container>
+			<v-layout row align-center>
 				<v-flex>
-					<h1>Enjoy Together.</h1>
-					<span>
-						Real-time syncronized playback. Optional voting system. <br>
-						No sign up required. All Open Source.
-					</span>
+					<h1>Simple and Easy.</h1>
 				</v-flex>
 			</v-layout>
-		</v-layout>
-	</v-container>
+		</v-container>
+	</div>
 </template>
 
 <script>
@@ -21,6 +30,11 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.home {
+	width: 100%;
+	height: 100%;
+}
+
 .hero {
 	background: linear-gradient(217deg, rgb(206, 74, 239), rgb(247, 208, 109));
 	color: white;
