2db8b97b11cbf423d0790cfabae520465c639053
Carson McManus <carson.mcmanus1@gmail.com>
2019-08-27 18:37:17 -0400

    add getVideoIdYoutube

--- infoextract.js
+++ infoextract.js
@@ -1,6 +1,18 @@
 const axios = require("axios");
+const url = require("url");
+const querystring = require('querystring');
 
 module.exports = {
+	getVideoIdYoutube(link) {
+		let urlParsed = url.parse(link);
+		if (urlParsed.host.endsWith("youtu.be")) {
+			return urlParsed.path.replace("/", "");
+		}
+		else {
+			return querystring.parse(urlParsed.query)["v"];
+		}
+	},
+
 	getVideoLengthYoutube: async (url) => {
 		let res = await axios.get(url);
 		let regexs = [/length_seconds":"\d+/, /lengthSeconds\\":\\"\d+/];
