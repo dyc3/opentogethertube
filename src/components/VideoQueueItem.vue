4bddc0842d1642149cb7dc95435aa1bfd15b9a17
Carson McManus <carson.mcmanus1@gmail.com>
2019-08-28 16:06:58 -0400

    add remove button to queue items

--- src/components/VideoQueueItem.vue
+++ src/components/VideoQueueItem.vue
@@ -3,14 +3,30 @@
 		<v-card-title>{{ item.id }}</v-card-title>
 		<v-card-text>{{ item.service }} {{ item.length }}</v-card-text>
 		<v-card-text>{{ item }}</v-card-text>
+		<v-btn icon @click="removeFromQueue">
+			<v-icon>fas fa-trash</v-icon>
+		</v-btn>
 	</v-card>
 </template>
 
 <script>
+import { API } from "@/common-http.js";
+
 export default {
 	name: "VideoQueueItem",
 	props: {
 		item: Object
 	},
+	methods: {
+		removeFromQueue() {
+			let data = {
+				service: this.item.service,
+				id: this.item.id,
+			};
+			API.delete(`/room/${this.$route.params.roomId}/queue`, { data: data }).then(res => {
+				console.log(`Remove ${data} from queue:`, res.data);
+			});
+		}
+	}
 }
 </script>
