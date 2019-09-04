7340309e15216ded7023c47397ee1121ef9d51cd
Carson McManus <carson.mcmanus1@gmail.com>
2019-09-04 09:55:05 -0400

    fix indentation

--- src/views/Room.vue
+++ src/views/Room.vue
@@ -248,17 +248,17 @@ export default {
   font-size: 10px;
 }
 .iframe-container {
-	position: relative;
-	padding-bottom: 56.25%;
-	height: 0;
-	overflow: hidden;
-	max-width: 100%;
+  position: relative;
+  padding-bottom: 56.25%;
+  height: 0;
+  overflow: hidden;
+  max-width: 100%;
 }
 .iframe-container iframe {
-	position: absolute;
-	top: 0;
-	left: 0;
-	width: 100%;
-	height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
 }
 </style>
