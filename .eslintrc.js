078b96d7539f3811ba43c5bd0d078f67e71f08c7
Carson McManus <carson.mcmanus1@gmail.com>
2019-09-04 10:40:00 -0400

    add some eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,9 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'curly': ['error', 'all'],
+    'no-eval': ['error', {}],
   },
   parserOptions: {
     ecmaVersion: 6,
