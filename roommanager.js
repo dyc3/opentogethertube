cd561cdd9212925f803860f1d7360c63d2f87dac
Carson McManus <carson.mcmanus1@gmail.com>
2019-10-02 16:36:28 -0400

    room manager completely refactored

--- roommanager.js
+++ roommanager.js
@@ -1,126 +1,36 @@
 const WebSocket = require('ws');
-const InfoExtract = require("./infoextract");
 const { uniqueNamesGenerator } = require('unique-names-generator');
 const _ = require("lodash");
 const moment = require("moment");
 const NanoTimer = require("nanotimer");
+const InfoExtract = require("./infoextract");
+const storage = require("./storage");
 
-module.exports = function (server, storage) {
-	function syncRoom(room) {
-		if (!room) {
-			return;
-		}
-		let syncMsg = {
-			action: "sync",
-			name: room.name,
-			title: room.title,
-			description: room.description,
-			isTemporary: room.isTemporary,
-			currentSource: room.currentSource,
-			queue: room.queue,
-			isPlaying: room.isPlaying,
-			playbackPosition: room.playbackPosition,
-			users: [],
-		};
-
-		for (let i = 0; i < room.clients.length; i++) {
-			let ws = room.clients[i].socket;
-
-			// make sure the socket is still open
-			if (ws.readyState != 1) {
-				continue;
-			}
-
-			syncMsg.users = [];
-			for (let u = 0; u < room.clients.length; u++) {
-				syncMsg.users.push({
-					name: room.clients[u].name,
-					isYou: ws == room.clients[u].socket,
-				});
-			}
-
-			ws.send(JSON.stringify(syncMsg));
-		}
+/**
+ * Represents a Room and all it's associated state, settings, connected clients.
+ */
+class Room {
+	/**
+	 * DO NOT CREATE NEW ROOMS WITH THIS CONSTRUCTOR. Create/get Rooms using the RoomManager.
+	 */
+	constructor() {
+		this.name = "";
+		this.title = "";
+		this.description = "";
+		this.isTemporary = false;
+		this.currentSource = {};
+		this.queue = [];
+		this.isPlaying = false;
+		this.playbackPosition = 0;
+		this.clients = [];
+		this.keepAlivePing = null;
 	}
 
-	function updateRoom(room) {
-		if (_.isEmpty(room.currentSource) && room.queue.length > 0) {
-			room.currentSource = room.queue.shift();
-		}
-		else if (!_.isEmpty(room.currentSource) && room.playbackPosition > room.currentSource.length) {
-			room.currentSource = room.queue.length > 0 ? room.queue.shift() : {};
-			room.playbackPosition = 0;
-		}
-		if (_.isEmpty(room.currentSource) && room.queue.length == 0 && room.isPlaying) {
-			room.isPlaying = false;
-			room.playbackPosition = 0;
-		}
-		syncRoom(room);
-	}
-
-	function createRoom(roomName, isTemporary=false) {
-		// temporary rooms are not stored in the database
-		let newRoom = {
-			name: roomName,
-			title: "",
-			description: "",
-			isTemporary: isTemporary,
-			currentSource: {},
-			queue: [],
-			clients: [],
-			isPlaying: false,
-			playbackPosition: 0,
-		};
-		if (isTemporary) {
-			// Used to delete temporary rooms after a certain amount of time with no users connected
-			newRoom.keepAlivePing = new Date();
-		}
-		else {
-			storage.saveRoom(newRoom);
-		}
-		rooms[roomName] = newRoom;
-	}
-
-	function deleteRoom(roomName) {
-		for (let i = 0; i < rooms[roomName].clients.length; i++) {
-			rooms[roomName].clients[i].socket.send(JSON.stringify({
-				action: "room-delete",
-			}));
-			rooms[roomName].clients[i].socket.close(4003, "Room has been deleted");
-		}
-		delete rooms[roomName];
-	}
-
-	function getRoom(roomName) {
-		if (rooms.hasOwnProperty(roomName)) {
-			console.log("Room already loaded from db");
-			return new Promise(resolve => resolve(rooms[roomName]));
-		}
-
-		// load the room from storage if it exists
-		console.log("Grabbing room", roomName, "from db");
-		return storage.getRoomByName(roomName).then(result => {
-			if (!result) {
-				return false;
-			}
-
-			let room = {
-				name: result.name,
-				title: result.title,
-				description: result.description,
-				isTemporary: false,
-				currentSource: {},
-				queue: [],
-				clients: [],
-				isPlaying: false,
-				playbackPosition: 0,
-			};
-			rooms[roomName] = room;
-			return room;
-		});
-	}
-
-	function addToQueue(roomName, video) {
+	/**
+	 * Obtains metadata for a given video and adds it to the queue
+	 * @param {Object} video The video to add. Should contain either a `url` property, or `service` and `id` properties.
+	 */
+	addToQueue(video) {
 		let queueItem = {
 			service: "",
 			id: "",
@@ -147,12 +57,11 @@ module.exports = function (server, storage) {
 			return InfoExtract.getVideoInfo(queueItem.service, queueItem.id).then(result => {
 				queueItem = result;
 			}).catch(err => {
-				console.error("Failed to get video info");
-				console.error(err);
+				console.error("Failed to get video info:", err);
 				queueItem.title = queueItem.id;
 			}).then(() => {
-				rooms[roomName].queue.push(queueItem);
-				updateRoom(rooms[roomName]);
+				this.queue.push(queueItem);
+				this.update();
 				return true;
 			});
 		}
@@ -161,148 +70,308 @@ module.exports = function (server, storage) {
 		}
 	}
 
-	const wss = new WebSocket.Server({ server });
-
-	let rooms = {
-		test: {
-			name: "test",
-			title: "Test Room",
-			description: "This is a test room.",
-			isTemporary: false,
-			currentSource: {},
-			queue: [],
-			clients: [],
-			isPlaying: false,
-			playbackPosition: 0,
-		},
-	};
-
-	wss.on('connection', (ws, req) => {
-		console.log("[ws] CONNECTION ESTABLISHED", ws.protocol, req.url, ws.readyState);
-
-		if (!req.url.startsWith("/api/room/")) {
-			console.error("[ws] Invalid connection url");
-			ws.close(4001, "Invalid connection url");
-			return;
+	/**
+	 * Updates the room state. Any logic that makes the room do
+	 * something automatically without a user's input goes here
+	 * (automatically playing the next video in the queue,
+	 * updating playbackPosition when playing, etc.)
+	 */
+	update() {
+		// remove inactive clients
+		for (let i = 0; i < this.clients.length; i++) {
+			let ws = this.clients[i].socket;
+			if (ws.readyState != 1) {
+				console.log("Remove inactive client:", i, this.clients[i].name);
+				this.clients.splice(i--, 1);
+				continue;
+			}
 		}
-		let roomName = req.url.replace("/api/room/", "");
-		getRoom(roomName).then(room => {
-			if (!room) {
-				console.error("[ws] Room doesn't exist");
-				ws.close(4002, "Room doesn't exist");
+
+		if (this.isPlaying) {
+			this.playbackPosition += 1;
+		}
+
+		if (_.isEmpty(this.currentSource) && this.queue.length > 0) {
+			this.currentSource = this.queue.shift();
+		}
+		else if (!_.isEmpty(this.currentSource) && this.playbackPosition > this.currentSource.length) {
+			this.currentSource = this.queue.length > 0 ? this.queue.shift() : {};
+			this.playbackPosition = 0;
+		}
+		if (_.isEmpty(this.currentSource) && this.queue.length == 0 && this.isPlaying) {
+			this.isPlaying = false;
+			this.playbackPosition = 0;
+		}
+
+		// remove empty temporary rooms
+		if (this.isTemporary && this.clients.length > 0) {
+			this.keepAlivePing = moment();
+		}
+	}
+
+	/**
+	 * Synchronize all clients in this room by sending a sync message.
+	 */
+	sync() {
+		let syncMsg = {
+			action: "sync",
+			name: this.name,
+			title: this.title,
+			description: this.description,
+			isTemporary: this.isTemporary,
+			currentSource: this.currentSource,
+			queue: this.queue,
+			isPlaying: this.isPlaying,
+			playbackPosition: this.playbackPosition,
+			users: [],
+		};
+
+		for (const client of this.clients) {
+			// make sure the socket is still open
+			if (client.socket.readyState != 1) {
+				continue;
+			}
+
+			syncMsg.users = this.clients.map(c => {
+				return {
+					name: c.name,
+					isYou: client.socket == c.socket,
+				};
+			});
+
+			client.socket.send(JSON.stringify(syncMsg));
+		}
+	}
+
+	/**
+	 * Called when a new client connects to this room.
+	 * @param {Object} ws Websocket for the client.
+	 */
+	onConnectionReceived(ws) {
+		let client = {
+			name: "client",
+			socket: ws,
+		};
+		this.clients.push(client);
+		ws.on('message', (message) => {
+			this.onMessageReceived(client, JSON.parse(message));
+		});
+	}
+
+	/**
+	 * Called when this room receives a message from one of it's users.
+	 * @param {Object} client The client that sent the message
+	 * @param {Object} message The message that the client sent as a object. Should always have an `action` attribute.
+	 */
+	onMessageReceived(client, msg) {
+		if (msg.action === "play") {
+			this.isPlaying = true;
+			this.sync();
+		}
+		else if (msg.action === "pause") {
+			this.isPlaying = false;
+			this.sync();
+		}
+		else if (msg.action === "seek") {
+			this.playbackPosition = msg.position;
+			this.sync();
+		}
+		else if (msg.action === "skip") {
+			this.playbackPosition = this.currentSource.length + 1;
+			this.update();
+		}
+		else if (msg.action === "set-name") {
+			if (!msg.name) {
+				console.warn("name not supplied");
 				return;
 			}
+			// for (let i = 0; i < this.clients.length; i++) {
+			// 	if (this.clients[i].socket == client.socket) {
+			// 		this.clients[i].name = msg.name;
+			// 		break;
+			// 	}
+			// }
+			client.name = msg.name;
+			this.update();
+		}
+		else if (msg.action === "generate-name") {
+			let generatedName = uniqueNamesGenerator();
+			client.socket.send(JSON.stringify({
+				action: "generatedName",
+				name: generatedName,
+			}));
 
-			room.clients.push({
-				name: "client",
-				socket: ws,
-			});
-			console.log("[ws] client joined", roomName);
-			ws.on('message', (message) => {
-				console.log('[ws] received:', typeof(message), message);
-				let msg = JSON.parse(message);
-				if (msg.action == "play") {
-					room.isPlaying = true;
-					syncRoom(room);
-				}
-				else if (msg.action == "pause") {
-					room.isPlaying = false;
-					syncRoom(room);
-				}
-				else if (msg.action == "seek") {
-					room.playbackPosition = msg.position;
-					syncRoom(room);
-				}
-				else if (msg.action == "skip") {
-					room.playbackPosition = room.currentSource.length + 1;
-					updateRoom(room);
-				}
-				else if (msg.action == "set-name") {
-					if (!msg.name) {
-						console.warn("name not supplied");
-						return;
-					}
-					for (let i = 0; i < room.clients.length; i++) {
-						if (room.clients[i].socket == ws) {
-							room.clients[i].name = msg.name;
-							break;
-						}
-					}
-					updateRoom(room);
-				}
-				else if (msg.action == "generate-name") {
-					let generatedName = uniqueNamesGenerator();
-					ws.send(JSON.stringify({
-						action: "generatedName",
-						name: generatedName,
-					}));
+			// for (let i = 0; i < this.clients.length; i++) {
+			// 	if (this.clients[i].socket == client.socket) {
+			// 		this.clients[i].name = generatedName;
+			// 		break;
+			// 	}
+			// }
+			client.name = generatedName;
+			this.update();
+		}
+		else {
+			console.warn("[ws] UNKNOWN ACTION", msg.action);
+		}
+	}
+}
 
-					for (let i = 0; i < room.clients.length; i++) {
-						if (room.clients[i].socket == ws) {
-							room.clients[i].name = generatedName;
-							break;
-						}
-					}
-					updateRoom(room);
+class RoomNotFoundException extends Error {
+	constructor(roomName) {
+		super(`The room "${roomName}" could not be found.`);
+		this.name = "RoomNotFoundException";
+	}
+}
+
+class RoomAlreadyLoadedException extends Error {
+	constructor(roomName) {
+		super(`The room "${roomName}" is already loaded.`);
+		this.name = "RoomAlreadyLoadedException";
+	}
+}
+
+module.exports = {
+	rooms: [],
+
+	/**
+	 * Start the room manager.
+	 * @param {Object} httpServer The http server to get websocket connections from.
+	 */
+	start(httpServer) {
+		const wss = new WebSocket.Server({ server: httpServer });
+
+		wss.on('connection', (ws, req) => {
+			console.log("[ws] CONNECTION ESTABLISHED", ws.protocol, req.url, ws.readyState);
+
+			if (!req.url.startsWith("/api/room/")) {
+				console.error("[ws] Invalid connection url");
+				ws.close(4001, "Invalid connection url");
+				return;
+			}
+			let roomName = req.url.replace("/api/room/", "");
+			this.getOrLoadRoom(roomName).then(room => {
+				room.onConnectionReceived(ws);
+			}).catch(err => {
+				if (err.name === "RoomNotFoundException") {
+					console.error("[ws] Room doesn't exist");
+					ws.close(4002, "Room doesn't exist");
+					return;
 				}
 				else {
-					console.warn("[ws] UNKNOWN ACTION", msg.action);
+					throw err;
 				}
 			});
-
-			// sync room immediately
-			syncRoom(rooms[roomName]);
 		});
-	});
 
-	const nanotimer = new NanoTimer();
-	nanotimer.setInterval(() => {
-		let roomsToDelete = [];
-		for (let roomName in rooms) {
-			let room = rooms[roomName];
+		const nanotimer = NanoTimer();
+		nanotimer.setInterval(() => {
+			for (const room of this.rooms) {
+				room.update();
+				room.sync();
 
-			// remove inactive clients
-			for (let i = 0; i < room.clients.length; i++) {
-				let ws = room.clients[i].socket;
-				if (ws.readyState != 1) {
-					console.log("Remove inactive client:", i, room.clients[i].name);
-					room.clients.splice(i, 1);
-					i--;
-					continue;
+				if (room.isTemporary && room.clients.length == 0 &&
+					moment().diff(room.keepAlivePing, 'seconds') > 10) {
+					this.unloadRoom(room);
 				}
 			}
+		}, '', '1000m');
+	},
 
-			if (room.isPlaying) {
-				room.playbackPosition += 1;
-				updateRoom(room);
-			}
+	/**
+	 * Create a new room using the given name.
+	 * @param {string} name The name for the new room
+	 * @param {boolean} isTemporary Whether or not the new room is temporary. Temporary rooms do not get stored in the database.
+	 */
+	createRoom(name, isTemporary=false) {
+		let newRoom = new Room();
+		newRoom.name = name;
+		newRoom.isTemporary = isTemporary;
+		if (isTemporary) {
+			// Used to delete temporary rooms after a certain amount of time with no users connected
+			newRoom.keepAlivePing = new Date();
+		}
+		else {
+			storage.saveRoom(newRoom);
+		}
+		this.rooms.push(newRoom);
+	},
 
-			// remove empty temporary rooms
-			if (room.isTemporary) {
-				if (room.clients.length > 0) {
-					room.keepAlivePing = moment();
-				}
-				else {
-					if (moment().diff(room.keepAlivePing, 'seconds') > 10) {
-						console.log("Removing inactive temporary room", roomName);
-						roomsToDelete.push(roomName);
-					}
-				}
-			}
+	/**
+	 * Loads the Room with the given name from the database. If the
+	 * room is already loaded, the promise resolves to the loaded Room.
+	 * @param {string} name The name of the room to load.
+	 * @returns {Promise} Promise that resolves to a Room.
+	 * @throws {RoomNotFoundException}
+	 */
+	loadRoom(name) {
+		if (_.findIndex(this.rooms, r => r.name === name) >= 0) {
+			throw new RoomAlreadyLoadedException(name);
 		}
 
-		for (let i = 0; i < roomsToDelete.length; i++) {
-			deleteRoom(roomsToDelete[i]);
-		}
-	}, '', '1000m');
+		return storage.getRoomByName(name).then(result => {
+			if (!result) {
+				throw new RoomNotFoundException(name);
+			}
 
-	return {
-		rooms,
-		syncRoom,
-		updateRoom,
-		createRoom,
-		deleteRoom,
-		addToQueue,
-		getRoom,
-	};
+			let room = new Room();
+			room.name = result.name;
+			room.title = result.title;
+			room.description = result.description;
+			room.isTemporary = false;
+			this.rooms.push(room);
+			return room;
+		});
+	},
+
+	/**
+	 * Unloads the room with the given name from memory.
+	 * @param {Room} room The room to unload
+	 */
+	unloadRoom(room) {
+		for (const client of room.clients) {
+			client.socket.send(JSON.stringify({
+				action: "room-unload",
+			}));
+			client.socket.close(4003, "Room has been unloaded");
+		}
+
+		const roomIdx = _.findIndex(this.rooms, r => r.name === room.name);
+		this.rooms.splice(roomIdx, 1);
+	},
+
+	/**
+	 * Gets the Room by name if it's loaded into memory, otherwise returns false.
+	 * @param {string} name The name of the room
+	 * @returns {(Room|boolean)}
+	 */
+	getLoadedRoom(name) {
+		return new Promise(resolve => {
+			for (const room of this.rooms) {
+				if (room.name === name) {
+					resolve(room);
+					return;
+				}
+			}
+			resolve(false);
+		});
+	},
+
+	/**
+	 * Gets the loaded Room, if its loaded, otherwise grab it from the database.
+	 * If the Room can't be found it will throw a RoomNotFoundException.
+	 * @param {string} name The name of the room
+	 * @throws {RoomNotFoundException}
+	 */
+	getOrLoadRoom(name) {
+		return this.getLoadedRoom(name).then(room => {
+			if (room) {
+				console.log(`Found room ${room.name} in loaded rooms`);
+				return room;
+			}
+			else {
+				console.log(`Looking for room ${name} in database`);
+				return this.loadRoom(name);
+			}
+		});
+	},
 };
