a7695e8038e16ddf9015f01bf74a404c8da1172f
adapap <19696846+adapap@users.noreply.github.com>
2019-10-17 10:39:02 -0400

    Fits by truncating text

--- src/components/VideoQueueItem.vue
+++ src/components/VideoQueueItem.vue
@@ -1,16 +1,16 @@
 <template>
-	<v-card class="video" style="margin-top: 10px" hover>
+	<v-card class="mt-2" hover>
 		<v-container class="pa-0">
 			<v-row no-gutters align="center" justify="space-between">
 				<v-col cols="4">
 					<v-img :src="item.thumbnail" contain>
-						<span class="length">{{ videoLength }}</span>
+						<span class="subtitle-2 video-length">{{ videoLength }}</span>
 					</v-img>
 				</v-col>
 				<v-col cols="7">
 					<v-container>
 						<v-row class="title" no-gutters>{{ item.title }}</v-row>
-						<v-row class="description" no-gutters>{{ item.description }}</v-row>
+						<v-row class="body-1 text-truncate" no-gutters>{{ item.description }}</v-row>
 					</v-container>
 				</v-col>
 				<v-col cols="1">
@@ -74,38 +74,13 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.title {
-	@media screen and (max-width: 960px) {
-		font-size: 10px !important;
-	}
-	@media screen and (min-width: 960px) and  (max-width: 1264px) {
-		font-size: 12px !important;
-	}
-	@media screen and (min-width: 1264px) {
-		font-size: 20px !important;
-	}
-	overflow: hidden;
-	text-overflow: ellipsis;
-}
-.description {
-	font-size: 11px;
-	line-height: 1;
-	overflow-y: hidden;
-	text-overflow: ellipsis;
-	height: 100px;
-
-	@media screen and (max-width: 1264px) {
-		display: none;
-	}
-}
-.length {
-	color: #fff;
-	background: rgba(0, 0, 0, 0.8);
-	padding: 2px 5px;
-	border-radius: 3px;
-	position: absolute;
-	bottom: 0;
-	right: 0;
-	z-index: 1000;
-}
+    .video-length {
+        background: rgba(0, 0, 0, 0.8);
+        padding: 2px 5px;
+        border-top-left-radius: 3px;
+        position: absolute;
+        bottom: 0;
+        right: 0;
+        z-index: 1000;
+    }
 </style>
