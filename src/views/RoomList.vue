a4037d4c427fd0a330515690d40234adef929914
Carson McManus <carson.mcmanus1@gmail.com>
2019-10-29 08:38:40 -0400

    better cards on room list page

--- src/views/RoomList.vue
+++ src/views/RoomList.vue
@@ -7,10 +7,15 @@
     </v-layout>
     <v-layout wrap v-if="!isLoading">
       <v-flex xs6 md3 v-for="(room, index) in rooms" :key="index">
-        <v-card hover :to="`/room/${room.name}`">
-          <v-card-title>{{ room.isTemporary ? "Temporary Room" : room.name }}</v-card-title>
-          <v-card-text>{{ room.description }}</v-card-text>
-          <v-card-text>{{ room.users }} users</v-card-text>
+        <v-card hover class="room" :to="`/room/${room.name}`">
+          <v-img :src="room.currentSource.thumbnail ? room.currentSource.thumbnail : require('@/assets/placeholder.svg')">
+            <span class="subtitle-2 users">{{ room.users }} <v-icon small>fas fa-user-friends</v-icon></span>
+          </v-img>
+          <v-card-title v-text="room.isTemporary ? 'Temporary Room' : room.name" />
+          <v-card-text>
+            <div class="description">{{ room.description }}</div>
+            <div class="video-title">{{ room.currentSource.title }}</div>
+          </v-card-text>
         </v-card>
       </v-flex>
     </v-layout>
@@ -39,3 +44,21 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.description, .video-title {
+  height: 25px;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+}
+.users {
+  background: rgba(0, 0, 0, 0.8);
+  padding: 2px 5px;
+  border-top-left-radius: 3px;
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  z-index: 1000;
+}
+</style>
