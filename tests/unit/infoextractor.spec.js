feda3c36801577acde0c6f5e14e51f23984fda52
Carson McManus <carson.mcmanus1@gmail.com>
2019-11-05 14:48:31 -0500

    fix linting errors

--- tests/unit/infoextractor.spec.js
+++ tests/unit/infoextractor.spec.js
@@ -1,5 +1,3 @@
-const fs = require('fs');
-const path = require('path');
 const InfoExtract = require("../../infoextract");
 const storage = require("../../storage");
 const { CachedVideo } = require("../../models");
