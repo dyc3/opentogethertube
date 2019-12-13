b85078cdfb8ca4b319d1a791e2bd53822e2d799d
Carson McManus <carson.mcmanus1@gmail.com>
2019-12-13 18:37:26 -0500

    add visibility field to room db model so it gets loaded

--- models/room.js
+++ models/room.js
@@ -4,6 +4,7 @@ module.exports = (sequelize, DataTypes) => {
     name: DataTypes.STRING,
     title: DataTypes.STRING,
     description: DataTypes.STRING,
+    visibility: DataTypes.STRING,
   }, {});
   // eslint-disable-next-line no-unused-vars
   Room.associate = function(models) {
