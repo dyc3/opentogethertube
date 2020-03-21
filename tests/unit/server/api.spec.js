62e59db93fc10218e5aa8f292311ee5227000097
Carson McManus <carson.mcmanus1@gmail.com>
2020-03-21 12:34:51 -0400

    dont load rooms from redis while testing

--- tests/unit/server/api.spec.js
+++ tests/unit/server/api.spec.js
@@ -1,6 +1,7 @@
 const request = require('supertest');
-const app = require('../../../app.js').app;
 const roommanager = require('../../../roommanager.js');
+jest.spyOn(roommanager, "getAllLoadedRooms").mockReturnValue(Promise.resolve([]));
+const app = require('../../../app.js').app;
 const InfoExtract = require('../../../infoextract.js');
 
 const TEST_API_KEY = "TESTAPIKEY";
