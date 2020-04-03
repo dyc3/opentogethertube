66587c9685b36881cd834ff0bfb25b5ccc9eac3d
Carson McManus <carson.mcmanus1@gmail.com>
2020-04-03 11:20:01 -0400

    stub out vue-events

--- tests/unit/client/Home.spec.js
+++ tests/unit/client/Home.spec.js
@@ -2,12 +2,14 @@ import Vue from 'vue';
 import { shallowMount, createLocalVue } from '@vue/test-utils';
 import Vuetify from 'vuetify';
 import Home from "@/views/Home.vue";
+import VueEvents from 'vue-events';
 
 // HACK: import globally to prevent it from yelling at us
 // https://github.com/vuetifyjs/vuetify/issues/4964
 Vue.use(Vuetify);
 
 const localVue = createLocalVue();
+localVue.use(VueEvents);
 
 describe("Home view", () => {
 	let vuetify;
