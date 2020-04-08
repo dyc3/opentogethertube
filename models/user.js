ab16d539701c4dbee5239b8e02bc34e38b10aa84
Carson McManus <carson.mcmanus1@gmail.com>
2020-04-07 22:25:58 -0400

    add unique constraint

--- models/user.js
+++ models/user.js
@@ -8,6 +8,7 @@ module.exports = (sequelize, DataTypes) => {
     email: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true,
     },
     salt: {
       type: DataTypes.STRING,
