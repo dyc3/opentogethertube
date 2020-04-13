a79fc56294c26fd3d8ef46f1cd65ab9a7ca3f841
Carson McManus <carson.mcmanus1@gmail.com>
2020-04-12 21:51:24 -0400

    reduce log spam when getting large add previews

--- storage.js
+++ storage.js
@@ -180,7 +180,7 @@ module.exports = {
 	 * created.
 	 * @param {Video|Object} video Video object to store
 	 */
-	updateVideoInfo(video) {
+	updateVideoInfo(video, shouldLog=true) {
 		video = _.cloneDeep(video);
 		if (!video.serviceId) {
 			video.serviceId = video.id;
@@ -188,9 +188,13 @@ module.exports = {
 		}
 
 		return CachedVideo.findOne({ where: { service: video.service, serviceId: video.serviceId } }).then(cachedVideo => {
-			log.info(`Found video ${video.service}:${video.serviceId} in cache`);
+			if (shouldLog) {
+				log.info(`Found video ${video.service}:${video.serviceId} in cache`);
+			}
 			return CachedVideo.update(video, { where: { id: cachedVideo.id } }).then(rowsUpdated => {
-				log.info(`Updated database records, updated ${rowsUpdated[0]} rows`);
+				if (shouldLog) {
+					log.info(`Updated database records, updated ${rowsUpdated[0]} rows`);
+				}
 				return true;
 			}).catch(err => {
 				log.error(`Failed to cache video info ${err}`);
@@ -198,7 +202,9 @@ module.exports = {
 			});
 		}).catch(() => {
 			return CachedVideo.create(video).then(() => {
-				log.info(`Stored video info for ${video.service}:${video.serviceId} in cache`);
+				if (shouldLog) {
+					log.info(`Stored video info for ${video.service}:${video.serviceId} in cache`);
+				}
 				return true;
 			}).catch(err => {
 				log.error(`Failed to cache video info ${err}`);
@@ -243,11 +249,12 @@ module.exports = {
 				toCreate,
 			] = _.partition(videos, video => _.find(foundVideos, { service: video.service, serviceId: video.serviceId }));
 			log.info(`bulk cache: should update ${toUpdate.length} rows, create ${toCreate.length} rows`);
-			for (let video of toUpdate) {
-				await this.updateVideoInfo(video, false);
+			let promises = toUpdate.map(video => this.updateVideoInfo(video, false));
+			if (toCreate.length) {
+				promises.push(CachedVideo.bulkCreate(toCreate));
 			}
-			return CachedVideo.bulkCreate(toCreate).then(cachedVideos => {
-				log.info(`bulk cache: created ${cachedVideos.length} rows`);
+			return Promise.all(promises).then(() => {
+				log.info(`bulk cache: created ${toCreate.length} rows, updated ${toUpdate.length} rows`);
 				return true;
 			}).catch(err => {
 				log.error(`Failed to bulk update video cache: ${err}`);
