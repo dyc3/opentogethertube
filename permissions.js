365b437c31570724c38d2252d19cd182ba5d9341
Carson McManus <carson.mcmanus1@gmail.com>
2020-06-22 13:40:54 -0400

    add base permissions grant logic with tests

---
+++ permissions.js
@@ -0,0 +1,124 @@
+const _ = require("lodash");
+
+const ROOM_PERMISSIONS = {
+	PLAYBACK: {
+		PLAYPAUSE: 1<<0,
+		SKIP: 1<<1,
+		SEEK: 1<<2,
+	},
+	MANAGE_QUEUE: {
+		ADD: 1<<3,
+		REMOVE: 1<<4,
+		ORDER: 1<<5,
+		VOTE: 1<<6,
+	},
+	CHAT: 1<<7,
+	CONFIGURE_ROOM: {
+		SET_TITLE: 1<<8,
+		SET_DESCRIPTION: 1<<9,
+		SET_VISIBILITY: 1<<10,
+		SET_QUEUE_MODE: 1<<11,
+		SET_PERMISSIONS: {
+			FOR_MODERATOR: 1<<12,
+			FOR_TRUSTED_USER: 1<<13,
+			FOR_ALL_REGISTERED_USERS: 1<<14,
+			FOR_ALL_UNREGISTERED_USERS: 1<<15,
+		},
+	},
+	MANAGE_USERS: {
+		PROMOTE_ADMIN: 1<<16,
+		DEMOTE_ADMIN: 1<<17,
+		PROMOTE_MODERATOR: 1<<18,
+		DEMOTE_MODERATOR: 1<<19,
+		PROMOTE_TRUSTED_USER: 1<<20,
+		DEMOTE_TRUSTED_USER: 1<<21,
+	},
+};
+
+const ROLES = {
+	ADMINISTRATOR: 4,
+	MODERATOR: 3,
+	TRUSTED_USER: 2,
+	REGISTERED_USER: 1,
+	UNREGISTERED_USER: 0,
+};
+
+let permMaskMap = {
+	"playback.play-pause": ROOM_PERMISSIONS.PLAYBACK.PLAYPAUSE,
+	"playback.skip": ROOM_PERMISSIONS.PLAYBACK.SKIP,
+	"playback.seek": ROOM_PERMISSIONS.PLAYBACK.SEEK,
+	"manage-queue.add": ROOM_PERMISSIONS.MANAGE_QUEUE.ADD,
+	"manage-queue.remove": ROOM_PERMISSIONS.MANAGE_QUEUE.REMOVE,
+	"manage-queue.order": ROOM_PERMISSIONS.MANAGE_QUEUE.ORDER,
+	"manage-queue.vote": ROOM_PERMISSIONS.MANAGE_QUEUE.VOTE,
+	"chat": ROOM_PERMISSIONS.CHAT,
+	"configure-room.set-title": ROOM_PERMISSIONS.CONFIGURE_ROOM.SET_TITLE,
+	"configure-room.set-description": ROOM_PERMISSIONS.CONFIGURE_ROOM.SET_DESCRIPTION,
+	"configure-room.set-visibility": ROOM_PERMISSIONS.CONFIGURE_ROOM.SET_VISIBILITY,
+	"configure-room.set-queue-mode": ROOM_PERMISSIONS.CONFIGURE_ROOM.SET_QUEUE_MODE,
+	"configure-room.set-permissions.for-moderator": ROOM_PERMISSIONS.CONFIGURE_ROOM.SET_PERMISSIONS.FOR_MODERATOR,
+	"configure-room.set-permissions.for-trusted-users": ROOM_PERMISSIONS.CONFIGURE_ROOM.SET_PERMISSIONS.FOR_TRUSTED_USER,
+	"configure-room.set-permissions.for-all-registered-users": ROOM_PERMISSIONS.CONFIGURE_ROOM.SET_PERMISSIONS.FOR_ALL_REGISTERED_USERS,
+	"configure-room.set-permissions.for-all-unregistered-users": ROOM_PERMISSIONS.CONFIGURE_ROOM.SET_PERMISSIONS.FOR_ALL_UNREGISTERED_USERS,
+	"manage-users.promote-admin": ROOM_PERMISSIONS.MANAGE_USERS.PROMOTE_ADMIN,
+	"manage-users.demote-admin": ROOM_PERMISSIONS.MANAGE_USERS.DEMOTE_ADMIN,
+	"manage-users.promote-moderator": ROOM_PERMISSIONS.MANAGE_USERS.PROMOTE_MODERATOR,
+	"manage-users.demote-moderator": ROOM_PERMISSIONS.MANAGE_USERS.DEMOTE_MODERATOR,
+	"manage-users.promote-trusted-user": ROOM_PERMISSIONS.MANAGE_USERS.PROMOTE_TRUSTED_USER,
+	"manage-users.demote-trusted-user": ROOM_PERMISSIONS.MANAGE_USERS.DEMOTE_TRUSTED_USER,
+};
+
+module.exports = {
+	ROOM_PERMISSIONS,
+	ROLES,
+
+	defaultPermissions() {
+		let roleGrants = {};
+		roleGrants[ROLES.UNREGISTERED_USER] = this.parseIntoGrantMask([
+			"playback",
+			"manage-queue",
+			"chat",
+			"configure-room.set-title",
+			"configure-room.set-description",
+			"configure-room.set-visibility",
+			"configure-room.set-queue-mode",
+		]);
+		roleGrants[ROLES.REGISTERED_USER] = this.parseIntoGrantMask([]);
+		roleGrants[ROLES.TRUSTED_USER] = this.parseIntoGrantMask([]);
+		roleGrants[ROLES.MODERATOR] = this.parseIntoGrantMask([
+			"manage-users.promote-trusted-user",
+			"manage-users.demote-trusted-user",
+		]);
+		roleGrants[ROLES.ADMINISTRATOR] = this.parseIntoGrantMask([
+			"configure-room.set-permissions",
+			"manage-users",
+		]);
+		return roleGrants;
+	},
+
+	/**
+	 * Creates a deterministic mask given a list of string form permissions.
+	 */
+	parseIntoGrantMask(perms) {
+		let mask = 0;
+		for (let perm of perms) {
+			_.forOwn(permMaskMap, (value, key) => {
+				if (key.startsWith(perm)) {
+					mask |= value;
+				}
+			});
+		}
+		return mask;
+	},
+
+	/**
+	 * Checks if the given role is granted the permission, given the grants.
+	 */
+	granted(grants, role, permission) {
+		let fullmask = grants[role];
+		for (let i = role - 1; i >= 0; i--) {
+			fullmask |= grants[i];
+		}
+		return (fullmask & permMaskMap[permission]) > 0;
+	},
+};
---
+++ tests/unit/server/permissions.spec.js
@@ -0,0 +1,44 @@
+const permissions = require("../../../permissions.js");
+
+describe('Permission System', () => {
+	it('should parse exact permissions list into correct grant mask', () => {
+		let grantMask = permissions.parseIntoGrantMask([
+			"playback.play-pause",
+			"manage-queue.add",
+			"chat",
+		]);
+		expect(grantMask).toEqual(1<<0 | 1<<3 | 1<<7);
+	});
+
+	it('should parse wildcard permissions list into correct grant mask', () => {
+		let grantMask = permissions.parseIntoGrantMask(["playback"]);
+		expect(grantMask).toEqual(1<<0 | 1<<1 | 1<<2);
+	});
+
+	it('should evaluate permission grants accurately', () => {
+		let grants = {
+			0: 1<<0 | 1<<1 | 1<<2,
+		};
+		expect(permissions.granted(grants, 0, "playback.play-pause")).toEqual(true);
+		expect(permissions.granted(grants, 0, "chat")).toEqual(false);
+	});
+
+	it('should evaluate inherited permission grants accurately', () => {
+		let grants = {
+			0: 1<<0 | 1<<1 | 1<<2,
+			1: 1<<3 | 1<<4 | 1<<7,
+			2: 1<<8,
+		};
+		expect(permissions.granted(grants, 0, "playback.play-pause")).toEqual(true);
+		expect(permissions.granted(grants, 0, "manage-queue.add")).toEqual(false);
+		expect(permissions.granted(grants, 0, "chat")).toEqual(false);
+		expect(permissions.granted(grants, 0, "configure-room.set-title")).toEqual(false);
+		expect(permissions.granted(grants, 1, "playback.play-pause")).toEqual(true);
+		expect(permissions.granted(grants, 1, "manage-queue.add")).toEqual(true);
+		expect(permissions.granted(grants, 1, "chat")).toEqual(true);
+		expect(permissions.granted(grants, 1, "configure-room.set-title")).toEqual(false);
+		expect(permissions.granted(grants, 2, "playback.play-pause")).toEqual(true);
+		expect(permissions.granted(grants, 2, "chat")).toEqual(true);
+		expect(permissions.granted(grants, 2, "configure-room.set-title")).toEqual(true);
+	});
+});
