ebbbc64939d695e7affbdb6346eb47b3cc7e3fbc
Marius Becker <marius.becker.8@gmail.com>
2020-08-09 16:29:24 +0200

    Implement Vimeo adapter

--- server/services/vimeo.js
+++ server/services/vimeo.js
@@ -1,13 +1,31 @@
 const URL = require("url");
+const axios = require("axios");
 const ServiceAdapter = require("../serviceadapter");
 const { InvalidVideoIdException } = require("../exceptions");
+const Video = require("../../common/video");
+const { getLogger } = require("../../logger");
+
+const log = getLogger("vimeo");
 
 class VimeoAdapter extends ServiceAdapter {
   static SERVICE_ID = "vimeo";
 
+  api = axios.create({
+    baseURL: "https://vimeo.com/api/oembed.json",
+  });
+
   canHandleLink(link) {
     const url = URL.parse(link);
     return url.host.endsWith("vimeo.com");
+  }
+
+  isCollectionURL() {
+    return false;
+  }
+
+  getVideoId(link) {
+    const url = URL.parse(link);
+    return url.pathname.split("/").slice(-1)[0].trim();
   }
 
   async fetchVideoInfo(videoId) {
@@ -15,6 +33,31 @@ class VimeoAdapter extends ServiceAdapter {
       return Promise.reject(
         new InvalidVideoIdException(this.serviceId, videoId)
       );
+    }
+
+    try {
+      const result = await this.api.get("", {
+        params: {
+          url: `https://vimeo.com/${videoId}`,
+        },
+      });
+
+      const video = new Video({
+        service: this.serviceId,
+        id: videoId,
+        title: result.data.title,
+        description: result.data.description,
+        thumbnail: result.data.thumbnail_url,
+        length: result.data.duration,
+      });
+
+      return video;
+    }
+    catch (err) {
+      if (err.response && err.response.status === 403) {
+        log.error("Failed to get video info: Embedding for this video is disabled!");
+      }
+      throw err;
     }
   }
 }
