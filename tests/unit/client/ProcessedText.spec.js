57fc5aaa601b907b75931f8ff626d26cab163d07
Carson McManus <carson.mcmanus1@gmail.com>
2020-08-12 12:59:22 -0400

    add ProcessedText component tests

---
+++ tests/unit/client/ProcessedText.spec.js
@@ -0,0 +1,59 @@
+import Vue from 'vue';
+import { mount, createLocalVue } from '@vue/test-utils';
+import Vuetify from 'vuetify';
+import ProcessedText from "@/components/ProcessedText.vue";
+
+const localVue = createLocalVue();
+
+// HACK: import globally to prevent it from yelling at us
+// https://github.com/vuetifyjs/vuetify/issues/4964
+Vue.use(Vuetify);
+
+describe("ProcessedText component", () => {
+	it("should render nothing", () => {
+		let wrapper = mount(ProcessedText, {
+			localVue,
+			propsData: { text: "" },
+			mounted: jest.fn(),
+		});
+		wrapper.vm.processText();
+		expect(wrapper.vm.content).toHaveLength(0);
+	});
+
+	it("should just render text as is", () => {
+		let wrapper = mount(ProcessedText, {
+			localVue,
+			propsData: { text: "test text" },
+			mounted: jest.fn(),
+		});
+		wrapper.vm.processText();
+		expect(wrapper.vm.content).toHaveLength(1);
+		expect(wrapper.vm.content).toEqual([{ type: "text", text: "test text" }]);
+	});
+
+	it("should render just the link", () => {
+		let wrapper = mount(ProcessedText, {
+			localVue,
+			propsData: { text: "https://example.com/" },
+			mounted: jest.fn(),
+		});
+		wrapper.vm.processText();
+		expect(wrapper.vm.content).toHaveLength(1);
+		expect(wrapper.vm.content).toEqual([{ type: "link", text: "https://example.com/" }]);
+	});
+
+	it("should render text and link", () => {
+		let wrapper = mount(ProcessedText, {
+			localVue,
+			propsData: { text: "peter https://example.com/ griffin" },
+			mounted: jest.fn(),
+		});
+		wrapper.vm.processText();
+		expect(wrapper.vm.content).toHaveLength(3);
+		expect(wrapper.vm.content).toEqual([
+			{ type: "text", text: "peter " },
+			{ type: "link", text: "https://example.com/" },
+			{ type: "text", text: " griffin" },
+		]);
+	});
+});
