50617436212ae1c34c5b5c3a302a940426f40f2e
Carson McManus <carson.mcmanus1@gmail.com>
2020-08-16 15:30:40 -0400

    fix home page hero stuff on mobile

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,19 @@
 <template>
 	<div class="home">
 		<v-container class="hero" fluid fill-height grid-list-md text-xs-center>
-			<v-row align="center" justify="center">
-				<div>
+			<v-row align="center" justify="center" no-gutters>
+				<div class="hero-text" cols="12" sm="10" md="8" lg="6">
 					<h1>Enjoy Together.</h1>
 					<span>
 						Real-time syncronized playback. Optional voting system.<br>
 						Dark theme. No sign up required. All Open Source.<br>
 						It's never been easier to watch videos together.
 					</span>
-					<v-layout :row="$vuetify.breakpoint.smAndUp"
-							:column="$vuetify.breakpoint.xs"
-							:justify-space-between="$vuetify.breakpoint.smAndUp"
-							:justify-space-around="$vuetify.breakpoint.xs">
+					<div class="hero-buttons">
 						<v-btn elevation="12" x-large @click="createTempRoom">Create Room</v-btn>
 						<v-btn elevation="12" x-large to="/rooms">Browse Rooms</v-btn>
 						<v-btn elevation="12" x-large href="https://github.com/dyc3/opentogethertube">View Source</v-btn>
-					</v-layout>
+					</div>
 				</div>
 			</v-row>
 		</v-container>
@@ -140,16 +137,10 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "../variables.scss";
+
 .home {
 	width: 100%;
-}
-
-@media only screen and (max-width: 1264px) {
-	.hero {
-		.v-btn {
-			margin-top: 22px;
-		}
-	}
 }
 
 .hero {
@@ -158,19 +149,39 @@ export default {
 	font-size: 22px;
 	height: 100vh;
 	min-height: 350px;
+	display: flex;
 
 	h1 {
 		font-size: 52px;
 	}
 
-	@media only screen and (max-width: 600px) {
+	@media only screen and (max-width: $xs-max) {
 		h1 {
 			font-size: 48px;
 		}
 	}
 
-	.v-btn {
-		background: transparent !important;
+	.hero-text {
+		display: flex;
+		flex-direction: column;
+
+		.hero-buttons {
+			display: flex;
+			flex-direction: row;
+			justify-content: space-between;
+
+			.v-btn {
+				background: transparent !important;
+				// margin: 0 10px;
+				@media only screen and (max-width: $md-max) {
+					margin-top: 22px;
+				}
+			}
+
+			@media screen and (max-width: $sm-max) {
+				flex-direction: column;
+			}
+		}
 	}
 }
 </style>
