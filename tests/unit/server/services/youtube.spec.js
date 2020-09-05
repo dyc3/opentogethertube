736bc6660c4e1b73df31896f4522096ec6f415fd
Marius Becker <marius.becker.8@gmail.com>
2020-09-05 20:59:38 +0200

    Add more tests for YouTube

--- tests/unit/server/services/youtube.spec.js
+++ tests/unit/server/services/youtube.spec.js
@@ -1,10 +1,15 @@
 const YouTubeAdapter = require("../../../../server/services/youtube");
 const Video = require("../../../../common/video");
-const { InvalidVideoIdException } = require("../../../../server/exceptions");
+const {
+  InvalidVideoIdException,
+  OutOfQuotaException,
+} = require("../../../../server/exceptions");
 const { redisClient } = require("../../../../redisclient");
 
-jest.spyOn(redisClient, 'get').mockImplementation((key, callback) => callback(null, null));
-jest.spyOn(redisClient, 'set').mockImplementation();
+jest
+  .spyOn(redisClient, "get")
+  .mockImplementation((key, callback) => callback(null, null));
+jest.spyOn(redisClient, "set").mockImplementation();
 
 const validVideoLinks = [
   ["3kw2_89ym31W", "https://youtube.com/watch?v=3kw2_89ym31W"],
@@ -31,17 +36,43 @@ const validCollectionLinks = [
 ];
 
 const youtubeVideoListSampleResponses = {
-  "BTZ5KVRUy1Q": '{"kind": "youtube#videoListResponse","etag": "\\"j6xRRd8dTPVVptg711_CSPADRfg/dqnBDym87ibK6816BZIGb9MCLYI\\"","pageInfo": {"totalResults": 1,"resultsPerPage": 1},"items": [{"kind": "youtube#video","etag": "\\"j6xRRd8dTPVVptg711_CSPADRfg/UyysisXjek5qf_mfkU7W8pFnmPs\\"","id": "BTZ5KVRUy1Q","snippet": {"publishedAt": "2019-08-26T11:32:44.000Z","channelId": "UCsLiV4WJfkTEHH0b9PmRklw","title": "tmpIwT4T4","description": "tmpIwT4T4","thumbnails": {"default": {"url": "https://i.ytimg.com/vi/BTZ5KVRUy1Q/default.jpg","width": 120,"height": 90},"medium": {"url": "https://i.ytimg.com/vi/BTZ5KVRUy1Q/mqdefault.jpg","width": 320,"height": 180},"high": {"url": "https://i.ytimg.com/vi/BTZ5KVRUy1Q/hqdefault.jpg","width": 480,"height": 360},"standard": {"url": "https://i.ytimg.com/vi/BTZ5KVRUy1Q/sddefault.jpg","width": 640,"height": 480}},"channelTitle": "Webdriver Torso","categoryId": "22","liveBroadcastContent": "none","localized": {"title": "tmpIwT4T4","description": "tmpIwT4T4"}},"contentDetails": {"duration": "PT10S","dimension": "2d","definition": "sd","caption": "false","licensedContent": true,"projection": "rectangular"}}]}',
-  "I3O9J02G67I": '{"kind": "youtube#videoListResponse","etag": "\\"j6xRRd8dTPVVptg711_CSPADRfg/Ly8EM_vOONCLOEzI8TMYnzfG37k\\"","pageInfo": {"totalResults": 1,"resultsPerPage": 1},"items": [{"kind": "youtube#video","etag": "\\"j6xRRd8dTPVVptg711_CSPADRfg/Xz7huLjXglgWYbMv-lMOshzynvk\\"","id": "I3O9J02G67I","snippet": {"publishedAt": "2019-07-26T13:02:54.000Z","channelId": "UCsLiV4WJfkTEHH0b9PmRklw","title": "tmpATT2Cp","description": "tmpATT2Cp","thumbnails": {"default": {"url": "https://i.ytimg.com/vi/I3O9J02G67I/default.jpg","width": 120,"height": 90},"high": {"url": "https://i.ytimg.com/vi/I3O9J02G67I/hqdefault.jpg","width": 480,"height": 360},"standard": {"url": "https://i.ytimg.com/vi/I3O9J02G67I/sddefault.jpg","width": 640,"height": 480}},"channelTitle": "Webdriver Torso","categoryId": "22","liveBroadcastContent": "none","localized": {"title": "tmpATT2Cp","description": "tmpATT2Cp"}},"contentDetails": {"duration": "PT10S","dimension": "2d","definition": "sd","caption": "false","licensedContent": false,"projection": "rectangular"}}]}',
-  "BTZ5KVRUy1Q,I3O9J02G67I": '{"kind": "youtube#videoListResponse","etag": "\\"j6xRRd8dTPVVptg711_CSPADRfg/dqnBDym87ibK6816BZIGb9MCLYI\\"","pageInfo": {"totalResults": 2,"resultsPerPage": 2},"items": [{"kind": "youtube#video","etag": "\\"j6xRRd8dTPVVptg711_CSPADRfg/UyysisXjek5qf_mfkU7W8pFnmPs\\"","id": "BTZ5KVRUy1Q","snippet": {"publishedAt": "2019-08-26T11:32:44.000Z","channelId": "UCsLiV4WJfkTEHH0b9PmRklw","title": "tmpIwT4T4","description": "tmpIwT4T4","thumbnails": {"default": {"url": "https://i.ytimg.com/vi/BTZ5KVRUy1Q/default.jpg","width": 120,"height": 90},"medium": {"url": "https://i.ytimg.com/vi/BTZ5KVRUy1Q/mqdefault.jpg","width": 320,"height": 180},"high": {"url": "https://i.ytimg.com/vi/BTZ5KVRUy1Q/hqdefault.jpg","width": 480,"height": 360},"standard": {"url": "https://i.ytimg.com/vi/BTZ5KVRUy1Q/sddefault.jpg","width": 640,"height": 480}},"channelTitle": "Webdriver Torso","categoryId": "22","liveBroadcastContent": "none","localized": {"title": "tmpIwT4T4","description": "tmpIwT4T4"}},"contentDetails": {"duration": "PT10S","dimension": "2d","definition": "sd","caption": "false","licensedContent": true,"projection": "rectangular"}}, {"kind": "youtube#video","etag": "\\"j6xRRd8dTPVVptg711_CSPADRfg/Xz7huLjXglgWYbMv-lMOshzynvk\\"","id": "I3O9J02G67I","snippet": {"publishedAt": "2019-07-26T13:02:54.000Z","channelId": "UCsLiV4WJfkTEHH0b9PmRklw","title": "tmpATT2Cp","description": "tmpATT2Cp","thumbnails": {"default": {"url": "https://i.ytimg.com/vi/I3O9J02G67I/default.jpg","width": 120,"height": 90},"high": {"url": "https://i.ytimg.com/vi/I3O9J02G67I/hqdefault.jpg","width": 480,"height": 360},"standard": {"url": "https://i.ytimg.com/vi/I3O9J02G67I/sddefault.jpg","width": 640,"height": 480}},"channelTitle": "Webdriver Torso","categoryId": "22","liveBroadcastContent": "none","localized": {"title": "tmpATT2Cp","description": "tmpATT2Cp"}},"contentDetails": {"duration": "PT10S","dimension": "2d","definition": "sd","caption": "false","licensedContent": false,"projection": "rectangular"}}]}',
+  BTZ5KVRUy1Q:
+    '{"kind": "youtube#videoListResponse","etag": "\\"j6xRRd8dTPVVptg711_CSPADRfg/dqnBDym87ibK6816BZIGb9MCLYI\\"","pageInfo": {"totalResults": 1,"resultsPerPage": 1},"items": [{"kind": "youtube#video","etag": "\\"j6xRRd8dTPVVptg711_CSPADRfg/UyysisXjek5qf_mfkU7W8pFnmPs\\"","id": "BTZ5KVRUy1Q","snippet": {"publishedAt": "2019-08-26T11:32:44.000Z","channelId": "UCsLiV4WJfkTEHH0b9PmRklw","title": "tmpIwT4T4","description": "tmpIwT4T4","thumbnails": {"default": {"url": "https://i.ytimg.com/vi/BTZ5KVRUy1Q/default.jpg","width": 120,"height": 90},"medium": {"url": "https://i.ytimg.com/vi/BTZ5KVRUy1Q/mqdefault.jpg","width": 320,"height": 180},"high": {"url": "https://i.ytimg.com/vi/BTZ5KVRUy1Q/hqdefault.jpg","width": 480,"height": 360},"standard": {"url": "https://i.ytimg.com/vi/BTZ5KVRUy1Q/sddefault.jpg","width": 640,"height": 480}},"channelTitle": "Webdriver Torso","categoryId": "22","liveBroadcastContent": "none","localized": {"title": "tmpIwT4T4","description": "tmpIwT4T4"}},"contentDetails": {"duration": "PT10S","dimension": "2d","definition": "sd","caption": "false","licensedContent": true,"projection": "rectangular"}}]}',
+  I3O9J02G67I:
+    '{"kind": "youtube#videoListResponse","etag": "\\"j6xRRd8dTPVVptg711_CSPADRfg/Ly8EM_vOONCLOEzI8TMYnzfG37k\\"","pageInfo": {"totalResults": 1,"resultsPerPage": 1},"items": [{"kind": "youtube#video","etag": "\\"j6xRRd8dTPVVptg711_CSPADRfg/Xz7huLjXglgWYbMv-lMOshzynvk\\"","id": "I3O9J02G67I","snippet": {"publishedAt": "2019-07-26T13:02:54.000Z","channelId": "UCsLiV4WJfkTEHH0b9PmRklw","title": "tmpATT2Cp","description": "tmpATT2Cp","thumbnails": {"default": {"url": "https://i.ytimg.com/vi/I3O9J02G67I/default.jpg","width": 120,"height": 90},"high": {"url": "https://i.ytimg.com/vi/I3O9J02G67I/hqdefault.jpg","width": 480,"height": 360},"standard": {"url": "https://i.ytimg.com/vi/I3O9J02G67I/sddefault.jpg","width": 640,"height": 480}},"channelTitle": "Webdriver Torso","categoryId": "22","liveBroadcastContent": "none","localized": {"title": "tmpATT2Cp","description": "tmpATT2Cp"}},"contentDetails": {"duration": "PT10S","dimension": "2d","definition": "sd","caption": "false","licensedContent": false,"projection": "rectangular"}}]}',
+  "BTZ5KVRUy1Q,I3O9J02G67I":
+    '{"kind": "youtube#videoListResponse","etag": "\\"j6xRRd8dTPVVptg711_CSPADRfg/dqnBDym87ibK6816BZIGb9MCLYI\\"","pageInfo": {"totalResults": 2,"resultsPerPage": 2},"items": [{"kind": "youtube#video","etag": "\\"j6xRRd8dTPVVptg711_CSPADRfg/UyysisXjek5qf_mfkU7W8pFnmPs\\"","id": "BTZ5KVRUy1Q","snippet": {"publishedAt": "2019-08-26T11:32:44.000Z","channelId": "UCsLiV4WJfkTEHH0b9PmRklw","title": "tmpIwT4T4","description": "tmpIwT4T4","thumbnails": {"default": {"url": "https://i.ytimg.com/vi/BTZ5KVRUy1Q/default.jpg","width": 120,"height": 90},"medium": {"url": "https://i.ytimg.com/vi/BTZ5KVRUy1Q/mqdefault.jpg","width": 320,"height": 180},"high": {"url": "https://i.ytimg.com/vi/BTZ5KVRUy1Q/hqdefault.jpg","width": 480,"height": 360},"standard": {"url": "https://i.ytimg.com/vi/BTZ5KVRUy1Q/sddefault.jpg","width": 640,"height": 480}},"channelTitle": "Webdriver Torso","categoryId": "22","liveBroadcastContent": "none","localized": {"title": "tmpIwT4T4","description": "tmpIwT4T4"}},"contentDetails": {"duration": "PT10S","dimension": "2d","definition": "sd","caption": "false","licensedContent": true,"projection": "rectangular"}}, {"kind": "youtube#video","etag": "\\"j6xRRd8dTPVVptg711_CSPADRfg/Xz7huLjXglgWYbMv-lMOshzynvk\\"","id": "I3O9J02G67I","snippet": {"publishedAt": "2019-07-26T13:02:54.000Z","channelId": "UCsLiV4WJfkTEHH0b9PmRklw","title": "tmpATT2Cp","description": "tmpATT2Cp","thumbnails": {"default": {"url": "https://i.ytimg.com/vi/I3O9J02G67I/default.jpg","width": 120,"height": 90},"high": {"url": "https://i.ytimg.com/vi/I3O9J02G67I/hqdefault.jpg","width": 480,"height": 360},"standard": {"url": "https://i.ytimg.com/vi/I3O9J02G67I/sddefault.jpg","width": 640,"height": 480}},"channelTitle": "Webdriver Torso","categoryId": "22","liveBroadcastContent": "none","localized": {"title": "tmpATT2Cp","description": "tmpATT2Cp"}},"contentDetails": {"duration": "PT10S","dimension": "2d","definition": "sd","caption": "false","licensedContent": false,"projection": "rectangular"}}]}',
+};
+
+const channelInfoSampleResponses = [
+  {
+    data: {
+      items: [
+        {
+          contentDetails: {
+            relatedPlaylists: { uploads: "channeluploadplaylistid" },
+          },
+        },
+      ],
+    },
+  },
+];
+
+const emptyPlaylistItemsResponse = {
+  data: {
+    items: [],
+  },
 };
 
 describe("canHandleLink", () => {
   const adapter = new YouTubeAdapter("");
 
-  it.each(validVideoLinks.map(l => l[1]).concat(validCollectionLinks))("Accepts %s", (link) => {
-    expect(adapter.canHandleLink(link)).toBe(true);
-  });
+  it.each(validVideoLinks.map((l) => l[1]).concat(validCollectionLinks))(
+    "Accepts %s",
+    (link) => {
+      expect(adapter.canHandleLink(link)).toBe(true);
+    }
+  );
 
   it.each(invalidLinks)("Rejects %s", (link) => {
     expect(adapter.canHandleLink(link)).toBe(false);
@@ -51,7 +82,7 @@ describe("canHandleLink", () => {
 describe("isCollectionURL", () => {
   const adapter = new YouTubeAdapter("");
 
-  it.each(validVideoLinks.map(l => l[1]))("Returns false for %s", (link) => {
+  it.each(validVideoLinks.map((l) => l[1]))("Returns false for %s", (link) => {
     expect(adapter.isCollectionURL(link)).toBe(false);
   });
 
@@ -72,9 +103,11 @@ describe("fetchVideoInfo", () => {
   const adapter = new YouTubeAdapter("");
   const apiGet = jest.fn();
   const videoId = "BTZ5KVRUy1Q";
-  apiGet.mockReturnValue(Promise.resolve({
-    data: JSON.parse(youtubeVideoListSampleResponses[videoId]),
-  }));
+  apiGet.mockReturnValue(
+    Promise.resolve({
+      data: JSON.parse(youtubeVideoListSampleResponses[videoId]),
+    })
+  );
   adapter.api.get = apiGet;
 
   beforeEach(() => {
@@ -103,11 +136,8 @@ describe("fetchVideoInfo", () => {
 });
 
 describe("resolveURL", () => {
-  const adapter = new YouTubeAdapter("");
-  const apiGet = jest.fn().mockReturnValue(Promise.resolve({
-    data: { items: [] },
-  }));
-  adapter.api.get = apiGet;
+  const adapter = new YouTubeAdapter("", redisClient);
+  const apiGet = jest.spyOn(adapter.api, "get");
 
   beforeEach(() => {
     apiGet.mockClear();
@@ -117,9 +147,77 @@ describe("resolveURL", () => {
 
   it("Resolves channel URLs", async () => {
     const channelId = "89hasd9h2lalskh8";
-    const channelURL = `https://www.youtube.com/watch?v=${channelId}`;
+    const channelURL = `https://www.youtube.com/channel/${channelId}`;
+
+    apiGet
+      .mockResolvedValueOnce(channelInfoSampleResponses[0])
+      .mockResolvedValueOnce(emptyPlaylistItemsResponse);
 
     await adapter.resolveURL(channelURL);
-    expect(apiGet).toBeCalled();
+    expect(apiGet).toHaveBeenCalledTimes(2);
+    expect(apiGet).toHaveBeenNthCalledWith(1, "/channels", {
+      params: expect.objectContaining({ id: channelId }),
+    });
+    expect(apiGet).toHaveBeenNthCalledWith(2, "/playlistItems", {
+      params: expect.objectContaining({
+        playlistId: "channeluploadplaylistid",
+      }),
+    });
+  });
+
+  it("Resolves user URLs", async () => {
+    const userName = "someuserthatdoesntactuallyexists";
+    const channelURL = `https://www.youtube.com/user/${userName}`;
+
+    apiGet
+      .mockResolvedValueOnce(channelInfoSampleResponses[0])
+      .mockResolvedValueOnce(emptyPlaylistItemsResponse);
+
+    await adapter.resolveURL(channelURL);
+    expect(apiGet).toHaveBeenCalledTimes(2);
+    expect(apiGet).toHaveBeenNthCalledWith(1, "/channels", {
+      params: expect.objectContaining({ forUsername: userName }),
+    });
+    expect(apiGet).toHaveBeenNthCalledWith(2, "/playlistItems", {
+      params: expect.objectContaining({
+        playlistId: "channeluploadplaylistid",
+      }),
+    });
+  });
+});
+
+describe("searchVideos", () => {
+  const adapter = new YouTubeAdapter("");
+  const apiGet = jest.spyOn(adapter.api, "get");
+
+  beforeEach(() => {
+    apiGet.mockClear();
+  });
+
+  it("Queries the YouTube API for videos", async () => {
+    apiGet.mockResolvedValue({ data: { items: [] } });
+    const searchQuery = "Testing";
+
+    await adapter.searchVideos(searchQuery);
+
+    expect(apiGet).toHaveBeenCalledTimes(1);
+    expect(apiGet).toHaveBeenCalledWith("/search", {
+      params: expect.objectContaining({ q: searchQuery }),
+    });
+  });
+
+  it("Reports out of quota errors", async () => {
+    apiGet.mockRejectedValue({ response: { status: 403 } });
+
+    await expect(adapter.searchVideos("")).rejects.toThrow(OutOfQuotaException);
+  });
+
+  it("Re-throws all other errors", async () => {
+    const response = { response: { status: 400 } };
+    apiGet.mockRejectedValue(response);
+
+    const promise = adapter.searchVideos("");
+    await expect(promise).rejects.toEqual(response);
+    await expect(promise).rejects.not.toBeInstanceOf(OutOfQuotaException);
   });
 });
