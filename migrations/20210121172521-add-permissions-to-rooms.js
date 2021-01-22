f8d2315928b5ee9b07b60a9b5072e888359e5534
Carson McManus <carson.mcmanus1@gmail.com>
2021-01-22 00:19:41 -0500

    save permissions to database

---
+++ migrations/20210121172521-add-permissions-to-rooms.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const ROLES = {
+	ADMINISTRATOR: 4,
+	MODERATOR: 3,
+	TRUSTED_USER: 2,
+	REGISTERED_USER: 1,
+	UNREGISTERED_USER: 0,
+};
+
+const ROLE_NAMES = {
+	[ROLES.ADMINISTRATOR]: "admin",
+	[ROLES.MODERATOR]: "mod",
+	[ROLES.TRUSTED_USER]: "trusted",
+	[ROLES.REGISTERED_USER]: "registered",
+	[ROLES.UNREGISTERED_USER]: "unregistered",
+};
+
+module.exports = {
+	up: async (queryInterface, Sequelize) => {
+		await queryInterface.addColumn('Rooms', 'permissions', {
+			type: Sequelize.ARRAY(Sequelize.TEXT),
+		});
+		for (const role of Object.values(ROLES)) {
+			if (role < ROLES.TRUSTED_USER) {
+				continue;
+			}
+			await queryInterface.addColumn('Rooms', `role-${ROLE_NAMES[role]}`, {
+				type: Sequelize.ARRAY(Sequelize.TEXT),
+			});
+		}
+	},
+
+	down: async (queryInterface, Sequelize) => {
+		await queryInterface.removeColumn('Rooms', 'permissions');
+		for (const role of Object.values(ROLES)) {
+			if (role < ROLES.TRUSTED_USER) {
+				continue;
+			}
+			await queryInterface.removeColumn('Rooms', `role-${ROLE_NAMES[role]}`);
+		}
+	},
+};
