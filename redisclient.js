38541579a069d56d6996844fabce09b22faec867
Carson McManus <carson.mcmanus1@gmail.com>
2021-05-08 18:46:39 -0400

    don't check redis certificate

--- redisclient.js
+++ redisclient.js
@@ -1,7 +1,11 @@
 const redis = require('redis');
 
 const redisClient = (process.env.REDIS_TLS_URL || process.env.REDIS_URL) ?
-	redis.createClient(process.env.REDIS_TLS_URL || process.env.REDIS_URL) :
+	redis.createClient(process.env.REDIS_TLS_URL || process.env.REDIS_URL, {
+		tls: {
+			rejectUnauthorized: false,
+		},
+	}) :
 	redis.createClient({
 		port: process.env.REDIS_PORT || undefined,
 		host: process.env.REDIS_HOST || undefined,
