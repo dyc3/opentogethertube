f41935c70a831ff9715d854bfff1f1be92a1ebbd
Carson McManus <carson.mcmanus1@gmail.com>
2021-05-23 11:24:47 -0400

    fix most linting warnings in clientmanager, throw a couple of exceptions

--- server/clientmanager.ts
+++ server/clientmanager.ts
@@ -11,15 +11,15 @@ import { ClientMessage, ClientMessageSeek, RoomRequest, RoomRequestType, ServerM
 import { RoomNotFoundException } from "./exceptions";
 import { ClientInfo, RoomState, MySession, OttWebsocketError } from "./types";
 // WARN: do NOT import roommanager
-import roommanager from "./roommanager" // this is temporary because these modules are supposed to be completely isolated. In the future, it should send room requests via the HTTP API to other nodes.
+import roommanager from "./roommanager"; // this is temporary because these modules are supposed to be completely isolated. In the future, it should send room requests via the HTTP API to other nodes.
 
 const log = getLogger("clientmanager");
 const redisSubscriber = createSubscriber();
 const get = promisify(redisClient.get).bind(redisClient);
 const subscribe: (channel: string) => Promise<string> = promisify(redisSubscriber.subscribe).bind(redisSubscriber);
-let connections: Client[] = [];
-let roomStates: Map<string, RoomState> = new Map();
-let roomJoins: Map<string, Client[]> = new Map();
+const connections: Client[] = [];
+const roomStates: Map<string, RoomState> = new Map();
+const roomJoins: Map<string, Client[]> = new Map();
 
 export class Client {
 	id: string
@@ -34,67 +34,67 @@ export class Client {
 		this.room = null;
 
 		this.Socket.on("close", async (code, reason) => {
-			log.debug(`socket closed: ${code}, ${reason}`)
-			let idx = _.findIndex(connections, { id: this.id });
-			connections.splice(idx, 1)
+			log.debug(`socket closed: ${code}, ${reason}`);
+			const idx = _.findIndex(connections, { id: this.id });
+			connections.splice(idx, 1);
 
 			if (this.room) {
-				let room = await roommanager.GetRoom(this.room);
+				const room = await roommanager.GetRoom(this.room);
 				await room.processRequest({
 					type: RoomRequestType.LeaveRequest,
-					id: this.id
-				})
+					id: this.id,
+				});
 			}
-		})
+		});
 	}
 
 	get clientInfo(): ClientInfo {
 		if (this.Session.passport && this.Session.passport.user) {
 			return {
 				id: this.id,
-				user_id: this.Session.passport.user
-			}
+				user_id: this.Session.passport.user,
+			};
 		}
 		else if (this.Session.username) {
 			return {
 				id: this.id,
 				username: this.Session.username,
-			}
+			};
 		}
 		else {
 			throw new TypeError("Session did not have username present, nor passport user id");
 		}
 	}
 
-	public async OnMessage(text: string) {
+	public async OnMessage(text: string): Promise<void> {
 		log.debug(text);
-		let msg: ClientMessage = JSON.parse(text);
+		const msg: ClientMessage = JSON.parse(text);
 		if (msg.action === "play") {
 			await this.makeRoomRequest({
 				type: RoomRequestType.PlaybackRequest,
 				permission: "playback.play-pause",
 				state: true,
-			})
+			});
 		}
 		else if (msg.action === "pause") {
 			await this.makeRoomRequest({
 				type: RoomRequestType.PlaybackRequest,
 				permission: "playback.play-pause",
 				state: false,
-			})
+			});
 		}
 		else if (msg.action === "skip") {
 			await this.makeRoomRequest({
 				type: RoomRequestType.SkipRequest,
 				permission: "playback.skip",
-			})
+			});
 		}
 		else if (msg.action === "seek") {
 			await this.makeRoomRequest({
 				type: RoomRequestType.SeekRequest,
 				permission: "playback.seek",
 				value: msg.position,
-			})
+			});
 		}
 		else if (msg.action === "queue-move") {
 			await this.makeRoomRequest({
@@ -102,7 +102,7 @@ export class Client {
 				permission: "manage-queue.order",
 				fromIdx: msg.currentIdx,
 				toIdx: msg.targetIdx,
-			})
+			});
 		}
 		else if (msg.action === "kickme") {
 			this.Socket.close(OttWebsocketError.UNKNOWN);
@@ -113,65 +113,65 @@ export class Client {
 		// }
 	}
 
-	public OnPing(data: Buffer) {
+	public OnPing(data: Buffer): void {
 		log.debug(`sending pong`);
 		this.Socket.pong();
 	}
 
-	public async JoinRoom(roomName: string) {
+	public async JoinRoom(roomName: string): Promise<void> {
 		log.debug(`client id=${this.id} joining ${roomName}`);
 		this.room = roomName;
 
-		let room = await roommanager.GetRoom(roomName);
+		const room = await roommanager.GetRoom(roomName);
 		if (!room) {
 			throw new RoomNotFoundException(roomName);
 		}
 		// full sync
 		let state = roomStates.get(roomName);
 		if (state === undefined) {
-			log.warn("room state not present, grabbing")
-			let stateText = await get(`room:${roomName}`);
-			state = JSON.parse(stateText!)!;
-			roomStates.set(roomName, state!);
+			log.warn("room state not present, grabbing");
+			const stateText = await get(`room:${roomName}`);
+			state = JSON.parse(stateText);
+			roomStates.set(roomName, state);
 		}
-		let syncMsg: ServerMessageSync = Object.assign({action: "sync"}, state) as ServerMessageSync;
+		const syncMsg: ServerMessageSync = Object.assign({action: "sync"}, state) as ServerMessageSync;
 		this.Socket.send(JSON.stringify(syncMsg));
 
 		// actually join the room
 		await room.processRequest({
 			type: RoomRequestType.JoinRequest,
 			info: this.clientInfo,
-		})
+		});
 		subscribe(`room:${roomName}`);
 		let clients = roomJoins.get(roomName);
 		if (clients === undefined) {
-			log.warn("room joins not present, creating")
+			log.warn("room joins not present, creating");
 			clients = [];
 		}
 		clients.push(this);
 		roomJoins.set(roomName, clients);
 	}
 
-	public async makeRoomRequest(request: RoomRequest) {
-		let room = await roommanager.GetRoom(this.room!); // FIXME: only get room if it is loaded already.
+	public async makeRoomRequest(request: RoomRequest): Promise<void> {
+		const room = await roommanager.GetRoom(this.room); // FIXME: only get room if it is loaded already.
 		if (!room) {
-			log.error(`room not found: ${this.room}`)
+			throw new RoomNotFoundException(this.room);
 		}
 		await room.processRequest(request);
 	}
 }
 
-export function Setup() {
+export function Setup(): void {
 	log.debug("setting up client manager...");
 	const server = wss as WebSocket.Server;
 	server.on("connection", async (ws, req: Request & { session: Session }) => {
-		if (!req.url!.startsWith("/api/room/")) {
+		if (!req.url.startsWith("/api/room/")) {
 			log.error("Rejecting connection because the connection url was invalid");
 			ws.close(OttWebsocketError.INVALID_CONNECTION_URL, "Invalid connection url");
 			return;
 		}
 		await OnConnect(req.session, ws, req);
-	})
+	});
 }
 
 /**
@@ -180,9 +180,9 @@ export function Setup() {
  * @param socket
  */
 async function OnConnect(session: Session, socket: WebSocket, req: Request) {
-	let roomName = req.url!.replace("/api/room/", "");
-	log.debug(`connection received: ${roomName}`)
-	let client = new Client(session as MySession, socket);
+	const roomName = req.url.replace("/api/room/", "");
+	log.debug(`connection received: ${roomName}`);
+	const client = new Client(session as MySession, socket);
 	connections.push(client);
 	socket.on("ping", (data) => client.OnPing(data));
 	socket.on("message", (data) => client.OnMessage(data as string));
@@ -193,7 +193,8 @@ async function OnConnect(session: Session, socket: WebSocket, req: Request) {
 		log.error(`Failed to join room: ${e.message}`);
 		if (e instanceof RoomNotFoundException) {
 			socket.close(OttWebsocketError.ROOM_NOT_FOUND);
-		} else {
+		}
+ else {
 			socket.close(OttWebsocketError.UNKNOWN);
 		}
 	}
@@ -205,18 +206,18 @@ redisSubscriber.on("message", async (channel, text) => {
 	if (!channel.startsWith("room:")) {
 		return;
 	}
-	let roomName = channel.replace("room:", "")
-	let msg = JSON.parse(text) as ServerMessage;
+	const roomName = channel.replace("room:", "");
+	const msg = JSON.parse(text) as ServerMessage;
 	if (msg.action === "sync") {
-		let state = roomStates.get(roomName!);
+		let state = roomStates.get(roomName);
 		if (state === undefined) {
-			let stateText = await get(`room:${roomName}`);
-			state = JSON.parse(stateText!)! as RoomState;
+			const stateText = await get(`room:${roomName}`);
+			state = JSON.parse(stateText) as RoomState;
 		}
-		Object.assign(state, _.omit(msg, "action"))
-		roomStates.set(roomName!, state);
+		Object.assign(state, _.omit(msg, "action"));
+		roomStates.set(roomName, state);
 
-		for (let client of roomJoins.get(roomName)!) {
+		for (const client of roomJoins.get(roomName)) {
 			try {
 				client.Socket.send(text);
 			}
@@ -226,21 +227,21 @@ redisSubscriber.on("message", async (channel, text) => {
 		}
 	}
 	else if (msg.action === "unload") {
-		for (let client of roomJoins.get(roomName)!) {
+		for (const client of roomJoins.get(roomName)) {
 			client.Socket.close(OttWebsocketError.ROOM_UNLOADED, "The room was unloaded.");
 		}
 	}
 });
 
 async function onUserModified(session: MySession, newUsername: string) {
-	log.debug(`User was modified: ${session}, newUsername=${newUsername}, telling rooms`)
-	for (let client of connections) {
+	log.debug(`User was modified: ${session}, newUsername=${newUsername}, telling rooms`);
+	for (const client of connections) {
 		if (client.Session.id === session.id) {
 			client.Session = session;
 			await client.makeRoomRequest({
 				type: RoomRequestType.UpdateUser,
 				info: client.clientInfo,
-			})
+			});
 		}
 	}
 }
@@ -248,4 +249,4 @@ async function onUserModified(session: MySession, newUsername: string) {
 export default {
 	Setup,
 	onUserModified,
-}
+};
