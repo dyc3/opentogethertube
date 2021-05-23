8d113d8a9ca02e4503cf46fac28767c054096fd4
Carson McManus <carson.mcmanus1@gmail.com>
2021-05-23 15:58:41 -0400

    remove old announcement component
    announcements must go through toast notifications

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,5 @@
 <template>
   <v-app id="app">
-    <div class="announcement-container">
-      <v-alert class="announcement" type="warning" border="left" dismissible close-label="Close Announcement" v-model="showAnnouncement" transition="scroll-y-transition">
-        SYSTEM: {{ announcement }}
-      </v-alert>
-    </div>
     <v-app-bar app :absolute="!$store.state.fullscreen" :inverted-scroll="$store.state.fullscreen">
       <v-app-bar-nav-icon @click="drawer = true" />
       <v-img :src="require('@/assets/logo.svg')" max-width="32" max-height="32" contain style="margin-right: 8px" />
@@ -107,18 +102,12 @@ export default {
   mixins: [RoomUtilsMixin],
   data() {
     return {
-      announcement: null,
-      showAnnouncement: false,
       showCreateRoomForm: false,
       showLogin: false,
       drawer: false,
     };
   },
   methods: {
-    onAnnouncement(text) {
-      this.showAnnouncement = true;
-      this.announcement = text;
-    },
     logout() {
       API.post("/user/logout").then(res => {
         if (res.data.success) {
@@ -165,18 +154,6 @@ export default {
 
 .vue-slider-process {
   background: $brand-color !important;
-}
-
-.announcement-container {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  z-index: 10000;
-
-  .announcement {
-    margin: 10px;
-  }
 }
 
 .text-muted {
