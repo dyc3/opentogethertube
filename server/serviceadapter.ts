94832e8defb9ec242cecb8e5b9de5a3ead17c4dd
Carson McManus <carson.mcmanus1@gmail.com>
2021-05-25 01:01:27 -0400

    convert serviceadapter to ts

--- server/serviceadapter.ts
+++ server/serviceadapter.ts
@@ -1,44 +1,42 @@
+import { Video, VideoId } from "../common/models/video";
 import { IncompleteServiceAdapterException } from "./exceptions";
+
 export class ServiceAdapter {
   /**
    * A string that identifies this service adapter.
    */
-  get serviceId() {
+  get serviceId(): string {
     throw new IncompleteServiceAdapterException(`Service adapter ${this.constructor.name} does note have a serviceId property`);
   }
 
   /**
    * A boolean that indicates whether video metadata can be safely cached.
-   * @returns {boolean}
    */
-  get isCacheSafe() {
+  get isCacheSafe(): boolean {
     return true;
   }
 
   /**
    * Returns true if this service adapter can handle a given link.
    * @param {string} link
-   * @returns {boolean}
    */
-  canHandleURL() {
+  canHandleURL(): boolean {
     return false;
   }
 
   /**
    * Determines whether a given URL points to a collection of videos.
    * @param {string} url
-   * @returns {boolean}
    */
-  isCollectionURL() {
+  isCollectionURL(): boolean {
     throw new IncompleteServiceAdapterException(`Service ${this.serviceId} does not implement method isCollectionURL`);
   }
 
   /**
    * Returns the video ID from a URL.
    * @param {string} url
-   * @returns {string}
    */
-  getVideoId() {
+  getVideoId(): string {
     throw new IncompleteServiceAdapterException(`Service ${this.serviceId} does not implement method getVideoId`);
   }
 
@@ -46,18 +44,16 @@ export class ServiceAdapter {
    * Fetches video metadata from the API.
    * @param {string} url
    * @param {string[]} properties
-   * @returns {Promise}
    */
-  fetchVideoInfo() {
+  fetchVideoInfo(): Promise<Video> {
     throw new IncompleteServiceAdapterException(`Service ${this.serviceId} does not implement method getVideoInfo`);
   }
 
   /**
    * Fetches video metadata for a list of IDs.
-   * @param {Array} requests List of objects with id and missingInfo keys
-   * @returns {Promise}
+   * @param {VideoId[]} requests List of objects with id and missingInfo keys
    */
-  fetchManyVideoInfo() {
+  fetchManyVideoInfo(): Promise<Video[]> {
     throw new IncompleteServiceAdapterException(`Service ${this.serviceId} does not implement method getManyVideoInfo`);
   }
 
@@ -65,18 +61,16 @@ export class ServiceAdapter {
    * Fetches all videos associated with a URL.
    * @param {string} url
    * @param {string[]} properties
-   * @returns {Promise}
    */
-  resolveURL() {
+  resolveURL(): Promise<Video[]> {
     throw new IncompleteServiceAdapterException(`Service ${this.serviceId} does not implement method resolveURL`);
   }
 
   /**
    * Searches a video service.
    * @param {string} query
-   * @returns {Promise}
    */
-  searchVideos() {
+  async searchVideos(): Promise<Video[]> {
     return [];
   }
 }
