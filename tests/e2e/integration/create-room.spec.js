cf96f3f82ea2641fcaf74cfe80bcc5f0742761c1
Carson McManus <carson.mcmanus1@gmail.com>
2021-05-28 20:11:13 -0400

    add create temporary room test

--- tests/e2e/integration/create-room.spec.js
+++ tests/e2e/integration/create-room.spec.js
@@ -4,3 +4,16 @@
 // If you're unfamiliar with how Cypress works,
 // check out the link below and learn how to write your first test:
 // https://on.cypress.io/writing-first-test
+
+describe("Creating Rooms", () => {
+	it("should create a temporary room", () => {
+		cy.visit(Cypress.config().baseUrl);
+		cy.contains("Create Room").should("be.visible").click();
+		cy.wait(500);
+		cy.location("pathname").should((path) => {
+			expect(path).to.include("room");
+		});
+		cy.get("h1").contains("Temporary Room").scrollIntoView().should("be.visible");
+		cy.get("#connectStatus").should("have.text", "Connected");
+	});
+});
