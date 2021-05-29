47c9b45cb87ee45cc3a362fe8dd4c969f8ab8046
Carson McManus <carson.mcmanus1@gmail.com>
2021-05-28 21:00:49 -0400

    add create permanent room test

--- tests/e2e/integration/create-room.spec.js
+++ tests/e2e/integration/create-room.spec.js
@@ -1,19 +1,30 @@
-// create-room.spec.js created with Cypress
-//
-// Start writing your Cypress tests below!
-// If you're unfamiliar with how Cypress works,
-// check out the link below and learn how to write your first test:
-// https://on.cypress.io/writing-first-test
-
 describe("Creating Rooms", () => {
 	it("should create a temporary room", () => {
 		cy.visit(Cypress.config().baseUrl);
 		cy.contains("Create Room").should("be.visible").click();
+		cy.get('[role="menu"]').contains('[role="menuitem"]', "Create Temporary Room").should("be.visible").click();
 		cy.wait(500);
 		cy.location("pathname").should((path) => {
 			expect(path).to.include("room");
 		});
-		cy.get("h1").contains("Temporary Room").scrollIntoView().should("be.visible");
+		cy.get("h1").contains("Temporary Room").scrollIntoView().should("be.visible").should("have.text", "Temporary Room");
+		cy.get("#connectStatus").should("have.text", "Connected");
+	});
+
+	it("should create a permanent room", () => {
+		cy.visit(Cypress.config().baseUrl);
+		cy.contains("Create Room").should("be.visible").click();
+		cy.get('[role="menu"]').contains('[role="menuitem"]', "Create Permanent Room").should("be.visible").click();
+
+		let roomName = Math.random().toString(36).substring(2);
+		cy.get('form').find("input").first().type(roomName);
+		cy.get('form').submit();
+
+		cy.wait(500);
+		cy.location("pathname").should((path) => {
+			expect(path).to.include("room");
+		});
+		cy.get("h1").contains(roomName).scrollIntoView().should("be.visible").should("have.text", roomName);
 		cy.get("#connectStatus").should("have.text", "Connected");
 	});
 });
