a38a249f204786d3905e9e037d0c6650b6fae948
Carson McManus <carson.mcmanus1@gmail.com>
2021-06-05 11:20:23 -0400

    add redisClientAsync with manual type annotations

--- redisclient.ts
+++ redisclient.ts
@@ -1,4 +1,5 @@
 import redis from 'redis';
+import { promisify } from 'util';
 
 const redisOptions: redis.ClientOpts = (process.env.REDIS_TLS_URL || process.env.REDIS_URL) ?
 	{
@@ -19,7 +20,18 @@ export function createSubscriber(): redis.RedisClient {
 	return redis.createClient(redisOptions);
 }
 
+// All of the other package solutions I've tried are broken af, so I'll just do this instead.
+// These are by no means complete type annotations, just minimal ones to make me happy.
+export const redisClientAsync = {
+	get: promisify(redisClient.get).bind(redisClient) as ((key: string) => Promise<string>),
+	set: promisify(redisClient.set).bind(redisClient) as ((key: string, value: string) => Promise<"OK">),
+	del: promisify(redisClient.del).bind(redisClient) as ((key: string) => Promise<number>),
+	exists: promisify(redisClient.exists).bind(redisClient) as ((key: string) => Promise<number>),
+	keys: promisify(redisClient.keys).bind(redisClient) as ((pattern: string) => Promise<string[]>),
+};
+
 module.exports = {
 	redisClient,
 	createSubscriber,
+	redisClientAsync,
 };
