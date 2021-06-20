9aca0c631e0cf748abd17348fb38f0e2175e7eed
Carson McManus <carson.mcmanus1@gmail.com>
2021-06-20 15:31:13 -0400

    improve the add fake user endpoint

--- server/api/dev.ts
+++ server/api/dev.ts
@@ -5,6 +5,7 @@ import roommanager from "../roommanager";
 import { RoomRequestType } from '../../common/models/messages';
 import usermanager from '../../usermanager.js';
 import faker from "faker";
+import tokens from "../auth/tokens";
 
 const router = express.Router();
 const log = getLogger("api/dev");
@@ -23,24 +24,41 @@ router.post("/reset-rate-limit/user", async (req, res) => {
 
 router.post("/room/:name/add-fake-user", async (req, res) => {
 	let user;
+	const token = await tokens.mint();
 	if (req.body.register) {
 		user = await usermanager.registerUser({
 			email: faker.internet.email(),
 			username: faker.internet.userName(),
 			password: faker.internet.password(12),
 		});
+		await tokens.setSessionInfo(token, { isLoggedIn: true, user_id: user.id });
+	}
+	else {
+		await tokens.setSessionInfo(token, { isLoggedIn: false, username: "fake_user" });
 	}
 
 	const room = await roommanager.GetRoom(req.params.name);
-	await room.processRequest({
-		type: RoomRequestType.JoinRequest,
-		client: "fake",
-		info: {
-			id: "fake",
-			user_id: user ? user.id : undefined,
-		},
-	});
-	res.json({ success: true });
+	try {
+		await room.processRequest({
+			type: RoomRequestType.JoinRequest,
+			client: "fake",
+			token: token,
+			info: {
+				id: "fake",
+				user_id: user ? user.id : undefined,
+			},
+		});
+		res.json({ success: true });
+	}
+	catch (e) {
+		res.json({
+			success: false,
+			error: {
+				name: e.name,
+				message: e.message,
+			},
+		});
+	}
 });
 
 export default router;
