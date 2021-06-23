086d8881dba3ebdad0790a9d5dc6d4fa8f7ec9b5
Carson McManus <carson.mcmanus1@gmail.com>
2021-06-22 21:28:52 -0400

    fix express type annotations

---
+++ express-types.d.ts
@@ -0,0 +1,32 @@
+import { User } from "./models/user";
+import { AuthToken } from "./common/models/types";
+import { SessionInfo } from './server/auth/tokens';
+import express from "express";
+
+declare module "express" {
+	export interface Request {
+		token?: AuthToken;
+		ottsession?: SessionInfo;
+		user?: User
+	}
+
+	export interface ParamsDictionary {
+		[key: string]: string;
+	}
+
+	export interface RequestHandler<
+		P = ParamsDictionary,
+		ResBody = any,
+		ReqBody = any,
+		ReqQuery = qs.ParsedQs,
+		Locals extends Record<string, any> = Record<string, any>
+	> {
+		(
+			req: Request<P, ResBody, ReqBody, ReqQuery, Locals>,
+			res: Response<ResBody, Locals>,
+			next: NextFunction,
+		): void;
+	}
+}
+
+export default express;
