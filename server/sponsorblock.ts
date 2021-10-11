abd3d7d6fbb8ff5c61cbee67a946de7274144ea8
Carson McManus <carson.mcmanus1@gmail.com>
2021-10-11 16:05:56 -0400

    import sponsorblock api package

---
+++ server/sponsorblock.ts
@@ -0,0 +1,20 @@
+import { SponsorBlock } from 'sponsorblock-api';
+import { redisClientAsync } from '../redisclient';
+import uuid from 'uuid';
+
+const SPONSORBLOCK_USERID_KEY = `sponsorblock-userid`;
+
+export async function getSponsorBlockUserId(): Promise<string> {
+	let userid = await redisClientAsync.get(SPONSORBLOCK_USERID_KEY);
+	if (!userid) {
+		userid = uuid.v4();
+		await redisClientAsync.set(SPONSORBLOCK_USERID_KEY, userid);
+	}
+	return userid;
+}
+
+export async function getSponsorBlock(): Promise<SponsorBlock> {
+	const userid = await getSponsorBlockUserId();
+	const sponsorblock = new SponsorBlock(userid);
+	return sponsorblock;
+}
