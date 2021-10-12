1a939952eafd173896a1e7ee7353251cb0a39be9
Carson McManus <carson.mcmanus1@gmail.com>
2021-10-11 21:59:26 -0400

    add autoSkipSegments to db model

---
+++ migrations/20211012010106-add-auto-skip-to-rooms.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('Rooms', 'autoSkipSegments', {
+      type: Sequelize.BOOLEAN,
+      defaultValue: true,
+      allowNull: false,
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('Rooms', 'autoSkipSegments');
+  },
+};
