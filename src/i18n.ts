c73cd5dce2988adb61edbac439c98dbe396dc607
Carson McManus <carson.mcmanus1@gmail.com>
2021-12-22 10:05:55 -0500

    rework i18n.ts

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,23 +1,43 @@
 import Vue from 'vue';
 import VueI18n, { LocaleMessages } from 'vue-i18n';
+import messages from "@/locales/en";
+import axios from 'axios';
 
 Vue.use(VueI18n);
 
-function loadLocaleMessages (): LocaleMessages {
-  const locales = require.context('./locales', true, /[A-Za-z0-9-_,\s]+\.json$/i);
-  const messages: LocaleMessages = {};
-  locales.keys().forEach(key => {
-    const matched = key.match(/([A-Za-z0-9-_]+)\./i);
-    if (matched && matched.length > 1) {
-      const locale = matched[1];
-      messages[locale] = locales(key);
-    }
-  });
-  return messages;
-}
-
-export default new VueI18n({
+export const i18n = new VueI18n({
   locale: process.env.VUE_APP_I18N_LOCALE || 'en',
   fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'en',
-  messages: loadLocaleMessages(),
+  messages,
 });
+
+const loadedLanguages = ['en']; // our default language that is preloaded
+
+function setI18nLanguage (lang) {
+  i18n.locale = lang;
+  axios.defaults.headers.common['Accept-Language'] = lang;
+  document.querySelector('html')?.setAttribute('lang', lang);
+  return lang;
+}
+
+export function loadLanguageAsync(lang: string) {
+  // If the same language
+  if (i18n.locale === lang) {
+    return Promise.resolve(setI18nLanguage(lang));
+  }
+
+  // If the language was already loaded
+  if (loadedLanguages.includes(lang)) {
+    return Promise.resolve(setI18nLanguage(lang));
+  }
+
+  // If the language hasn't been loaded yet
+  return import(/* webpackChunkName: "lang-[request]" */ `@/locales/${lang}.js`).then(
+    messages => {
+      i18n.setLocaleMessage(lang, messages.default);
+      loadedLanguages.push(lang);
+      return setI18nLanguage(lang);
+    }
+  );
+}
+
