6480a9852a66a39bb6ae6d03d3a46e5a6e9654eb
Carson McManus <carson.mcmanus1@gmail.com>
2022-11-04 12:38:11 -0400

    fix i18n setup i think

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,5 +3,6 @@ import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
 import { store } from "./store";
 import { router } from "./router";
+import { i18n } from "./i18n";
 
-createApp(App).use(store).use(router).use(vuetify).mount("#app");
+createApp(App).use(store).use(router).use(i18n).use(vuetify).mount("#app");
