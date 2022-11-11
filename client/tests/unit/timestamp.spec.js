294624b90512fe7ccb4d0416705266c24656666d
Carson McManus <carson.mcmanus1@gmail.com>
2022-11-11 14:20:14 -0500

    add vitest, but most unit tests are still broken

--- client/tests/unit/timestamp.spec.js
+++ client/tests/unit/timestamp.spec.js
@@ -1,3 +1,4 @@
+import { it, describe, expect } from "vitest";
 import dayjs from "dayjs";
 import {
 	secondsToTimestamp,
--- client/vite.config.js
+++ client/vite.config.js
@@ -26,4 +26,7 @@ export default defineConfig({
 			},
 		},
 	},
+	test: {
+		environment: "jsdom",
+	},
 });
