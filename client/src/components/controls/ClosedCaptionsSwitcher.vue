db6a72618af819437bbd1989e1f390afa104c842
Carson McManus <carson.mcmanus1@gmail.com>
2022-11-15 19:59:28 -0500

    ClosedCaptionsSwitcher: adjust styling and convert to vuetify 3

--- client/src/components/controls/ClosedCaptionsSwitcher.vue
+++ client/src/components/controls/ClosedCaptionsSwitcher.vue
@@ -1,45 +1,36 @@
 <template>
-	<v-menu top offset-y :disabled="!supported">
-		<template v-slot:activator="{ props }">
-			<v-btn v-bind="props" :disabled="!supported">
-				<v-icon>fa:fas fa-closed-captioning</v-icon>
-			</v-btn>
-		</template>
-		<v-list>
-			<v-list-item link @click="setCaptionsEnabled(true)" v-if="tracks.length === 0">
-				On
-			</v-list-item>
-			<v-list-item
-				link
-				@click="setCaptionsTrack(track)"
-				v-for="(track, idx) in tracks"
-				:key="idx"
-			>
-				{{ track }}
-			</v-list-item>
-			<v-list-item link @click="setCaptionsEnabled(false)"> Off </v-list-item>
-		</v-list>
-	</v-menu>
+	<v-btn variant="text" icon :disabled="!supported">
+		<v-icon>fa:fas fa-closed-captioning</v-icon>
+		<v-menu location="top" offset-y activator="parent" :disabled="!supported">
+			<v-list>
+				<v-list-item link @click="setCaptionsEnabled(true)" v-if="tracks.length === 0">
+					On
+				</v-list-item>
+				<v-list-item
+					link
+					@click="setCaptionsTrack(track)"
+					v-for="(track, idx) in tracks"
+					:key="idx"
+				>
+					{{ track }}
+				</v-list-item>
+				<v-list-item link @click="setCaptionsEnabled(false)"> Off </v-list-item>
+			</v-list>
+		</v-menu>
+	</v-btn>
 </template>
 
 <script lang="ts">
-import { defineComponent, toRefs } from "vue";
-
-interface ClosedCaptionsSwitcherProps {
-	supported: boolean;
-	tracks: string[];
-}
+import { defineComponent, PropType } from "vue";
 
 const ClosedCaptionsSwitcher = defineComponent({
 	name: "ClosedCaptionsSwitcher",
 	emits: ["enable-cc", "cc-track"],
 	props: {
 		supported: { type: Boolean, default: true },
-		tracks: { type: Array, default: [] },
+		tracks: { type: Array as PropType<string[]>, default: [] },
 	},
-	setup(props: ClosedCaptionsSwitcherProps, { emit }) {
-		let { tracks } = toRefs(props);
-
+	setup(props, { emit }) {
 		function setCaptionsEnabled(value: boolean) {
 			emit("enable-cc", value);
 		}
