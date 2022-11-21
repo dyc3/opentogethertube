68073b6416487b45cea131435f891a073619029e
Carson McManus <carson.mcmanus1@gmail.com>
2022-11-21 08:16:08 -0500

    remove QuickRoom

--- client/src/router.ts
+++ client/src/router.ts
@@ -17,11 +17,6 @@ export const routes: RouteRecordRaw[] = [
 		component: () => import("./views/Room.vue"),
 	},
 	{
-		path: "/quickroom",
-		name: "quickroom",
-		component: () => import("./views/QuickRoom.vue"),
-	},
-	{
 		path: "/faq",
 		name: "faq",
 		component: () => import("./views/Faq.vue"),
