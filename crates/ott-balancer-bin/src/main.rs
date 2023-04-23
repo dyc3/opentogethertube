d0e0992645459584250a32fdf0ef10024e5da829
Carson McManus <carson.mcmanus1@gmail.com>
2023-04-23 08:25:44 -0400

    set up project for load balancer

---
+++ crates/ott-balancer-bin/src/main.rs
@@ -0,0 +1,17 @@
+#[macro_use]
+extern crate rocket;
+use rocket_ws as ws;
+
+#[get("/")]
+fn index(ws: ws::WebSocket) -> ws::Stream!['static] {
+    ws::Stream! { ws =>
+        for await message in ws {
+            yield message?;
+        }
+    }
+}
+
+#[launch]
+fn launch() -> _ {
+    rocket::build().mount("/", routes![index])
+}
