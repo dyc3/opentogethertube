8c0668b741e9fb98f2c2914befce0e3a7ed35b44
Carson McManus <carson.mcmanus1@gmail.com>
2023-06-10 16:11:51 -0400

    add a parser for m3u8 files

--- server/services/direct.ts
+++ server/services/direct.ts
@@ -10,6 +10,8 @@ import { getMimeType, isSupportedMimeType } from "../mime";
 import ffprobe from "../ffprobe";
 import { getLogger } from "../logger";
 import { Video } from "../../common/models/video";
+import { Parser as M3u8Parser } from "m3u8-parser";
+import axios from "axios";
 
 const log = getLogger("direct");
 
@@ -67,6 +69,9 @@ export default class DirectVideoAdapter extends ServiceAdapter {
 		if (!isSupportedMimeType(mime)) {
 			throw new UnsupportedMimeTypeException(mime);
 		}
+		if (mime === "application/x-mpegURL") {
+			return await this.handleM3u8(url);
+		}
 		const fileInfo = await ffprobe.getFileInfo(link);
 		const duration = Math.ceil(this.getDuration(fileInfo));
 		const title = fileInfo.format?.tags?.title ?? fileName;
@@ -81,6 +86,63 @@ export default class DirectVideoAdapter extends ServiceAdapter {
 
 		return video;
 	}
+
+	/**
+	 * @returns The link to the lowest bitrate video stream
+	 */
+	async handleM3u8(url: URL.UrlWithStringQuery): Promise<Video> {
+		const parser = new M3u8Parser();
+		const resp = await axios.get(url.href);
+
+		// const entries: any[] = [];
+		// parser.addParser({
+		// 	expression: /^#EXT-X-I-FRAME-STREAM-INF/,
+		// 	customType: 'iframes',
+		// 	dataParser: (line: string) => {
+		// 		const entry = {};
+		// 		const attributes = line.replace('#EXT-X-I-FRAME-STREAM-INF:', '').split(',');
+		// 		for (let attribute of attributes) {
+		// 			const [key, value] = attribute.split('=');
+		// 			entry[key] = value;
+		// 		}
+		// 		entries.push(entry);
+		// 		return entries;
+		// 	}
+		//   });
+		parser.push(resp.data);
+		parser.end();
+		const manifest = parser.manifest;
+		log.silly(`Got m3u8 manifest with ${JSON.stringify(manifest)}`);
+
+		const lowestBitratePlaylist = manifest.playlists.reduce((acc, cur) => {
+			if (cur.attributes.BANDWIDTH < acc.attributes.BANDWIDTH) {
+				return cur;
+			} else {
+				return acc;
+			}
+		}, { attributes: { BANDWIDTH: Infinity }});
+		const newPath = url.pathname?.split("/").slice(0, -1).join("/") + "/" + lowestBitratePlaylist.uri;
+		log.silly(`new playlist path ${newPath}`);
+		const respStreams = await axios.get("https://" + url.hostname + newPath);
+		const parser2 = new M3u8Parser();
+		parser2.push(respStreams.data);
+		parser2.end();
+		const manifest2 = parser2.manifest;
+		log.silly(`Got m3u8 manifest with ${JSON.stringify(manifest2)}`);
+
+
+
+		return {
+			service: this.serviceId,
+			id: url.href,
+			title: manifest2.attributes?.NAME ?? url.href,
+			description: `Full Link: ${url.href}`,
+			mime: "application/x-mpegURL",
+			length: manifest2.segments.reduce((acc, cur) => acc + cur.duration, 0),
+			hls_url: url.href,
+		};
+	}
 }
+
 
 module.exports = DirectVideoAdapter;
