540c8ed5238777974aa031983e1ad34e91aeb99a
Carson McManus <carson.mcmanus1@gmail.com>
2023-06-16 09:12:29 -0400

    update migrations to work with postgres

--- server/migrations/20230615122836-to-json-columns.js
+++ server/migrations/20230615122836-to-json-columns.js
@@ -2,19 +2,51 @@
 
 const { DataTypes } = require("sequelize");
 
+const COLUMNS = ["permissions", "role-admin", "role-mod", "role-trusted"];
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
 	async up(queryInterface, Sequelize) {
-		await queryInterface.changeColumn("rooms", "permissions", DataTypes.JSONB);
-		await queryInterface.changeColumn("rooms", "role-admin", DataTypes.JSONB);
-		await queryInterface.changeColumn("rooms", "role-mod", DataTypes.JSONB);
-		await queryInterface.changeColumn("rooms", "role-trusted", DataTypes.JSONB);
+		const dialect = queryInterface.sequelize.getDialect();
+		for (const column of COLUMNS) {
+			if (dialect === "sqlite") {
+				await queryInterface.changeColumn("Rooms", column, DataTypes.JSONB);
+			} else if (dialect === "postgres") {
+				// await queryInterface.addColumn("Rooms", `${column}-new`, DataTypes.JSONB);
+				// await queryInterface.sequelize.query(
+				// 	`UPDATE Rooms SET \`${column}-new\` = to_jsonb(\`${column}\`);`
+				// );
+				// await queryInterface.removeColumn("Rooms", column);
+				// await queryInterface.renameColumn("Rooms", `${column}-new`, column);
+
+				await queryInterface.sequelize.query(
+					`ALTER TABLE "Rooms" ALTER "${column}" TYPE JSONB USING "${column}"::JSONB`
+				);
+			} else {
+				throw new Error(`Unsupported dialect: ${dialect}`);
+			}
+		}
 	},
 
 	async down(queryInterface, Sequelize) {
-		await queryInterface.changeColumn("rooms", "permissions", DataTypes.TEXT);
-		await queryInterface.changeColumn("rooms", "role-admin", DataTypes.TEXT);
-		await queryInterface.changeColumn("rooms", "role-mod", DataTypes.TEXT);
-		await queryInterface.changeColumn("rooms", "role-trusted", DataTypes.TEXT);
+		const dialect = queryInterface.sequelize.getDialect();
+		for (const column of COLUMNS) {
+			if (dialect === "sqlite") {
+				await queryInterface.changeColumn("Rooms", column, DataTypes.TEXT);
+			} else if (dialect === "postgres") {
+				// await queryInterface.addColumn("Rooms", `${column}-new`, DataTypes.TEXT);
+				// await queryInterface.sequelize.query(
+				// 	`UPDATE Rooms SET \`${column}-new\` = \`${column}\`::TEXT;`
+				// );
+				// await queryInterface.removeColumn("Rooms", column);
+				// await queryInterface.renameColumn("Rooms", `${column}-new`, column);
+
+				await queryInterface.sequelize.query(
+					`ALTER TABLE "Rooms" ALTER "${column}" TYPE TEXT USING "${column}"::TEXT`
+				);
+			} else {
+				throw new Error(`Unsupported dialect: ${dialect}`);
+			}
+		}
 	},
 };
--- server/migrations/20230615125602-null-owner-id.js
+++ server/migrations/20230615125602-null-owner-id.js
@@ -3,18 +3,20 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
 	async up(queryInterface, Sequelize) {
-		await queryInterface.changeColumn("rooms", "ownerId", {
+		await queryInterface.changeColumn("Rooms", "ownerId", {
 			type: Sequelize.INTEGER,
 			allowNull: true,
 		});
-		await queryInterface.sequelize.query("UPDATE rooms SET ownerId = NULL WHERE ownerId = -1;");
+		await queryInterface.sequelize.query(
+			`UPDATE "Rooms" SET "ownerId" = NULL WHERE "ownerId" = -1;`
+		);
 	},
 
 	async down(queryInterface, Sequelize) {
 		await queryInterface.sequelize.query(
-			"UPDATE rooms SET ownerId = -1 WHERE ownerId IS NULL;"
+			`UPDATE "Rooms" SET "ownerId" = -1 WHERE "ownerId" IS NULL;`
 		);
-		await queryInterface.changeColumn("rooms", "ownerId", {
+		await queryInterface.changeColumn("Rooms", "ownerId", {
 			type: Sequelize.INTEGER,
 			defaultValue: -1,
 			allowNull: false,
