3bf68472ca5ded966d9fe147540aee3b8c6d3136
Carson McManus <carson.mcmanus1@gmail.com>
2023-06-28 13:03:23 -0400

    refactor cached video storage to be a lot cleaner

--- server/storage/cachedvideo.ts
+++ server/storage/cachedvideo.ts
@@ -1,10 +1,10 @@
 import dayjs from "dayjs";
-import { CachedVideo } from "../models";
+import { CachedVideo as DbCachedVideo } from "../models";
 import Sequelize from "sequelize";
 import type { Video, VideoMetadata } from "../../common/models/video";
 import { getLogger } from "../logger";
 import _ from "lodash";
-import { CachedVideoCreationAttributes } from "server/models/cachedvideo";
+import { CachedVideoCreationAttributes, CachedVideo } from "../models/cachedvideo";
 
 const log = getLogger("storage/cachedvideo");
 
@@ -16,53 +16,20 @@ const log = getLogger("storage/cachedvideo");
  * @return Video object, but it may contain missing properties.
  */
 export async function getVideoInfo(service: string, id: string): Promise<Video> {
-	return CachedVideo.findOne({ where: { service: service, serviceId: id } })
-		.then(cachedVideo => {
-			if (cachedVideo === null) {
-				log.info(`Cache missed: ${service} ${id}`);
-				return { service, id };
-			}
-			const origCreatedAt = dayjs(cachedVideo.createdAt);
-			const lastUpdatedAt = dayjs(cachedVideo.updatedAt);
-			const today = dayjs();
-			// We check for changes every at an interval of 30 days, unless the original cache date was
-			// less than 7 days ago, then the interval is 7 days. The reason for this is that the uploader
-			// is unlikely to change the video info after a week of the original upload. Since we don't store
-			// the upload date, we pretend the original cache date is the upload date. This is potentially an
-			// over optimization.
-			const isCachedInfoValid =
-				lastUpdatedAt.diff(today, "days") <=
-				(origCreatedAt.diff(today, "days") <= 7 ? 7 : 30);
-			const video: Video = {
-				service: cachedVideo.service,
-				id: cachedVideo.serviceId,
-			};
-			// We only invalidate the title and description because those are the only ones that can change.
-			if (cachedVideo.title !== null && isCachedInfoValid) {
-				video.title = cachedVideo.title;
-			}
-			if (cachedVideo.description !== null && isCachedInfoValid) {
-				video.description = cachedVideo.description;
-			}
-			if (
-				cachedVideo.thumbnail !== null &&
-				((video.service !== "googledrive" && isCachedInfoValid) ||
-					(video.service === "googledrive" && lastUpdatedAt.diff(today, "hour") <= 12))
-			) {
-				video.thumbnail = cachedVideo.thumbnail;
-			}
-			if (cachedVideo.length !== null && isCachedInfoValid) {
-				video.length = cachedVideo.length;
-			}
-			if (cachedVideo.mime !== null) {
-				video.mime = cachedVideo.mime;
-			}
-			return video;
-		})
-		.catch(err => {
-			log.warn(`Cache failure ${err}`);
-			return { service, id };
+	try {
+		const cachedVideo = await DbCachedVideo.findOne({
+			where: { service: service, serviceId: id },
 		});
+		if (cachedVideo) {
+			return filterVideoInfo(cachedVideo);
+		} else {
+			log.debug(`video not found in cache ${service} ${id}`);
+			return { service, id };
+		}
+	} catch (err) {
+		log.warn(`Cache failure ${err}`);
+		return { service, id };
+	}
 }
 /**
  * Gets cached video information from the database. If cached information
@@ -71,7 +38,7 @@ export async function getVideoInfo(service: string, id: string): Promise<Video> 
  * @param	{Array.<Video|Object>} videos The videos to find in the cache.
  * @return	{Promise.<Object>} Video object, but it may contain missing properties.
  */
-export function getManyVideoInfo(videos) {
+export async function getManyVideoInfo(videos) {
 	const { or, and } = Sequelize.Op;
 
 	videos = videos.map(video => {
@@ -81,64 +48,63 @@ export function getManyVideoInfo(videos) {
 		return video;
 	});
 
-	return CachedVideo.findAll({
-		where: {
-			[or]: videos.map(video => {
-				return {
-					[and]: [{ service: video.service }, { serviceId: video.serviceId }],
-				};
-			}),
-		},
-	})
-		.then(foundVideos => {
-			if (videos.length !== foundVideos.length) {
-				for (let video of videos) {
-					if (!_.find(foundVideos, video)) {
-						foundVideos.push(video);
-					}
+	try {
+		const foundVideos = await DbCachedVideo.findAll({
+			where: {
+				[or]: videos.map(video => {
+					return {
+						[and]: [{ service: video.service }, { serviceId: video.serviceId }],
+					};
+				}),
+			},
+		});
+		if (videos.length !== foundVideos.length) {
+			for (let video of videos) {
+				if (!_.find(foundVideos, video)) {
+					foundVideos.push(video);
 				}
 			}
-			return foundVideos.map(cachedVideo => {
-				const origCreatedAt = dayjs(cachedVideo.createdAt);
-				const lastUpdatedAt = dayjs(cachedVideo.updatedAt);
-				const today = dayjs();
-				// We check for changes every at an interval of 30 days, unless the original cache date was
-				// less than 7 days ago, then the interval is 7 days. The reason for this is that the uploader
-				// is unlikely to change the video info after a week of the original upload. Since we don't store
-				// the upload date, we pretend the original cache date is the upload date. This is potentially an
-				// over optimization.
-				const isCachedInfoValid =
-					lastUpdatedAt.diff(today, "days") <=
-					(origCreatedAt.diff(today, "days") <= 7 ? 7 : 30);
-				let video: Video = {
-					service: cachedVideo.service,
-					id: cachedVideo.serviceId,
-				};
-				// We only invalidate the title and description because those are the only ones that can change.
-				if (cachedVideo.title && isCachedInfoValid) {
-					video.title = cachedVideo.title;
-				}
-				if (cachedVideo.description && isCachedInfoValid) {
-					video.description = cachedVideo.description;
-				}
-				if (cachedVideo.thumbnail && isCachedInfoValid) {
-					video.thumbnail = cachedVideo.thumbnail;
-				}
-				if (cachedVideo.length && isCachedInfoValid) {
-					video.length = cachedVideo.length;
-				}
-				if (cachedVideo.mime) {
-					video.mime = cachedVideo.mime;
-				}
-				return video;
-			});
-		})
-		.catch(err => {
-			log.warn(`Cache failure ${err}`);
-			return videos;
-		});
+		}
+		return foundVideos.map(filterVideoInfo);
+	} catch (err) {
+		log.warn(`Cache failure ${err}`);
+		return videos;
+	}
 }
 
+function filterVideoInfo(cachedVideo: CachedVideo): Video {
+	const origCreatedAt = dayjs(cachedVideo.createdAt);
+	const lastUpdatedAt = dayjs(cachedVideo.updatedAt);
+	const today = dayjs();
+	// We check for changes every at an interval of 30 days, unless the original cache date was
+	// less than 7 days ago, then the interval is 7 days. The reason for this is that the uploader
+	// is unlikely to change the video info after a week of the original upload. Since we don't store
+	// the upload date, we pretend the original cache date is the upload date. This is potentially an
+	// over optimization.
+	const isCachedInfoValid =
+		lastUpdatedAt.diff(today, "days") <= (origCreatedAt.diff(today, "days") <= 7 ? 7 : 30);
+	const video: Video = {
+		service: cachedVideo.service,
+		id: cachedVideo.serviceId,
+	};
+	// We only invalidate the title and description because those are the only ones that can change.
+	if (cachedVideo.title && isCachedInfoValid) {
+		video.title = cachedVideo.title;
+	}
+	if (cachedVideo.description && isCachedInfoValid) {
+		video.description = cachedVideo.description;
+	}
+	if (cachedVideo.thumbnail && isCachedInfoValid) {
+		video.thumbnail = cachedVideo.thumbnail;
+	}
+	if (cachedVideo.length && isCachedInfoValid) {
+		video.length = cachedVideo.length;
+	}
+	if (cachedVideo.mime) {
+		video.mime = cachedVideo.mime;
+	}
+	return video;
+}
 /**
  * Updates the database with the given video. If the video exists in
  * the database, it is overwritten. Omitted properties will not be
@@ -157,7 +123,7 @@ export async function updateVideoInfo(video: Video, shouldLog = true): Promise<b
 		if (shouldLog) {
 			log.debug(`Updating video cache: ${video.service} ${video.id}`);
 		}
-		await CachedVideo.upsert(toDbVideo(video), {
+		await DbCachedVideo.upsert(toDbVideo(video), {
 			conflictWhere: { service: video.service, serviceId: video.id },
 			conflictFields: ["service", "serviceId"],
 		});
@@ -183,15 +149,17 @@ export async function updateManyVideoInfo(videos: Video[]): Promise<boolean> {
 
 	videos = videos.map(video => _.cloneDeep(video));
 
-	return CachedVideo.findAll({
-		where: {
-			[or]: videos.map(video => {
-				return {
-					[and]: [{ service: video.service }, { serviceId: video.id }],
-				};
-			}),
-		},
-	}).then(async foundVideos => {
+	try {
+		const foundVideos = await DbCachedVideo.findAll({
+			where: {
+				[or]: videos.map(video => {
+					return {
+						[and]: [{ service: video.service }, { serviceId: video.id }],
+					};
+				}),
+			},
+		});
+
 		let [toUpdate, toCreate] = _.partition(videos, video =>
 			_.find(foundVideos, { service: video.service, serviceId: video.id })
 		);
@@ -200,15 +168,15 @@ export async function updateManyVideoInfo(videos: Video[]): Promise<boolean> {
 		);
 		let promises: Promise<unknown>[] = toUpdate.map(video => updateVideoInfo(video, false));
 		if (toCreate.length) {
-			promises.push(CachedVideo.bulkCreate(toCreate.map(toDbVideo)));
+			promises.push(DbCachedVideo.bulkCreate(toCreate.map(toDbVideo)));
 		}
-		return Promise.all(promises).then(() => {
-			log.info(
-				`bulk cache: created ${toCreate.length} rows, updated ${toUpdate.length} rows`
-			);
-			return true;
-		});
-	});
+		await Promise.all(promises);
+		log.info(`bulk cache: created ${toCreate.length} rows, updated ${toUpdate.length} rows`);
+		return true;
+	} catch (err) {
+		log.error(`Failed to cache video info ${err}`);
+		return false;
+	}
 }
 
 function toDbVideo(video: Video): CachedVideoCreationAttributes {
@@ -225,7 +193,7 @@ function toDbVideo(video: Video): CachedVideoCreationAttributes {
 
 export function getVideoInfoFields(service?: string): (keyof VideoMetadata)[] {
 	let fields: (keyof VideoMetadata)[] = [];
-	for (let column in CachedVideo.rawAttributes) {
+	for (let column in DbCachedVideo.rawAttributes) {
 		if (
 			column === "id" ||
 			column === "createdAt" ||
