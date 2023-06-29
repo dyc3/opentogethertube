fc4949fc95ab3ac2eae3fae8ed23ae5fa475df0b
Carson McManus <dyc3@users.noreply.github.com>
2023-06-28 20:47:00 -0400

    hotfix for migration (#950)

--- server/migrations/20230628203138-add-prev-queue-to-room.js
+++ server/migrations/20230628203138-add-prev-queue-to-room.js
@@ -3,13 +3,13 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
 	async up(queryInterface, Sequelize) {
-		await queryInterface.addColumn("rooms", "prevQueue", {
+		await queryInterface.addColumn("Rooms", "prevQueue", {
 			type: Sequelize.JSONB,
 			allowNull: true,
 		});
 	},
 
 	async down(queryInterface, Sequelize) {
-		await queryInterface.removeColumn("rooms", "prevQueue");
+		await queryInterface.removeColumn("Rooms", "prevQueue");
 	},
 };
