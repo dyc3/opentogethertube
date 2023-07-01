5a0f7a0e3bcad324ccfde9d738ff43a6f54e4735
Carson McManus <carson.mcmanus1@gmail.com>
2023-07-01 17:24:16 -0400

    mostly fix #963

--- server/storage.metrics.ts
+++ server/storage.metrics.ts
@@ -1,4 +1,4 @@
-import { Sequelize } from "sequelize-typescript";
+import { Sequelize } from "sequelize";
 import { QueryTypes } from "sequelize";
 import { Counter, Gauge } from "prom-client";
 import { getLogger } from "./logger.js";
