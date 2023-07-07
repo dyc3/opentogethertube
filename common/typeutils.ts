d408134ee05fce802d395ce1d0bef2e3acecff16
Carson McManus <carson.mcmanus1@gmail.com>
2023-07-07 13:14:53 -0400

    fix lints

--- common/typeutils.ts
+++ common/typeutils.ts
@@ -7,10 +7,10 @@ import { NonNever } from "ts-essentials";
  * extends the type of the condition.
  */
 type NeverIfMatch<T, Cond> = {
-	[P in keyof T]: T[P] extends Cond ? never : P
+	[P in keyof T]: T[P] extends Cond ? never : P;
 };
 type NeverIfNotMatch<T, Cond> = {
-	[P in keyof T]: T[P] extends Cond ? P : never
+	[P in keyof T]: T[P] extends Cond ? P : never;
 };
 
 /**
@@ -25,16 +25,30 @@ export type OmitTypes<T, Cond> = Pick<T, NeverIfMatch<T, Cond>[keyof T]>;
 export type PickTypes<T, Cond> = Pick<T, NeverIfNotMatch<T, Cond>[keyof T]>;
 
 export type NeverIfArg1NotMatch<T, Cond> = {
-	[P in keyof T]: T[P] extends ((arg: any) => Promise<void>) ? (Parameters<T[P]>[0] extends Cond ? P : never) : never
-}
+	[P in keyof T]: T[P] extends (arg: any) => Promise<void>
+		? Parameters<T[P]>[0] extends Cond
+			? P
+			: never
+		: never;
+};
 
 export type NeverIfArg2NotMatch<T, Arg1, Arg2> = {
-	[P in keyof T]: T[P] extends ((arg: any, arg2: any) => Promise<void>) ? (Parameters<T[P]>[0] extends Arg1 ? P : never) & (Parameters<T[P]>[1] extends Arg2 ? P : never) : never
-}
+	[P in keyof T]: T[P] extends (arg: any, arg2: any) => Promise<void>
+		? (Parameters<T[P]>[0] extends Arg1 ? P : never) &
+				(Parameters<T[P]>[1] extends Arg2 ? P : never)
+		: never;
+};
 
 export type HasNoArgs<T> = {
-	[P in keyof T]: T[P] extends ((arg: never) => Promise<void>) ? (Parameters<T[P]>[0] extends number ? P : never) : never
-}
+	[P in keyof T]: T[P] extends (arg: never) => Promise<void>
+		? Parameters<T[P]>[0] extends number
+			? P
+			: never
+		: never;
+};
 
 // export type PickFunctions<T, Arg1> = Omit<Pick<T, NeverIfArg1NotMatch<T, Arg1>[keyof T]>, keyof NonNever<HasNoArgs<T>>>
-export type PickFunctions<T, Arg1, Arg2> = Omit<Pick<T, NeverIfArg2NotMatch<T, Arg1, Arg2>[keyof T]>, keyof NonNever<HasNoArgs<T>>>
+export type PickFunctions<T, Arg1, Arg2> = Omit<
+	Pick<T, NeverIfArg2NotMatch<T, Arg1, Arg2>[keyof T]>,
+	keyof NonNever<HasNoArgs<T>>
+>;
