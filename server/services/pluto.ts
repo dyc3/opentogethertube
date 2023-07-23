de03fcc72d882788a78ce2bde2b9aa9580bddac1
Carson McManus <carson.mcmanus1@gmail.com>
2023-07-23 16:18:19 -0400

    fix more parsing stuff

--- server/services/pluto.ts
+++ server/services/pluto.ts
@@ -237,7 +237,7 @@ export default class PlutoAdapter extends ServiceAdapter {
 			}
 		}
 
-		return [];
+		return videos;
 	}
 
 	parseBootResponseIntoSlugs(resp: PlutoBootResponse): PlutoParsedSlugs {
