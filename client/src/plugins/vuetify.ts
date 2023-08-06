a7d4d925187c5fecbd4d8da507bcf7b489f8d465
Carson McManus <carson.mcmanus1@gmail.com>
2023-08-06 08:59:09 -0400

    adjust theming

--- client/src/plugins/vuetify.ts
+++ client/src/plugins/vuetify.ts
@@ -10,8 +10,10 @@ import * as directives from "vuetify/directives";
 const themeDark: ThemeDefinition = {
 	dark: true,
 	colors: {
-		primary: "#ffb300", // orange
-		secondary: "#42A5F5", // blue
+		"primary": "#ffb300", // orange
+		"primary-lighten-1": "#FFC233",
+		"primary-darken-1": "#F5AB00",
+		"secondary": "#42A5F5", // blue
 	},
 };
 
@@ -19,26 +21,31 @@ const themeLight: ThemeDefinition = {
 	dark: false,
 	colors: {
 		"media-control-surface": "#ffffff",
+		"primary-lighten-1": "#7C1FFF",
 	},
 };
 
 const themeDeepRed: ThemeDefinition = {
 	dark: true,
 	colors: {
-		primary: "#D00000",
-		secondary: "#FFBA08",
-		background: "#280411",
-		surface: "#370617",
+		"primary": "#D00000",
+		"primary-lighten-1": "#f50000",
+		"primary-darken-1": "#b80000",
+		"secondary": "#FFBA08",
+		"background": "#280411",
+		"surface": "#370617",
 	},
 };
 
 const themeDeepBlue: ThemeDefinition = {
 	dark: true,
 	colors: {
-		primary: "#a8eff0",
-		secondary: "#42A5F5",
-		background: "#001021",
-		surface: "#001a37",
+		"primary": "#a8eff0",
+		"primary-lighten-1": "#b9f2f3",
+		"primary-darken-1": "#8539ea",
+		"secondary": "#42A5F5",
+		"background": "#001021",
+		"surface": "#001a37",
 	},
 };
 
