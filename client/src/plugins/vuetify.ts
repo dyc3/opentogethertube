2d100f4c74d561755a2ff687af1f27449735c1cc
Carson McManus <dyc3@users.noreply.github.com>
2023-08-07 18:35:47 -0400

    add greenslate theme (#1052)

--- client/src/plugins/vuetify.ts
+++ client/src/plugins/vuetify.ts
@@ -55,6 +55,19 @@ const themeDeepBlue: ThemeDefinition = {
 	},
 };
 
+const themeGreenSlate: ThemeDefinition = {
+	dark: true,
+	colors: {
+		primary: "#48e87d",
+		secondary: "#d9bc6a",
+		background: "#1f1f1f",
+		surface: "#303030",
+		success: "#48e87d",
+		warning: "#efeb68",
+		error: "#f66f64",
+	},
+};
+
 const vuetify = createVuetify({
 	components,
 	directives,
@@ -72,6 +85,7 @@ const vuetify = createVuetify({
 			light: themeLight,
 			deepred: themeDeepRed,
 			deepblue: themeDeepBlue,
+			greenslate: themeGreenSlate,
 		},
 	},
 });
