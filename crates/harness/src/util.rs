c3432fd14b7d6db88286468d818d80e9e42dc98f
Carson McManus <carson.mcmanus1@gmail.com>
2023-09-27 10:33:26 -0400

    impl random_unused_port

--- crates/harness/src/util.rs
+++ crates/harness/src/util.rs
@@ -1,3 +1,18 @@
+use rand::Rng;
+
 pub(crate) fn random_unused_port() -> u16 {
-    10000
+    loop {
+        let port = rand::thread_rng().gen_range(1024..65535);
+
+        if port_is_available(port) {
+            return port;
+        }
+    }
+}
+
+pub(crate) fn port_is_available(port: u16) -> bool {
+    match std::net::TcpListener::bind(("localhost", port)) {
+        Ok(_) => true,
+        Err(_) => false,
+    }
 }
