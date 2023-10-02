f467718a8b53de99b220fb584ba2804a426639ab
Carson McManus <dyc3@users.noreply.github.com>
2023-10-02 10:20:55 -0400

    balancer: fix monolith discovery not removing monoliths when told to (#1087)
    * make it so that `show` and `hide` block until the balancer has connected/disconnected
    
    * balancer: fix monolith discovery not removing monoliths when told to

--- crates/harness-tests/src/main.rs
+++ crates/harness-tests/src/main.rs
@@ -1,17 +1,29 @@
-use std::time::Duration;
-
 use harness::{Monolith, TestRunner};
 use test_context::test_context;
 
 #[test_context(TestRunner)]
 #[tokio::test]
 async fn sample_test(ctx: &mut TestRunner) {
-    let m = Monolith::new(ctx).await.unwrap();
+    let mut m = Monolith::new(ctx).await.unwrap();
     println!("monolith port: {}", m.port());
     assert_ne!(m.port(), 0);
     m.show().await;
+}
 
-    tokio::time::sleep(Duration::from_secs(30)).await;
+/// Add and remove a monolith a bunch of times to make sure that monolith discovery works.
+#[test_context(TestRunner)]
+#[tokio::test]
+async fn discovery_add_remove(ctx: &mut TestRunner) {
+    let mut m = Monolith::new(ctx).await.unwrap();
+    println!("monolith port: {}", m.port());
+    assert_ne!(m.port(), 0);
+
+    for _ in 0..10 {
+        m.show().await;
+        assert!(m.connected());
+        m.hide().await;
+        assert!(!m.connected());
+    }
 }
 
 fn main() {}
--- crates/harness/src/monolith.rs
+++ crates/harness/src/monolith.rs
@@ -5,7 +5,8 @@ use std::{
 
 use futures_util::{SinkExt, StreamExt};
 use ott_balancer_protocol::monolith::*;
-use tokio::net::TcpListener;
+use tokio::{net::TcpListener, sync::Notify};
+use tracing::warn;
 use tungstenite::Message;
 
 use crate::TestRunner;
@@ -20,6 +21,10 @@ pub struct Monolith {
 
     monolith_add_tx: tokio::sync::mpsc::Sender<SocketAddr>,
     monolith_remove_tx: tokio::sync::mpsc::Sender<SocketAddr>,
+
+    notif_connect: Arc<Notify>,
+    notif_disconnect: Arc<Notify>,
+    pub(crate) connected: bool,
 }
 
 impl Monolith {
@@ -28,11 +33,15 @@ impl Monolith {
         // for prototyping, using a fixed port.
         let listener =
             Arc::new(TcpListener::bind(SocketAddr::new(IpAddr::V6(Ipv6Addr::LOCALHOST), 0)).await?);
+        let notif_connect = Arc::new(Notify::new());
+        let notif_disconnect = Arc::new(Notify::new());
 
         let (outgoing_tx, mut outgoing_rx) = tokio::sync::mpsc::channel(50);
         let (incoming_tx, incoming_rx) = tokio::sync::mpsc::channel(50);
 
         let _listener = listener.clone();
+        let _notif_connect = notif_connect.clone();
+        let _notif_disconnect = notif_disconnect.clone();
         let task = tokio::task::Builder::new()
             .name("emulated monolith")
             .spawn(async move {
@@ -42,17 +51,26 @@ impl Monolith {
                     let init = M2BInit { port: addr.port() };
                     let msg = serde_json::to_string(&MsgM2B::Init(init)).unwrap();
                     ws.send(Message::Text(msg)).await.unwrap();
+                    _notif_connect.notify_one();
                     loop {
+                        println!("monolith: waiting for message");
                         tokio::select! {
                             Some(msg) = outgoing_rx.recv() => {
                                 ws.send(msg).await.unwrap();
                             }
                             Some(msg) = ws.next() => {
-                                incoming_tx.send(msg.unwrap()).await.unwrap();
+                                match msg {
+                                    Ok(msg) => incoming_tx.send(msg).await.unwrap(),
+                                    Err(e) => {
+                                        warn!("monolith: websocket error: {:?}", e);
+                                        break;
+                                    }
+                                }
                             }
                             else => break,
                         }
                     }
+                    _notif_disconnect.notify_one();
                 }
             })?;
 
@@ -64,6 +82,9 @@ impl Monolith {
             task,
             monolith_add_tx: ctx.monolith_add_tx.clone(),
             monolith_remove_tx: ctx.monolith_remove_tx.clone(),
+            notif_connect,
+            notif_disconnect,
+            connected: false,
         })
     }
 
@@ -71,20 +92,32 @@ impl Monolith {
         self.listener.local_addr().unwrap().port()
     }
 
+    pub fn connected(&self) -> bool {
+        self.connected
+    }
+
     /// Tell the provider to add this monolith to the list of available monoliths.
-    pub async fn show(&self) {
+    pub async fn show(&mut self) {
+        println!("showing monolith");
         self.monolith_add_tx
             .send(self.listener.local_addr().unwrap())
             .await
             .unwrap();
+        println!("waiting for notification");
+        self.notif_connect.notified().await;
+        self.connected = true;
     }
 
     /// Tell the provider to remove this monolith from the list of available monoliths.
-    pub async fn hide(&self) {
+    pub async fn hide(&mut self) {
+        println!("hiding monolith");
         self.monolith_remove_tx
             .send(self.listener.local_addr().unwrap())
             .await
             .unwrap();
+        println!("waiting for notification");
+        self.notif_disconnect.notified().await;
+        self.connected = false;
     }
 
     pub async fn recv(&mut self) {
