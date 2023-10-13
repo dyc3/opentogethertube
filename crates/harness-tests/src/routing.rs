54aab7370686cf62f77b1850515745463b25fec7
Carson McManus <carson.mcmanus1@gmail.com>
2023-10-13 08:26:43 -0400

    minor refactor

--- crates/harness-tests/src/routing.rs
+++ crates/harness-tests/src/routing.rs
@@ -19,7 +19,7 @@ async fn route_http_to_correct_monolith(ctx: &mut TestRunner) {
     );
 
     m.show().await;
-    m.load_room("foo".to_string()).await;
+    m.load_room("foo").await;
 
     // Without this sleep, this test can trigger a race condition where the client connects to the balancer before the monolith has the room loaded.
     // This will cause the other monolith to get the room loaded, and the client will connect to that monolith instead.
@@ -99,7 +99,7 @@ async fn route_ws_to_correct_monolith(ctx: &mut TestRunner) {
 
     let mut m = Monolith::new(ctx).await.unwrap();
     m.show().await;
-    m.load_room("foo".to_string()).await;
+    m.load_room("foo").await;
 
     // Without this sleep, this test can trigger a race condition where the client connects to the balancer before the monolith has the room loaded.
     // This will cause the other monolith to get the room loaded, and the client will connect to that monolith instead.
