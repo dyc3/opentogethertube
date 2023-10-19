4c6c44a2ea1bf5e8c0fdb2a8c0a7be676a9096f0
Christopher Roddy <76755988+cjrkoa@users.noreply.github.com>
2023-10-18 23:06:23 -0400

    prometheus metrics
    fixes #1094
    in progress

--- crates/ott-balancer-bin/src/service.rs
+++ crates/ott-balancer-bin/src/service.rs
@@ -15,6 +15,7 @@ use reqwest::Url;
 use route_recognizer::Router;
 use tokio::sync::RwLock;
 use tracing::{debug, error, info, warn};
+use prometheus::{Opts, Registry, Counter, TextEncoder, Encoder};
 
 use crate::balancer::{BalancerContext, BalancerLink};
 use crate::client::client_entry;
@@ -83,7 +84,26 @@ impl Service<Request<IncomingBody>> for BalancerService {
                     .join("\n");
                     mk_response(rendered)
                 }
-                "metrics" => mk_response("TODO: prometheus metrics".to_owned()),
+                "metrics" => {
+                    // Create a Counter.
+                    let counter_opts = Opts::new("test_counter", "test counter help");
+                    let counter = Counter::with_opts(counter_opts).unwrap();
+
+                    // Create a Registry and register Counter.
+                    let r = Registry::new();
+                    r.register(Box::new(counter.clone())).unwrap();
+
+                    // Inc.
+                    counter.inc();
+
+                    // Gather the metrics.
+                    let mut buffer = vec![];
+                    let encoder = TextEncoder::new();
+                    let metric_families = r.gather();
+                    encoder.encode(&metric_families, &mut buffer).unwrap();
+
+                    mk_response(String::from_utf8(buffer).unwrap().to_owned())
+                },
                 "room" => {
                     let Some(room_name) = route.params().find("room_name") else {
                         return Ok(not_found());
