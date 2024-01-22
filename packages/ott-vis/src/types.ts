8b76b0189c5b45ebf82bc76f292ef4308685f63f
Carson McManus <dyc3@users.noreply.github.com>
2024-01-22 13:46:03 -0500

    ci: add grafana plugin to workflows (#1250)
    * add grafana package to release jobs
    
    * add compatibility check job
    
    * make all packages get built instead of just the client
    
    * add grafana e2e test job
    
    * maybe fix grafana e2e job
    
    * fix typo
    
    * update scripts
    
    * format

--- packages/ott-vis/src/types.ts
+++ packages/ott-vis/src/types.ts
@@ -1,7 +1,7 @@
-type SeriesSize = 'sm' | 'md' | 'lg';
+type SeriesSize = "sm" | "md" | "lg";
 
 export interface SimpleOptions {
-  text: string;
-  showSeriesCount: boolean;
-  seriesCountSize: SeriesSize;
+	text: string;
+	showSeriesCount: boolean;
+	seriesCountSize: SeriesSize;
 }
