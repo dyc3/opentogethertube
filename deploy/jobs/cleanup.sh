b9ec299158fb8be1eba76771a3274d0a207eb2da
Carson McManus <carson.mcmanus1@gmail.com>
2024-01-26 09:17:52 -0500

    hack: fix connection

--- deploy/jobs/cleanup.sh
+++ deploy/jobs/cleanup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-psql --no-password --echo-queries -d "ott_prod" "$DATABASE_URL" -f "cleanup.sql"
+DB_NAME="ott_prod"
+# HACK: in order to connect to the right database, we need to replace the database name in the path of the DATABASE_URL, but NOT the username
+CONNECTION_URL=$(echo "$DATABASE_URL" | sed --regexp-extended "s/$FLY_APP_NAME(\?|$)/$DB_NAME/g")
+
+psql --no-password --echo-queries "$CONNECTION_URL" -f "cleanup.sql"
