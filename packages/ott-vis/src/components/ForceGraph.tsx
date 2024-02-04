482e15a68deae94fee4461a856ac403d6a78b80b
Christopher Roddy <cjrkoa@gmail.com>
2024-02-04 00:23:53 -0500

    bring over force graph code
    There were many many errors. All of them are gone but nothing renders right now in the grafana panel

--- packages/ott-vis/src/components/ForceGraph.tsx
+++ packages/ott-vis/src/components/ForceGraph.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import * as d3 from "d3";
 
 interface ForceGraphProps {
-  data: number[];
+  data: any;
   width?: number;
   height?: number;
   marginTop?: number;
@@ -17,20 +17,104 @@ const ForceGraph: React.FC<ForceGraphProps> = ({
   height = 400,
   marginTop = 20,
   marginRight = 20,
-  marginBottom = 20,
+  marginBottom = 50,
   marginLeft = 20
 }) => {
-  const x = d3.scaleLinear().domain([0, data.length - 1]).range([marginLeft, width - marginRight]);
-  const y = d3.scaleLinear().domain(d3.extent(data) as [number, number]).range([height - marginBottom, marginTop]);
-  const line = d3.line<number>()
-    .x((d, i) => x(i))
-    .y(d => y(d));
+  //const x = d3.scaleLinear().domain([0, data.length - 1]).range([marginLeft, width - marginRight]);
+  //const y = d3.scaleLinear().domain(d3.extent(data) as [number, number]).range([height - marginBottom, marginTop]);
+  //const line = d3.line<number>()
+  //  .x((d, i) => x(i))
+  //  .y(d => y(d));
+
+  // The force simulation mutates links and nodes, so create a copy
+  // so that re-evaluating this cell produces the same result.
+  const links = data.links.map((d: any) => ({...d}));
+  const nodes = data.nodes.map((d: any) => ({...d}));
+  
+  // Create a simulation with several forces.
+  const simulation = d3.forceSimulation(nodes)
+    .force("link", d3.forceLink(links).id((d: any) => d.id))
+      .force("charge", d3.forceManyBody())
+      .force("x", d3.forceX())
+      .force("y", d3.forceY());
+  
+  // Create the SVG container.
+  const svg = d3.create("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .attr("viewBox", [-width / 2, -height / 2, width, height])
+      .attr("style", "max-width: 100%; height: auto;");
+  
+  // Add a line for each link, and a circle for each node.
+  const link = svg.append("g")
+      .attr("stroke", "#999")
+      .attr("stroke-opacity", 0.6)
+    .selectAll("line")
+    .data(links)
+    .join("line")
+      .attr("stroke-width", (d: any) => Math.sqrt(d.value));
+  
+  const node = svg.append("g")
+      .attr("stroke", "#fff")
+      .attr("stroke-width", 1.5)
+    .selectAll("circle")
+    .data(nodes)
+    .join("circle")
+      .attr("r", (d: any) => radius(d.radius))
+      //.attr("fill", (d: any) => color(d.group));
+  
+  node.append("title")
+      .text((d: any) => d.id);
+  
+  // Add a drag behavior.
+  /*node.call(d3.drag()
+        .on("start", dragstarted)
+        .on("drag", dragged)
+        .on("end", dragended));*/
+    
+  // Set the position attributes of links and nodes each time the simulation ticks.
+  simulation.on("tick", () => {
+    link
+        .attr("x1", (d: any) => d.source.x)
+        .attr("y1", (d: any) => d.source.y)
+        .attr("x2", (d: any) => d.target.x)
+        .attr("y2", (d: any) => d.target.y);
+  
+    node
+        .attr("cx", (d: any) => d.x)
+        .attr("cy", (d: any) => d.y);
+  });
+  
+  // Reheat the simulation when drag starts, and fix the subject position.
+  function dragstarted(event: any) {
+    if (!event.active) simulation.alphaTarget(0.3).restart();
+    event.subject.fx = event.subject.x;
+    event.subject.fy = event.subject.y;
+  }
+  
+  // Update the subject (dragged node) position during drag.
+  function dragged(event: any) {
+    event.subject.fx = event.x;
+    event.subject.fy = event.y;
+  }
+  
+  // Restore the target alpha so the simulation cools after dragging ends.
+  // Unfix the subject position now that it’s no longer being dragged.
+  function dragended(event: any) {
+    if (!event.active) simulation.alphaTarget(0);
+    event.subject.fx = null;
+    event.subject.fy = null;
+  }
+  
+  function radius(num: number) {
+    return num * 2;
+  }
 
   return (
     <svg width={width} height={height}>
-      <path fill="none" stroke="currentColor" strokeWidth="1.5" d={line(data) || ''} />
+      <path fill="none" stroke="currentColor" strokeWidth="1.5" d={d3.line()(data) || ''} />
       <g fill="white" stroke="currentColor" strokeWidth="1.5">
-        {data.map((d, i) => (<circle key={i} cx={x(i)} cy={y(d)} r={2.5} />))}
+        {data.map(((d: any, i: any) => (<circle key={i} /*cx={d3.forceX(i).x} cy={d3.forceY(d).y}*/ r={2.5} />)))}
       </g>
     </svg>
   );
--- packages/ott-vis/src/components/SimplePanel.tsx
+++ packages/ott-vis/src/components/SimplePanel.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { PanelProps } from "@grafana/data";
 import { SimpleOptions } from "types";
 import { css, cx } from "@emotion/css";
-import { useStyles2, useTheme2 } from "@grafana/ui";
-import { ForceGraph } from "components/ForceGraph";
+import { useStyles2 } from "@grafana/ui";
+import ForceGraph from "components/ForceGraph";
 
 interface Props extends PanelProps<SimpleOptions> {}
 
@@ -28,7 +28,7 @@ const getStyles = () => {
 };
 
 export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) => {
-	const theme = useTheme2();
+	//const theme = useTheme2();
 	const styles = useStyles2(getStyles);
 	return (
 		<div
@@ -40,18 +40,190 @@ export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) =
 				`
 			)}
 		>
-			<svg
-				className={styles.svg}
-				width={width}
-				height={height}
-				xmlns="http://www.w3.org/2000/svg"
-				xmlnsXlink="http://www.w3.org/1999/xlink"
-				viewBox={`-${width / 2} -${height / 2} ${width} ${height}`}
-			>
-				<g>
-					<ForceGraph />
-				</g>
-			</svg>
+			<g>
+				<ForceGraph
+					height={height}
+					width={width}
+					data={
+						{
+							"nodes": [
+								{
+									"id": "B1",
+									"group": "Balancer",
+									"color": "Purple",
+									"radius": 15
+								},
+								{
+									"id": "M1",
+									"group": "Monolith",
+									"color": "Red",
+									"radius": 10
+								},
+								{
+									"id": "M2",
+									"group": "Monolith",
+									"color": "Red",
+									"radius": 10
+								},
+								{
+									"id": "R1",
+									"group": "Room",
+									"color": "Blue",
+									"radius": 7
+								},
+								{
+									"id": "R2",
+									"group": "Room",
+									"color": "Blue",
+									"radius": 7
+								},
+								{
+									"id": "R3",
+									"group": "Room",
+									"color": "Blue",
+									"radius": 7
+								},
+								{
+									"id": "R4",
+									"group": "Room",
+									"color": "Blue",
+									"radius": 7
+								},
+								{
+									"id": "C1",
+									"group": "Client",
+									"color": "Blue",
+									"radius": 4
+								},
+								{
+									"id": "C2",
+									"group": "Client",
+									"color": "Blue",
+									"radius": 4
+								},
+								{
+									"id": "C3",
+									"group": "Client",
+									"color": "Blue",
+									"radius": 4
+								},
+								{
+									"id": "C4",
+									"group": "Client",
+									"color": "Blue",
+									"radius": 4
+								},
+								{
+									"id": "C5",
+									"group": "Client",
+									"color": "Blue",
+									"radius": 4
+								},
+								{
+									"id": "C6",
+									"group": "Client",
+									"color": "Blue",
+									"radius": 4
+								},
+								{
+									"id": "C7",
+									"group": "Client",
+									"color": "Blue",
+									"radius": 4
+								},
+								{
+									"id": "C8",
+									"group": "Client",
+									"color": "Blue",
+									"radius": 4
+								},
+								{
+									"id": "C9",
+									"group": "Client",
+									"color": "Blue",
+									"radius": 4
+								}
+							],
+							"links": [
+								{
+									"source": "B1",
+									"target": "M1",
+									"value": "10"
+								},
+								{
+									"source": "B1",
+									"target": "M2",
+									"value": "10"
+								},
+								{
+									"source": "M1",
+									"target": "R1",
+									"value": "5"
+								},
+								{
+									"source": "M1",
+									"target": "R2",
+									"value": "5"
+								},
+								{
+									"source": "M2",
+									"target": "R3",
+									"value": "5"
+								},
+								{
+									"source": "M2",
+									"target": "R4",
+									"value": "5"
+								},
+								{
+									"source": "R1",
+									"target": "C1",
+									"value": "5"
+								},
+								{
+									"source": "R1",
+									"target": "C2",
+									"value": "3"
+								},
+								{
+									"source": "R2",
+									"target": "C3",
+									"value": "3"
+								},
+								{
+									"source": "R2",
+									"target": "C4",
+									"value": "3"
+								},
+								{
+									"source": "R3",
+									"target": "C5",
+									"value": "3"
+								},
+								{
+									"source": "R3",
+									"target": "C6",
+									"value": "3"
+								},
+								{
+									"source": "R4",
+									"target": "C7",
+									"value": "3"
+								},
+								{
+									"source": "R4",
+									"target": "C8",
+									"value": "3"
+								},
+								{
+									"source": "R4",
+									"target": "C9",
+									"value": "3"
+								}
+							]
+						}
+					} />
+			</g>
 
 			<div className={styles.textBox}>
 				{options.showSeriesCount && <div>Number of series: {data.series.length}</div>}
@@ -60,3 +232,4 @@ export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) =
 		</div>
 	);
 };
+
