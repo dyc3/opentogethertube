35bc20151d65ae7ca40d73af0ab315e43d7887dc
moreno-michael <patapon9099@gmail.com>
2024-02-08 12:11:32 -0500

    Adjusted format for Docker DNS

--- crates/ott-balancer/src/discovery/dns.rs
+++ crates/ott-balancer/src/discovery/dns.rs
@@ -8,12 +8,12 @@ use super::*;
 pub struct DockerDiscoveryConfig {
     /// The port that monoliths should be listening on for load balancer connections.
     pub monolith_port: u16,
-    pub fly_app: String,
+    pub : Option<String>,
 }
 
 pub struct DockerMonolithDiscoverer {
     config: DockerDiscoveryConfig,
-    query: String,
+    query: Option<String>,
 }
 
 impl DockerMonolithDiscoverer {
@@ -22,7 +22,7 @@ impl DockerMonolithDiscoverer {
             "Creating DockerMonolithDiscoverer, Docker DNS server: {}",
             &config.docker_dns_server
         );
-        let query = format!("global.{}.internal", &config.dns_server);
+        let query = format!("{}", &config.dns_server);
         Self { config, query }
     }
 }
@@ -32,9 +32,6 @@ impl MonolithDiscoverer for DockerMonolithDiscoverer {
     async fn discover(&mut self) -> anyhow::Result<Vec<MonolithConnectionConfig>> {
         let resolver =
             TokioAsyncResolver::tokio_from_system_conf().expect("failed to create resolver");
-
-        let docker_dns_server = format!("{.docker}")
-        let name = Name::from_str(&docker_service_name)?;
 
         let lookup = resolver.ipv4_lookup(&self.query).await?;
         let monoliths = lookup
