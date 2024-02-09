d42516905dce56e4d091fbf14e2f66651e4715c5
Carson McManus <dyc3@users.noreply.github.com>
2024-02-08 21:17:13 -0500

    restructure `ott-vis` to allow for multiple visualization types (#1304)
    * restructure `ott-vis` to allow for multiple visualization types
    
    * fix lints

--- packages/ott-vis/src/types.ts
+++ packages/ott-vis/src/types.ts
@@ -1,7 +1,30 @@
 type SeriesSize = "sm" | "md" | "lg";
 
-export interface SimpleOptions {
+export interface CoreOptions {
+	view: "global" | "region";
 	text: string;
 	showSeriesCount: boolean;
 	seriesCountSize: SeriesSize;
+}
+
+/**
+ * Describes the state of OTT
+ */
+export type SystemState = Balancer[];
+
+export interface Balancer {
+	id: string;
+	region: string;
+	monoliths: Monolith[];
+}
+
+export interface Monolith {
+	id: string;
+	region: string;
+	rooms: Room[];
+}
+
+export interface Room {
+	name: string;
+	clients: number;
 }
