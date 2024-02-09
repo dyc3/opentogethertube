840dd2d8633edf7d19994f9cca6f4e34ecabb542
Carson McManus <carson.mcmanus1@gmail.com>
2024-02-09 17:28:49 -0500

    add some toy k6 load tests

---
+++ tests/load/utils.js
@@ -0,0 +1,66 @@
+import http from "k6/http";
+import { sleep, check } from "k6";
+
+export const HOSTNAME = "localhost:8080";
+
+/**
+ *
+ * @returns {string}
+ */
+export function getAuthToken() {
+	let resp = http.get(`http://${HOSTNAME}/api/auth/grant`);
+	check(resp, { "token status is 200": r => r && r.status === 200 });
+	const token = JSON.parse(resp.body).token;
+	check(token, { "token is not empty": t => t && t.length > 0 });
+	return token;
+}
+
+/**
+ * Generate a random room name.
+ * @returns {string}
+ */
+export function randomRoomName() {
+	const prefix = "load-test-";
+	const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_-";
+	let name = prefix;
+	for (let i = 0; i < 10; i++) {
+		name += characters.charAt(Math.floor(Math.random() * characters.length));
+	}
+	return name;
+}
+
+/**
+ * Generate a list of random room names. Each name is guaranteed to be unique in the list.
+ * @param {number} count The number of room names to generate
+ * @returns {string[]}
+ */
+export function randomRoomNames(count) {
+	const names = [];
+	while (names.length < count) {
+		const name = randomRoomName();
+		if (!names.includes(name)) {
+			names.push(name);
+		}
+	}
+	return names;
+}
+
+export function createRoom(name, token, roomOptions = {}, options = { doCheck: true }) {
+	const body = Object.assign(
+		{
+			name: name,
+			visibility: "public",
+			isTemporary: false,
+		},
+		roomOptions
+	);
+	const url = `http://${HOSTNAME}/api/room/create`;
+	let resp = http.post(url, JSON.stringify(body), {
+		headers: {
+			Authorization: `Bearer ${token}`,
+		},
+	});
+	if (options.doCheck) {
+		check(resp, { "room created": r => r && r.status === 201 });
+	}
+}
