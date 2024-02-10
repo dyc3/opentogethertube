b060206da04c210ec54ba9a67e4f859787e0c296
Victor Giraldo <vgiraldo@stevens.edu>
2024-02-10 18:48:58 -0500

    only load config once

--- server/app.ts
+++ server/app.ts
@@ -55,7 +55,6 @@ export async function main() {
 	buildRateLimiter();
 
 	if (process.argv.includes("--validate")) {
-		loadConfigFile();
 		try {
 			conf.validate({ allowed: "strict" });
 		} catch (e) {
