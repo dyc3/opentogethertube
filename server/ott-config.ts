080f03830190cbde7ea211d0407e46f1e0398d13
Victor <vgiraldo@stevens.edu>
2024-02-13 21:32:12 -0500

    add validateConf()

--- server/ott-config.ts
+++ server/ott-config.ts
@@ -530,6 +530,15 @@ function postProcessConfig(): void {
 	}
 }
 
+function validateConf(): Result<void, Error> {
+	try {
+		conf.validate({ allowed: "strict" });
+		return ok(undefined);
+	} catch (e) {
+		return err(e);
+	}
+}
+
 function validateMail(): Result<void, Error> {
 	const sender = conf.get("mail.sender_email");
 
