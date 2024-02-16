9c169f8786c6e5e6df837f97d262ed2777aef4bc
Carson McManus <dyc3@users.noreply.github.com>
2024-02-16 15:54:45 -0500

    add load test: `average-load` (#1340)

--- tests/load/utils.js
+++ tests/load/utils.js
@@ -77,3 +77,71 @@ export function createRoom(name, token, roomOptions = {}, options = { doCheck: t
 		console.log(`Failed to create room ${name}: ${resp.body}`);
 	}
 }
+
+/**
+ * Add, remove, or vote for a video in a room.
+ * @param {*} room
+ * @param {*} token
+ * @param {*} videoId
+ */
+export function reqVideo(room, token, videoId, options = { action: "add", target: "queue" }) {
+	const url =
+		options.target === "queue"
+			? `http://${HOSTNAME}/api/room/${room}/queue`
+			: `http://${HOSTNAME}/api/room/${room}/vote`;
+	const body = videoId;
+	let fn;
+	if (options.action === "add") {
+		fn = http.post;
+	} else if (options.action === "remove") {
+		fn = http.del;
+	} else {
+		throw new Error(`Invalid action: ${options.action}`);
+	}
+	const resp = fn(url, body, {
+		headers: {
+			Authorization: `Bearer ${token}`,
+		},
+	});
+	return resp;
+}
+
+/**
+ * Helper class to keep track of the state of a room. Does roughly what the client does to keep track of the room state.
+ */
+export class RoomState {
+	constructor(roomName) {
+		this.roomName = roomName;
+		this.state = {};
+		this.you = null;
+		this.users = new Map();
+	}
+
+	handleMessage(msg) {
+		if (msg.action === "sync") {
+			delete msg.action;
+			this.state = Object.assign(this.state, msg);
+		} else if (msg.action === "you") {
+			this.you = msg.info.id;
+		} else if (msg.action === "user") {
+			this.handleUserUpdate(msg.update);
+		}
+	}
+
+	handleUserUpdate(update) {
+		if (update.kind === "init") {
+			for (const user of update.value) {
+				this.users.set(user.id, user);
+			}
+		} else if (update.kind === "update") {
+			let user = this.users.get(update.value.id);
+			if (!user) {
+				this.users.set(update.value.id, update.value);
+			} else {
+				Object.assign(user, update.value);
+			}
+		} else if (update.kind === "remove") {
+			this.users.delete(update.value);
+		}
+	}
+}
