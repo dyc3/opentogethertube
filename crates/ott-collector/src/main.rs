0cbb2180e9543b7fd49e15d7b4871cf45348a788
Christopher Roddy <cjrkoa@gmail.com>
2024-02-25 07:27:52 +0000

    system state

--- crates/ott-collector/src/main.rs
+++ crates/ott-collector/src/main.rs
@@ -1,10 +1,88 @@
 #[macro_use]
 extern crate rocket;
 
+use serde_json::json;
+use serde_json::Value;
+
+const sampleSystemState: [Value; 3] = [
+	json!({
+		"id": "154d9d41-128c-45ab-83d8-28661882c9e3",
+		"region": "ewr",
+		"monoliths": [
+			json!({
+				"id": "2bd5e4a7-14f6-4da4-bedd-72946864a7bf",
+				"region": "ewr",
+				"rooms": [
+					{ "name": "foo", "clients": 2 },
+					{ "name": "bar", "clients": 0 },
+				],
+			}),
+			json!({
+				"id": "419580cb-f576-4314-8162-45340c94bae1",
+				"region": "ewr",
+				"rooms": [{ "name": "baz", "clients": 3 }],
+			}),
+			json!({
+				"id": "0c85b46e-d343-46a3-ae4f-5f2aa1a8bdac",
+				"region": "cdg",
+				"rooms": [{ "name": "qux", "clients": 0 }],
+			}),
+		],
+	}),
+	json!({
+		"id": "c91d183c-980e-4160-b196-43658148f469",
+		"region": "ewr",
+		"monoliths": [
+			json!({
+				"id": "2bd5e4a7-14f6-4da4-bedd-72946864a7bf",
+				"region": "ewr",
+				"rooms": [
+					{ "name": "foo", "clients": 1 },
+					{ "name": "bar", "clients": 2 },
+				],
+			}),
+			json!({
+				"id": "419580cb-f576-4314-8162-45340c94bae1",
+				"region": "ewr",
+				"rooms": [{ "name": "baz", "clients": 0 }],
+			}),
+			json!({
+				"id": "0c85b46e-d343-46a3-ae4f-5f2aa1a8bdac",
+				"region": "cdg",
+				"rooms": [{ "name": "qux", "clients": 0 }],
+			}),
+		],
+	}),
+	json!({
+		"id": "5a2e3b2d-f27b-4e3d-9b59-c921442f7ff0",
+		"region": "cdg",
+		"monoliths": [
+			json!({
+				"id": "2bd5e4a7-14f6-4da4-bedd-72946864a7bf",
+				"region": "ewr",
+				"rooms": [
+					{ "name": "foo", "clients": 0 },
+					{ "name": "bar", "clients": 0 },
+				],
+			}),
+			json!({
+				"id": "419580cb-f576-4314-8162-45340c94bae1",
+				"region": "ewr",
+				"rooms": [{ "name": "baz", "clients": 0 }],
+			}),
+			json!({
+				"id": "0c85b46e-d343-46a3-ae4f-5f2aa1a8bdac",
+				"region": "cdg",
+				"rooms": [{ "name": "qux", "clients": 4 }],
+			}),
+		],
+	}),
+];
+
 /// Serve the current system state
 #[get("/state")]
-fn serve_state() {
-    todo!("Serve the current system state")
+fn serve_state() -> [Value; 3] {
+    return sampleSystemState;
 }
 
 #[launch]
