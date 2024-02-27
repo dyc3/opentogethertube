670820346e6041cb82a9fadcbd40107f69a18452
Christopher Roddy <76755988+cjrkoa@users.noreply.github.com>
2024-02-27 18:28:08 -0500

    Make ott-vis-datasource and ott-collector communicate (#1397)
    * system state
    
    * serve sample state
    
    * http request + minor changes to collector
    
    * changed Vec<Balancer> to Box<Balancer> and get request change (did not test any of this yet)
    
    * changes
    
    * display data
    
    * cargo fmt
    
    * fmt (again)
    
    * fmt
    
    * prettier
    
    * move rocket import
    
    * Update packages/ott-vis-datasource/src/datasource.ts
    
    Co-authored-by: Carson McManus <dyc3@users.noreply.github.com>
    
    * requested changes
    
    * remove unused type
    
    * remove unused type
    
    ---------
    
    Co-authored-by: Carson McManus <dyc3@users.noreply.github.com>

--- crates/ott-collector/src/main.rs
+++ crates/ott-collector/src/main.rs
@@ -1,12 +1,153 @@
 #[macro_use]
 extern crate rocket;
 
+use rocket::serde::json::Json;
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct SystemState(Vec<Balancer>);
+
+#[derive(Serialize)]
+struct Balancer {
+    id: String,
+    region: String,
+    monoliths: Vec<Monolith>,
+}
+
+#[derive(Serialize)]
+struct Monolith {
+    id: String,
+    region: String,
+    rooms: Vec<Room>,
+}
+
+#[derive(Serialize)]
+struct Room {
+    name: String,
+    clients: i32,
+}
+
+fn return_sample_state() -> SystemState {
+    SystemState({
+        vec![
+            Balancer {
+                id: "154d9d41-128c-45ab-83d8-28661882c9e3".to_string(),
+                region: "ewr".to_string(),
+                monoliths: vec![
+                    Monolith {
+                        id: "2bd5e4a7-14f6-4da4-bedd-72946864a7bf".to_string(),
+                        region: "ewr".to_string(),
+                        rooms: vec![
+                            Room {
+                                name: "foo".to_string(),
+                                clients: 2,
+                            },
+                            Room {
+                                name: "bar".to_string(),
+                                clients: 0,
+                            },
+                        ],
+                    },
+                    Monolith {
+                        id: "419580cb-f576-4314-8162-45340c94bae1".to_string(),
+                        region: "ewr".to_string(),
+                        rooms: vec![Room {
+                            name: "baz".to_string(),
+                            clients: 3,
+                        }],
+                    },
+                    Monolith {
+                        id: "0c85b46e-d343-46a3-ae4f-5f2aa1a8bdac".to_string(),
+                        region: "cdg".to_string(),
+                        rooms: vec![Room {
+                            name: "qux".to_string(),
+                            clients: 0,
+                        }],
+                    },
+                ],
+            },
+            Balancer {
+                id: "c91d183c-980e-4160-b196-43658148f469".to_string(),
+                region: "ewr".to_string(),
+                monoliths: vec![
+                    Monolith {
+                        id: "2bd5e4a7-14f6-4da4-bedd-72946864a7bf".to_string(),
+                        region: "ewr".to_string(),
+                        rooms: vec![
+                            Room {
+                                name: "foo".to_string(),
+                                clients: 1,
+                            },
+                            Room {
+                                name: "bar".to_string(),
+                                clients: 2,
+                            },
+                        ],
+                    },
+                    Monolith {
+                        id: "419580cb-f576-4314-8162-45340c94bae1".to_string(),
+                        region: "ewr".to_string(),
+                        rooms: vec![Room {
+                            name: "baz".to_string(),
+                            clients: 0,
+                        }],
+                    },
+                    Monolith {
+                        id: "0c85b46e-d343-46a3-ae4f-5f2aa1a8bdac".to_string(),
+                        region: "cdg".to_string(),
+                        rooms: vec![Room {
+                            name: "qux".to_string(),
+                            clients: 0,
+                        }],
+                    },
+                ],
+            },
+            Balancer {
+                id: "5a2e3b2d-f27b-4e3d-9b59-c921442f7ff0".to_string(),
+                region: "cdg".to_string(),
+                monoliths: vec![
+                    Monolith {
+                        id: "2bd5e4a7-14f6-4da4-bedd-72946864a7bf".to_string(),
+                        region: "ewr".to_string(),
+                        rooms: vec![
+                            Room {
+                                name: "foo".to_string(),
+                                clients: 0,
+                            },
+                            Room {
+                                name: "bar".to_string(),
+                                clients: 0,
+                            },
+                        ],
+                    },
+                    Monolith {
+                        id: "419580cb-f576-4314-8162-45340c94bae1".to_string(),
+                        region: "ewr".to_string(),
+                        rooms: vec![Room {
+                            name: "baz".to_string(),
+                            clients: 0,
+                        }],
+                    },
+                    Monolith {
+                        id: "0c85b46e-d343-46a3-ae4f-5f2aa1a8bdac".to_string(),
+                        region: "cdg".to_string(),
+                        rooms: vec![Room {
+                            name: "qux".to_string(),
+                            clients: 4,
+                        }],
+                    },
+                ],
+            },
+        ]
+    })
+}
+
 mod cors;
 
 /// Serve the current system state
 #[get("/state")]
-fn serve_state() {
-    todo!("Serve the current system state")
+fn serve_state() -> Json<SystemState> {
+    Json(return_sample_state())
 }
 
 #[launch]
@@ -22,3 +163,6 @@ fn rocket() -> _ {
 fn status() -> &'static str {
     "OK"
 }
+
+#[cfg(test)]
+mod test {}
