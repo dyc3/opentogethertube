8bea6060ff5da3cc1a896415057b378aa717a8d6
Victor <vgiraldo@stevens.edu>
2024-02-27 10:34:41 -0500

    add error messages

--- common/models/zod-schemas.ts
+++ common/models/zod-schemas.ts
@@ -3,8 +3,11 @@ import { Visibility } from "ott-common/models/types";
 import { z } from "zod";
 
 export const createRoomSchema = z.object({
-	name: z.string().min(3).max(32),
-	title: z.string().max(255).optional(),
+	name: z
+		.string()
+		.min(3, "too short, must be atleast 3 characters")
+		.max(32, "too long, must be at most 32 characters"),
+	title: z.string().max(255, "too long, must be at most 255 characters").optional(),
 	isTemporary: z.boolean().optional(),
 	visibility: z.enum([Visibility.Public, Visibility.Unlisted, Visibility.Private]).optional(),
 });
