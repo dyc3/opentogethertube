6d1e0c586b230e9592224b593c85192fe102e5ca
Carson McManus <carson.mcmanus1@gmail.com>
2024-02-29 10:03:04 -0500

    fix lints

--- crates/ott-collector/src/main.rs
+++ crates/ott-collector/src/main.rs
@@ -1,9 +1,8 @@
 use std::sync::Arc;
 
 use collector::{Collector, CURRENT_STATE};
-use ott_balancer_protocol::collector::{BalancerState, MonolithState, RoomState};
+use ott_balancer_protocol::collector::BalancerState;
 use ott_common::discovery::{start_discovery_task, DnsDiscoveryConfig, DnsMonolithDiscoverer};
-use rocket::serde::json::Json;
 use rocket::{serde::json::Json, State};
 use serde::Serialize;
 use tokio::sync::Mutex;
@@ -17,121 +16,6 @@ mod cors;
 #[derive(Debug, Clone, Serialize)]
 pub struct SystemState(Vec<BalancerState>);
 
-fn return_sample_state() -> SystemState {
-    SystemState({
-        vec![
-            BalancerState {
-                id: uuid::uuid!("154d9d41-128c-45ab-83d8-28661882c9e3").into(),
-                region: "ewr".to_string(),
-                monoliths: vec![
-                    MonolithState {
-                        id: uuid::uuid!("2bd5e4a7-14f6-4da4-bedd-72946864a7bf").into(),
-                        region: "ewr".to_string(),
-                        rooms: vec![
-                            RoomState {
-                                name: "foo".into(),
-                                clients: 2,
-                            },
-                            RoomState {
-                                name: "bar".into(),
-                                clients: 0,
-                            },
-                        ],
-                    },
-                    MonolithState {
-                        id: uuid::uuid!("419580cb-f576-4314-8162-45340c94bae1").into(),
-                        region: "ewr".to_string(),
-                        rooms: vec![RoomState {
-                            name: "baz".into(),
-                            clients: 3,
-                        }],
-                    },
-                    MonolithState {
-                        id: uuid::uuid!("0c85b46e-d343-46a3-ae4f-5f2aa1a8bdac").into(),
-                        region: "cdg".to_string(),
-                        rooms: vec![RoomState {
-                            name: "qux".into(),
-                            clients: 0,
-                        }],
-                    },
-                ],
-            },
-            BalancerState {
-                id: uuid::uuid!("c91d183c-980e-4160-b196-43658148f469").into(),
-                region: "ewr".to_string(),
-                monoliths: vec![
-                    MonolithState {
-                        id: uuid::uuid!("2bd5e4a7-14f6-4da4-bedd-72946864a7bf").into(),
-                        region: "ewr".to_string(),
-                        rooms: vec![
-                            RoomState {
-                                name: "foo".into(),
-                                clients: 1,
-                            },
-                            RoomState {
-                                name: "bar".into(),
-                                clients: 2,
-                            },
-                        ],
-                    },
-                    MonolithState {
-                        id: uuid::uuid!("419580cb-f576-4314-8162-45340c94bae1").into(),
-                        region: "ewr".to_string(),
-                        rooms: vec![RoomState {
-                            name: "baz".into(),
-                            clients: 0,
-                        }],
-                    },
-                    MonolithState {
-                        id: uuid::uuid!("0c85b46e-d343-46a3-ae4f-5f2aa1a8bdac").into(),
-                        region: "cdg".to_string(),
-                        rooms: vec![RoomState {
-                            name: "qux".into(),
-                            clients: 0,
-                        }],
-                    },
-                ],
-            },
-            BalancerState {
-                id: uuid::uuid!("5a2e3b2d-f27b-4e3d-9b59-c921442f7ff0").into(),
-                region: "cdg".to_string(),
-                monoliths: vec![
-                    MonolithState {
-                        id: uuid::uuid!("2bd5e4a7-14f6-4da4-bedd-72946864a7bf").into(),
-                        region: "ewr".to_string(),
-                        rooms: vec![
-                            RoomState {
-                                name: "foo".into(),
-                                clients: 0,
-                            },
-                            RoomState {
-                                name: "bar".into(),
-                                clients: 0,
-                            },
-                        ],
-                    },
-                    MonolithState {
-                        id: uuid::uuid!("419580cb-f576-4314-8162-45340c94bae1").into(),
-                        region: "ewr".to_string(),
-                        rooms: vec![RoomState {
-                            name: "baz".into(),
-                            clients: 0,
-                        }],
-                    },
-                    MonolithState {
-                        id: uuid::uuid!("0c85b46e-d343-46a3-ae4f-5f2aa1a8bdac").into(),
-                        region: "cdg".to_string(),
-                        rooms: vec![RoomState {
-                            name: "qux".into(),
-                            clients: 4,
-                        }],
-                    },
-                ],
-            },
-        ]
-    })
-}
-
 /// Serve the current system state
 #[get("/state")]
 async fn serve_state(state: &State<Arc<Mutex<SystemState>>>) -> Json<SystemState> {
@@ -141,7 +25,7 @@ async fn serve_state(state: &State<Arc<Mutex<SystemState>>>) -> Json<SystemState
 
 #[rocket::main]
 async fn main() -> anyhow::Result<()> {
-    let (discovery_tx, _discovery_rx) = tokio::sync::mpsc::channel(2);
+    let (discovery_tx, discovery_rx) = tokio::sync::mpsc::channel(2);
     let discovery = DnsMonolithDiscoverer::new(DnsDiscoveryConfig {
         monolith_port: 8081,
         dns_server: None,
