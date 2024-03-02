83178357ba5aa95e2fb1843a21fe414fc7e50b4f
Carson McManus <carson.mcmanus1@gmail.com>
2024-03-02 14:28:56 -0500

    ignore some files for code coverage

--- common/vitest.config.ts
+++ common/vitest.config.ts
@@ -4,6 +4,9 @@ import { defineConfig, configDefaults } from "vitest/config";
 export default defineConfig({
 	test: {
 		exclude: [...configDefaults.exclude, "ts-out"],
+		coverage: {
+			exclude: [...(configDefaults.coverage.exclude ?? []), "**/*.spec-d.ts"],
+		},
 		typecheck: {
 			enabled: true,
 			include: ["**/*.spec-d.ts"],
--- server/vitest.config.ts
+++ server/vitest.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
 				"migrations/**",
 				"**/tests/**",
 				"**/common/**",
+				"**/*.spec-d.ts",
 			],
 		},
 		typecheck: {
