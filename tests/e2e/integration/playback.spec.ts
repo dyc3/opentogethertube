3b17325acebc64cb77243f29643bd6cf258f409d
Carson McManus <carson.mcmanus1@gmail.com>
2024-03-03 09:33:37 -0500

    tests: add a new playback e2e test and make the youtube e2e test pass if the youtube api key is not available only in CI
    update ci so the env vars get passed into cypress

--- tests/e2e/integration/playback.spec.ts
+++ tests/e2e/integration/playback.spec.ts
@@ -1,5 +1,7 @@
 import "cypress-iframe";
 
+// TODO: skip this test if youtube api key is not available AND/OR create another test that uses a different video source
+
 describe("Video playback", () => {
 	// HACK: sometimes the player doesn't load in time. Ideally we'd fix this in the app, but for now we'll just
 	// ignore it because I want this test to pass, and it still makes sure that the video is added and played.
@@ -16,7 +18,12 @@ describe("Video playback", () => {
 		});
 	});
 
-	it("should add and play a video", () => {
+	it("should add and play a youtube video", () => {
+		if (Cypress.env("CI") && !Cypress.env("YOUTUBE_API_KEY")) {
+			cy.log("Skipping test because YOUTUBE_API_KEY is not set");
+			return;
+		}
+
 		cy.contains("button", "Add a video").click();
 		cy.get('[data-cy="add-preview-input"]').type("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
 		cy.get(".video button").eq(1).click();
@@ -39,6 +46,21 @@ describe("Video playback", () => {
 				.should(element => {
 					expect(element[0].paused).to.be.false;
 				});
+		});
+	});
+
+	it("should add and play a direct video", () => {
+		cy.contains("button", "Add a video").click();
+		cy.get('[data-cy="add-preview-input"]').type("https://vjs.zencdn.net/v/oceans.mp4");
+		cy.get(".video button").eq(1).click();
+		cy.get("video").should("exist").scrollIntoView();
+		cy.get("video").should(element => {
+			expect(element[0].paused).to.be.true;
+		});
+		cy.wait(500);
+		cy.get(".video-controls button").eq(1).click();
+		cy.get("video").should("exist").should(element => {
+			expect(element[0].paused).to.be.false;
 		});
 	});
 });
