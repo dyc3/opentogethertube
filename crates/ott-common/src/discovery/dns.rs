2459c37424215862ef2215e3146bff54b77693f0
Christopher Roddy <76755988+cjrkoa@users.noreply.github.com>
2024-03-06 07:43:24 -0500

    add ability for dns_server in discovery to be customizable (#1436)
    * account for case where config.dns_server is some
    
    * changes
    
    * account for case where self.config.dns_server is Some
    
    * remove unused import
    
    * add deserialization test
    
    * imports for test
    
    * compiler complains when I have the imports and when I don't have the imports
    
    * changes
    
    * .
    
    * test does not work
    
    * add async keyword to function
    
    * Update crates/ott-common/src/discovery/dns.rs
    
    Co-authored-by: Carson McManus <dyc3@users.noreply.github.com>
    
    * Update crates/ott-common/src/discovery/dns.rs
    
    Co-authored-by: Carson McManus <dyc3@users.noreply.github.com>
    
    * test passes
    
    * remove async keyword
    
    * Update crates/ott-common/src/discovery/dns.rs
    
    Co-authored-by: Carson McManus <dyc3@users.noreply.github.com>
    
    * simplify assertion
    
    ---------
    
    Co-authored-by: Carson McManus <dyc3@users.noreply.github.com>
    Co-authored-by: Carson McManus <carson.mcmanus1@gmail.com>

--- crates/ott-common/src/discovery/dns.rs
+++ crates/ott-common/src/discovery/dns.rs
@@ -1,6 +1,9 @@
 use async_trait::async_trait;
 use tracing::info;
-use trust_dns_resolver::TokioAsyncResolver;
+use trust_dns_resolver::{
+    config::{NameServerConfig, Protocol, ResolverConfig, ResolverOpts},
+    TokioAsyncResolver,
+};
 
 use super::*;
 
@@ -9,7 +12,7 @@ pub struct DnsDiscoveryConfig {
     /// The port that monoliths should be listening on for load balancer connections.
     pub service_port: u16,
     /// The DNS server to query. Optional. If not provided, the system configuration will be used instead.
-    pub dns_server: Option<String>,
+    pub dns_server: Option<SocketAddr>,
     /// The A record to query. If using docker-compose, this should be the service name for the monolith.
     pub query: String,
 }
@@ -31,8 +34,18 @@ impl DnsServiceDiscoverer {
 #[async_trait]
 impl ServiceDiscoverer for DnsServiceDiscoverer {
     async fn discover(&mut self) -> anyhow::Result<Vec<ConnectionConfig>> {
-        let resolver =
-            TokioAsyncResolver::tokio_from_system_conf().expect("failed to create resolver");
+        let resolver = match self.config.dns_server {
+            None => {
+                TokioAsyncResolver::tokio_from_system_conf().expect("failed to create resolver")
+            }
+            Some(server) => {
+                let mut resolver_config = ResolverConfig::new();
+                resolver_config.add_name_server(NameServerConfig::new(server, Protocol::Udp));
+
+                TokioAsyncResolver::tokio(resolver_config, ResolverOpts::default())
+                    .expect("failed to create resolver")
+            }
+        };
 
         let lookup = resolver.ipv4_lookup(&self.config.query).await?;
         let monoliths = lookup
@@ -48,5 +61,24 @@ impl ServiceDiscoverer for DnsServiceDiscoverer {
 
     fn mode(&self) -> DiscoveryMode {
         DiscoveryMode::Polling(Duration::from_secs(10))
+    }
+}
+#[cfg(test)]
+mod test {
+    use super::*;
+    use serde_json::json;
+
+    #[test]
+    fn server_deserializes_correctly() {
+        let json = json!({
+            "service_port": 8080,
+            "dns_server": "127.0.0.1:100",
+            "query": "".to_string(),
+        });
+
+        let config: DnsDiscoveryConfig =
+            serde_json::from_value(json).expect("Failed to deserialize json");
+
+        assert_eq!(config.dns_server, Some(([127, 0, 0, 1], 100).into()));
     }
 }
