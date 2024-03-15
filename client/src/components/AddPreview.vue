5a623f5c0398e0291abdc1774859532a2d932b47
Carson McManus <carson.mcmanus1@gmail.com>
2024-03-15 10:07:50 -0400

    move add video helper into it's own component

--- client/src/components/AddPreview.vue
+++ client/src/components/AddPreview.vue
@@ -58,7 +58,7 @@
 					!isAddPreviewInputUrl
 				"
 			>
-				<v-row justify="center" align="center">
+				<v-row justify="center">
 					<v-col cols="12">
 						{{ $t("add-preview.search-for", { search: inputAddPreview }) }}<br />
 						<v-btn
@@ -71,77 +71,8 @@
 				</v-row>
 			</v-container>
 			<v-container v-else-if="inputAddPreview.length === 0">
-				<v-row justify="center" align="center">
-					<div class="add-video-helper">
-						<h1>{{ $t("add-preview.title") }}</h1>
-						<h3>{{ $t("add-preview.single-videos") }}</h3>
-						<ul>
-							<li>
-								<ProcessedText
-									:text="
-										$t('add-preview.platforms.youtube-videos', {
-											url: 'https://youtube.com/watch?v=LP8GRjv6AIo',
-										})
-									"
-									@link-click="setAddPreviewText"
-								/>
-							</li>
-							<li>
-								<ProcessedText
-									:text="
-										$t('add-preview.platforms.vimeo-videos', {
-											url: 'https://vimeo.com/94338566',
-										})
-									"
-									@link-click="setAddPreviewText"
-								/>
-							</li>
-							<li>
-								<ProcessedText
-									:text="
-										$t('add-preview.platforms.dailymotion-videos', {
-											url: 'https://dailymotion.com/video/x31i1so',
-										})
-									"
-									@link-click="setAddPreviewText"
-								/>
-							</li>
-							<li>
-								<ProcessedText
-									:text="
-										$t('add-preview.platforms.any-mp4-videos', {
-											url: 'https://vjs.zencdn.net/v/oceans.mp4',
-										})
-									"
-									@link-click="setAddPreviewText"
-								/>
-							</li>
-						</ul>
-						<h3>{{ $t("add-preview.playlists") }}</h3>
-						<ul>
-							<li>
-								<ProcessedText
-									:text="
-										$t('add-preview.platforms.youtube-playlists', {
-											url: 'https://youtube.com/playlist?list=PLv-kM7bcufALqOQvMsrVCQCEL1pIWScoQ',
-										})
-									"
-									@link-click="setAddPreviewText"
-								/>
-							</li>
-							<li>
-								<ProcessedText
-									:text="
-										$t('add-preview.platforms.youtube-channels', {
-											url: 'https://youtube.com/channel/UCI1XS_GkLGDOgf8YLaaXNRA',
-										})
-									"
-									@link-click="setAddPreviewText"
-								/>
-							</li>
-						</ul>
-						<span>{{ $t("add-preview.text") }}</span>
-					</div>
+				<v-row justify="center">
+					<AddPreviewHelper @link-click="setAddPreviewText" />
 				</v-row>
 			</v-container>
 		</v-row>
@@ -170,18 +101,18 @@ import { useI18n } from "vue-i18n";
 import { API } from "@/common-http";
 import _ from "lodash";
 import VideoQueueItem from "@/components/VideoQueueItem.vue";
-import ProcessedText from "@/components/ProcessedText.vue";
 import { ToastStyle } from "@/models/toast";
 import toast from "@/util/toast";
 import { Video } from "ott-common/models/video";
 import { OttResponseBody, OttApiResponseAddPreview } from "ott-common/models/rest-api";
 import axios from "axios";
+import AddPreviewHelper from "./AddPreviewHelper.vue";
 
 export const AddPreview = defineComponent({
 	name: "AddPreview",
 	components: {
 		VideoQueueItem,
-		ProcessedText,
+		AddPreviewHelper,
 	},
 	setup() {
 		const store = useStore();
@@ -396,29 +327,8 @@ export default AddPreview;
 </script>
 
 <style lang="scss" scoped>
-@import "../variables.scss";
-
 .video-add {
 	margin: 20px 30px;
 	min-height: 500px;
-}
-
-.add-video-helper {
-	width: 400px;
-	@media (max-width: $sm-max) {
-		width: 80%;
-	}
-
-	h1 {
-		width: 100%;
-		text-align: center;
-	}
-
-	li {
-		white-space: nowrap;
-		overflow: hidden;
-		text-overflow: ellipsis;
-		-webkit-line-clamp: 1;
-	}
 }
 </style>
