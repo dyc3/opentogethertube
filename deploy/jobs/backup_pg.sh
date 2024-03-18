98fedbf934a7208ecae299d0f751c93d57ceff71
Carson McManus <carson.mcmanus1@gmail.com>
2024-03-18 12:02:06 -0400

    jobs: fix pg backup job

--- deploy/jobs/backup_pg.sh
+++ deploy/jobs/backup_pg.sh
@@ -29,6 +29,16 @@ EOF
 chmod 0600 ~/.pgpass
 
 OUTPUT_FILE="ott-prod-$(date +%Y-%m-%d).sql.gz"
-pg_dumpall --no-password -d "$DATABASE_URL" | gzip --best > "$OUTPUT_FILE"
+
+DB_NAME="ott_prod"
+
+# HACK: the app name is used as the username for the database, but it can't contain dashes
+DB_USERNAME=${FLY_APP_NAME//-/_}
+echo "DB_USERNAME: $DB_USERNAME"
+# HACK: in order to connect to the right database, we need to replace the database name in the path of the DATABASE_URL, but NOT the username
+CONNECTION_URL=$(echo "$DATABASE_URL" | sed --regexp-extended "s/$DB_USERNAME(\?|$)/$DB_NAME?/g")
+echo "CONNECTION_URL: $CONNECTION_URL"
+
+pg_dump --no-password -d "$CONNECTION_URL" | gzip --best > "$OUTPUT_FILE"
 ./rclone --progress --config ./rclone.conf --b2-chunk-size 64M --b2-upload-cutoff 100M --checkers 1 --transfers 1 copy "$OUTPUT_FILE" "b2:ott-backups/ott-prod"
 rm "$OUTPUT_FILE"
