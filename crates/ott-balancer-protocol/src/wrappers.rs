c9138a7f333707a71f04bea5c8d736674d1f4412
Carson McManus <carson.mcmanus1@gmail.com>
2024-03-27 09:24:46 -0400

    treat room names as case insensitive in rust code

--- crates/ott-balancer-protocol/src/wrappers.rs
+++ crates/ott-balancer-protocol/src/wrappers.rs
@@ -1,4 +1,4 @@
-use std::{fmt::Display, sync::Arc};
+use std::{fmt::Display, hash::Hash, sync::Arc};
 
 use serde::{Deserialize, Serialize};
 use typeshare::typeshare;
@@ -7,7 +7,7 @@ use uuid::Uuid;
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord, Serialize, Deserialize)]
 #[typeshare(serialized_as = "String")]
 pub struct ClientId(Uuid);
-#[derive(Debug, Clone, PartialEq, Eq, Hash, PartialOrd, Ord, Serialize, Deserialize)]
+#[derive(Debug, Clone, Eq, Ord, Serialize, Deserialize)]
 #[typeshare(serialized_as = "String")]
 pub struct RoomName(Arc<str>);
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord, Serialize, Deserialize)]
@@ -127,9 +127,39 @@ where
     }
 }
 
+impl PartialEq for RoomName {
+    fn eq(&self, other: &Self) -> bool {
+        self.0.eq_ignore_ascii_case(&other.0)
+    }
+}
+
+impl PartialOrd for RoomName {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.0
+            .to_ascii_lowercase()
+            .partial_cmp(&other.0.to_ascii_lowercase())
+    }
+}
+
+impl Hash for RoomName {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        for c in self.0.chars() {
+            c.to_ascii_lowercase().hash(state);
+        }
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
+
+    #[test]
+    fn room_name_eq_itself() {
+        let room = RoomName::from("foo");
+        assert_eq!(room, room);
+        let room2 = RoomName::from("foo");
+        assert_eq!(room, room2);
+    }
 
     #[test]
     fn room_name_case_insensitive() {
@@ -145,5 +175,29 @@ mod test {
         let mut map = std::collections::HashMap::new();
         map.insert(room1, 1);
         assert_eq!(map.get(&room2), Some(&1));
+    }
+
+    #[test]
+    fn room_name_partial_ord() {
+        // verifying the std implementation as reference
+        assert!("Foo" < "bar");
+        assert!("Foo" < "foo");
+        assert!("Bar" < "foo");
+        assert!("1" < "2");
+
+        // verifying the custom implementation
+        assert!(RoomName::from("Foo") > RoomName::from("bar"));
+        assert_eq!(RoomName::from("Foo"), RoomName::from("foo"));
+        assert!(RoomName::from("Bar") < RoomName::from("foo"));
+        assert!(RoomName::from("1") < RoomName::from("2"));
+    }
+
+    #[test]
+    fn room_name_ord() {
+        // this is to verify we don't break the properies of Ord
+        // probably would be better to have a property based test
+        let a = RoomName::from("a");
+        let b = RoomName::from("b");
+        assert_eq!(a.partial_cmp(&b), Some(a.cmp(&b)));
     }
 }
