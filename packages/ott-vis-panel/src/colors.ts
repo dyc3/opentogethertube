64904f3a8cbebea005d38fc887460e6e01c9d013
Victor Giraldo <117304729+Victor-M-Giraldo@users.noreply.github.com>
2024-04-08 19:32:51 -0400

    Add color provider hook to manage what colors are assigned to which node (#1644)
    * initial commit
    
    * requested changes

--- packages/ott-vis-panel/src/colors.ts
+++ packages/ott-vis-panel/src/colors.ts
@@ -1,12 +1,18 @@
+import * as d3 from "d3";
+
 class ColorProvider {
+	private color = d3.scaleOrdinal(d3.schemeCategory10);
+	private assignments: Map<string, string> = new Map();
+
 	public assign(thing: string): string {
-		// TODO: implement
-		return "";
+		if (!this.assignments.has(thing)) {
+			this.assignments.set(thing, this.color(thing));
+		}
+		return this.assignments.get(thing)!;
 	}
 
-	public assignments(): Map<string, string> {
-		// TODO: implement
-		return new Map();
+	public getAssignments(): Map<string, string> {
+		return this.assignments;
 	}
 }
 
