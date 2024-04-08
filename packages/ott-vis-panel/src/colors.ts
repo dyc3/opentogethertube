d7a29bf570635a56e9493527da58f05c6469c85c
Victor <vgiraldo@stevens.edu>
2024-04-08 19:10:36 -0400

    requested changes

--- packages/ott-vis-panel/src/colors.ts
+++ packages/ott-vis-panel/src/colors.ts
@@ -1,19 +1,14 @@
 import * as d3 from "d3";
 
 class ColorProvider {
-	private color: d3.ScaleOrdinal<string, string>;
-	private assignments: Map<string, string>;
-
-	constructor() {
-		this.color = d3.scaleOrdinal(d3.schemeCategory10);
-		this.assignments = new Map();
-	}
+	private color = d3.scaleOrdinal(d3.schemeCategory10);
+	private assignments: Map<string, string> = new Map();
 
 	public assign(thing: string): string {
 		if (!this.assignments.has(thing)) {
 			this.assignments.set(thing, this.color(thing));
 		}
-		return this.assignments.get(thing) as string;
+		return this.assignments.get(thing)!;
 	}
 
 	public getAssignments(): Map<string, string> {
