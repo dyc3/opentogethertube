54c21ef09e7e4009a361e8f1ba23c99fa45a91cd
Carson McManus <carson.mcmanus1@gmail.com>
2024-04-12 16:22:53 -0400

    client: refactor components to make them more simple

--- client/src/components/LogInForm.vue
+++ client/src/components/LogInForm.vue
@@ -28,7 +28,6 @@
 										>
 											{{ $t("login-form.login-discord") }}
 										</v-btn>
-										<!-- TODO: <v-btn size="x-large" block>Log in with Google</v-btn> -->
 									</v-container>
 								</v-col>
 								<v-divider vertical />
@@ -158,222 +157,180 @@
 	</v-sheet>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import { API } from "@/common-http";
 import isEmail from "validator/es/lib/isEmail";
 import { USERNAME_LENGTH_MAX } from "ott-common/constants";
-import { defineComponent, reactive, ref, watch } from "vue";
+import { reactive, ref, watch } from "vue";
 import { useStore } from "@/store";
 import { useI18n } from "vue-i18n";
 import { VForm } from "vuetify/lib/components/VForm/VForm.mjs";
 import { goLoginDiscord } from "@/util/discord";
 import ForgotPassword from "./ForgotPassword.vue";
 
-const LogInForm = defineComponent({
-	name: "LogInForm",
-	emits: ["shouldClose"],
-	components: {
-		ForgotPassword,
-	},
-	setup(props, { emit }) {
-		const store = useStore();
-		const { t } = useI18n();
+const emit = defineEmits(["shouldClose"]);
 
-		const email = ref("");
-		const username = ref("");
-		const emailOrUsername = ref("");
-		const password = ref("");
-		const password2 = ref("");
+const store = useStore();
+const { t } = useI18n();
 
-		const mode = ref("");
-		const isLoading = ref(false);
-		const logInFailureMessage = ref("");
-		const registerFailureMessage = ref("");
+const email = ref("");
+const username = ref("");
+const emailOrUsername = ref("");
+const password = ref("");
+const password2 = ref("");
 
-		const loginValid = ref(false);
-		const registerValid = ref(false);
-		let registerFieldErrors = reactive({
-			email: "",
-			username: "",
-			password: "",
-			password2: "",
-		});
+const mode = ref("");
+const isLoading = ref(false);
+const logInFailureMessage = ref("");
+const registerFailureMessage = ref("");
 
-		// magic line that grabs a static ref to the DOM element/component with ref="loginForm"
-		const loginForm = ref<VForm | undefined>(null);
-		const registerForm = ref<VForm | undefined>(null);
-
-		const emailRules = [v => (v && isEmail(v)) || !v || t("login-form.rules.valid-email")];
-		const usernameRules = [
-			v => !!v || t("login-form.rules.username-required"),
-			v =>
-				(!!v && v.length > 0 && v.length <= USERNAME_LENGTH_MAX) ||
-				t("login-form.rules.username-length", { length: USERNAME_LENGTH_MAX }),
-		];
-
-		const passwordRules = [
-			v => !!v || t("login-form.rules.password-required"),
-			v =>
-				(v && v.length >= 10) ||
-				(import.meta.env.DEV && v === "1") ||
-				t("login-form.rules.password-length"),
-		];
-		const retypePasswordRules = [
-			v => !!v || t("login-form.rules.retype-password"),
-			v => comparePassword(v) || t("login-form.rules.passwords-match"),
-		];
-
-		function comparePassword(v: string) {
-			// HACK: required because otherwise this.password is undefined for some reason in the validation rule's context
-			return password.value === v;
-		}
-
-		watch(email, () => {
-			logInFailureMessage.value = "";
-			registerFailureMessage.value = "";
-			registerFieldErrors.email = "";
-		});
-		watch(password, () => {
-			logInFailureMessage.value = "";
-		});
-		watch(username, () => {
-			registerFailureMessage.value = "";
-			registerFieldErrors.username = "";
-		});
-
-		async function login() {
-			loginForm.value.validate();
-			if (!loginValid.value) {
-				return;
-			}
-
-			isLoading.value = true;
-			logInFailureMessage.value = "";
-			try {
-				const resp = await API.post("/user/login", {
-					user: emailOrUsername.value,
-					password: password.value,
-				});
-				if (resp.data.success) {
-					console.log("Log in success");
-					store.commit("LOGIN", resp.data.user);
-					emit("shouldClose");
-					emailOrUsername.value = "";
-					password.value = "";
-				} else {
-					console.log("Log in failed");
-					logInFailureMessage.value = t(
-						"login-form.errors.something-weird-happened"
-					) as string;
-				}
-			} catch (err) {
-				if (err.response && !err.response.data.success) {
-					if (err.response.data.error) {
-						logInFailureMessage.value = err.response.data.error.message;
-					} else {
-						logInFailureMessage.value = t(
-							"login-form.errors.login-failed-noserver"
-						) as string;
-					}
-				} else {
-					console.log("could not log in", err, err.response);
-					logInFailureMessage.value = t("login-form.errors.login-failed") as string;
-				}
-			}
-			isLoading.value = false;
-		}
-
-		async function register() {
-			registerForm.value.validate();
-			if (!registerValid.value) {
-				return;
-			}
-
-			isLoading.value = true;
-			registerFailureMessage.value = "";
-			registerFieldErrors = {
-				email: "",
-				username: "",
-				password: "",
-				password2: "",
-			};
-			try {
-				const resp = await API.post("/user/register", {
-					email: email.value,
-					username: username.value,
-					password: password.value,
-				});
-				if (resp.data.success) {
-					console.log("Registration success");
-					store.commit("LOGIN", resp.data.user);
-					emit("shouldClose");
-					email.value = "";
-					username.value = "";
-					password.value = "";
-					password2.value = "";
-				} else {
-					console.log("Registration failed");
-					registerFailureMessage.value = t(
-						"login-form.errors.something-weird-happened"
-					) as string;
-				}
-			} catch (err) {
-				if (err.response && !err.response.data.success) {
-					if (err.response.data.error) {
-						if (err.response.data.error.name === "AlreadyInUse") {
-							if (err.response.data.error.fields.includes("email")) {
-								registerFieldErrors.email = t("login-form.errors.in-use") as string;
-							}
-							if (err.response.data.error.fields.includes("username")) {
-								registerFieldErrors.username = t(
-									"login-form.errors.in-use"
-								) as string;
-							}
-						}
-						registerFailureMessage.value = err.response.data.error.message;
-					} else {
-						registerFailureMessage.value = t(
-							"login-form.errors.register-failed-noserver"
-						) as string;
-					}
-				} else {
-					console.log("could not register", err);
-					registerFailureMessage.value = t("login-form.errors.register-failed") as string;
-				}
-			}
-			isLoading.value = false;
-		}
-
-		return {
-			email,
-			username,
-			emailOrUsername,
-			password,
-			password2,
-			mode,
-			isLoading,
-			logInFailureMessage,
-			registerFailureMessage,
-			loginValid,
-			registerValid,
-			registerFieldErrors,
-
-			loginForm,
-			registerForm,
-
-			emailRules,
-			usernameRules,
-			passwordRules,
-			retypePasswordRules,
-
-			login,
-			register,
-			goLoginDiscord,
-		};
-	},
+const loginValid = ref(false);
+const registerValid = ref(false);
+let registerFieldErrors = reactive({
+	email: "",
+	username: "",
+	password: "",
+	password2: "",
 });
 
-export default LogInForm;
+// magic line that grabs a static ref to the DOM element/component with ref="loginForm"
+const loginForm = ref<VForm | undefined>(null);
+const registerForm = ref<VForm | undefined>(null);
+
+const emailRules = [v => (v && isEmail(v)) || !v || t("login-form.rules.valid-email")];
+const usernameRules = [
+	v => !!v || t("login-form.rules.username-required"),
+	v =>
+		(!!v && v.length > 0 && v.length <= USERNAME_LENGTH_MAX) ||
+		t("login-form.rules.username-length", { length: USERNAME_LENGTH_MAX }),
+];
+
+const passwordRules = [
+	v => !!v || t("login-form.rules.password-required"),
+	v =>
+		(v && v.length >= 10) ||
+		(import.meta.env.DEV && v === "1") ||
+		t("login-form.rules.password-length"),
+];
+const retypePasswordRules = [
+	v => !!v || t("login-form.rules.retype-password"),
+	v => comparePassword(v) || t("login-form.rules.passwords-match"),
+];
+
+function comparePassword(v: string) {
+	// HACK: required because otherwise this.password is undefined for some reason in the validation rule's context
+	return password.value === v;
+}
+
+watch(email, () => {
+	logInFailureMessage.value = "";
+	registerFailureMessage.value = "";
+	registerFieldErrors.email = "";
+});
+watch(password, () => {
+	logInFailureMessage.value = "";
+});
+watch(username, () => {
+	registerFailureMessage.value = "";
+	registerFieldErrors.username = "";
+});
+
+async function login() {
+	loginForm.value.validate();
+	if (!loginValid.value) {
+		return;
+	}
+
+	isLoading.value = true;
+	logInFailureMessage.value = "";
+	try {
+		const resp = await API.post("/user/login", {
+			user: emailOrUsername.value,
+			password: password.value,
+		});
+		if (resp.data.success) {
+			console.log("Log in success");
+			store.commit("LOGIN", resp.data.user);
+			emit("shouldClose");
+			emailOrUsername.value = "";
+			password.value = "";
+		} else {
+			console.log("Log in failed");
+			logInFailureMessage.value = t("login-form.errors.something-weird-happened") as string;
+		}
+	} catch (err) {
+		if (err.response && !err.response.data.success) {
+			if (err.response.data.error) {
+				logInFailureMessage.value = err.response.data.error.message;
+			} else {
+				logInFailureMessage.value = t("login-form.errors.login-failed-noserver") as string;
+			}
+		} else {
+			console.log("could not log in", err, err.response);
+			logInFailureMessage.value = t("login-form.errors.login-failed") as string;
+		}
+	}
+	isLoading.value = false;
+}
+
+async function register() {
+	registerForm.value.validate();
+	if (!registerValid.value) {
+		return;
+	}
+
+	isLoading.value = true;
+	registerFailureMessage.value = "";
+	registerFieldErrors = {
+		email: "",
+		username: "",
+		password: "",
+		password2: "",
+	};
+	try {
+		const resp = await API.post("/user/register", {
+			email: email.value,
+			username: username.value,
+			password: password.value,
+		});
+		if (resp.data.success) {
+			console.log("Registration success");
+			store.commit("LOGIN", resp.data.user);
+			emit("shouldClose");
+			email.value = "";
+			username.value = "";
+			password.value = "";
+			password2.value = "";
+		} else {
+			console.log("Registration failed");
+			registerFailureMessage.value = t(
+				"login-form.errors.something-weird-happened"
+			) as string;
+		}
+	} catch (err) {
+		if (err.response && !err.response.data.success) {
+			if (err.response.data.error) {
+				if (err.response.data.error.name === "AlreadyInUse") {
+					if (err.response.data.error.fields.includes("email")) {
+						registerFieldErrors.email = t("login-form.errors.in-use") as string;
+					}
+					if (err.response.data.error.fields.includes("username")) {
+						registerFieldErrors.username = t("login-form.errors.in-use") as string;
+					}
+				}
+				registerFailureMessage.value = err.response.data.error.message;
+			} else {
+				registerFailureMessage.value = t(
+					"login-form.errors.register-failed-noserver"
+				) as string;
+			}
+		} else {
+			console.log("could not register", err);
+			registerFailureMessage.value = t("login-form.errors.register-failed") as string;
+		}
+	}
+	isLoading.value = false;
+}
 </script>
 
 <style lang="scss" scoped></style>
--- client/src/components/ProcessedText.vue
+++ client/src/components/ProcessedText.vue
@@ -19,8 +19,8 @@
 	</span>
 </template>
 
-<script lang="ts">
-import { defineComponent, ref, onMounted, watch, Ref, toRefs } from "vue";
+<script lang="ts" setup>
+import { ref, onMounted, watch, Ref, toRefs } from "vue";
 
 const urlRegex = /(https?:\/\/[^\s]+)/;
 
@@ -29,74 +29,69 @@ interface ContentItem {
 	text: string;
 }
 
-const ProcessedText = defineComponent({
-	name: "ProcessedText",
-	props: {
-		text: { type: String, required: true },
-		showAddQueueTooltip: { type: Boolean, default: true },
-	},
-	emits: ["link-click"],
-	setup(props, { emit }) {
-		const { text } = toRefs(props);
-		const content: Ref<ContentItem[]> = ref([]);
+const props = withDefaults(
+	defineProps<{
+		text: string;
+		showAddQueueTooltip: boolean;
+	}>(),
+	{
+		showAddQueueTooltip: true,
+	}
+);
 
-		function onLinkClick(e: Event, link: string) {
-			if (props.showAddQueueTooltip) {
-				e.preventDefault();
-				e.stopPropagation();
-				emit("link-click", link);
-			}
+const emit = defineEmits<{
+	"link-click": [link: string];
+}>();
+
+const { text } = toRefs(props);
+const content: Ref<ContentItem[]> = ref([]);
+
+function onLinkClick(e: Event, link: string) {
+	if (props.showAddQueueTooltip) {
+		e.preventDefault();
+		e.stopPropagation();
+		emit("link-click", link);
+	}
+}
+
+function processText() {
+	content.value = [];
+	if (!text.value) {
+		return;
+	}
+	let match;
+	let index = 0;
+	let loop = 0;
+	while ((match = urlRegex.exec(text.value.substring(index))) !== null) {
+		// console.log("msg:", this.text, "match", match, "content", this.content);
+		if (match.index > index) {
+			content.value.push({
+				type: "text",
+				text: text.value.slice(index, index + match.index),
+			});
 		}
-
-		function processText() {
-			content.value = [];
-			if (!text.value) {
-				return;
-			}
-			let match;
-			let index = 0;
-			let loop = 0;
-			while ((match = urlRegex.exec(text.value.substring(index))) !== null) {
-				// console.log("msg:", this.text, "match", match, "content", this.content);
-				if (match.index > index) {
-					content.value.push({
-						type: "text",
-						text: text.value.slice(index, index + match.index),
-					});
-				}
-				content.value.push({ type: "link", text: match[0] });
-				index += match.index + match[0].length;
-				loop++;
-				if (loop > 10) {
-					break;
-				}
-			}
-			if (index < text.value.length) {
-				content.value.push({ type: "text", text: text.value.substring(index) });
-			}
+		content.value.push({ type: "link", text: match[0] });
+		index += match.index + match[0].length;
+		loop++;
+		if (loop > 10) {
+			break;
 		}
+	}
+	if (index < text.value.length) {
+		content.value.push({ type: "text", text: text.value.substring(index) });
+	}
+}
 
-		onMounted(() => {
-			processText();
-		});
-
-		watch(
-			() => text,
-			() => {
-				processText();
-			}
-		);
-
-		return {
-			content,
-
-			onLinkClick,
-			processText,
-		};
-	},
+onMounted(() => {
+	processText();
 });
 
-export default ProcessedText;
+watch(
+	() => text,
+	() => {
+		processText();
+	}
+);
 </script>
 
 <style lang="scss" scoped>
