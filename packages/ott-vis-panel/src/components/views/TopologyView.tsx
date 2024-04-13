33e766986e4d11b449f4c60697f390911ccb42d8
Carson McManus <dyc3@users.noreply.github.com>
2024-04-13 11:37:56 -0400

    vis: refactor activity animations into a hook (#1689)

--- packages/ott-vis-panel/src/components/views/TopologyView.tsx
+++ packages/ott-vis-panel/src/components/views/TopologyView.tsx
@@ -16,9 +16,8 @@ import {
 } from "treeutils";
 import "./topology-view.css";
 import { useColorProvider } from "colors";
-import { useD3Zoom } from "chartutils";
+import { useActivityAnimations, useD3Zoom } from "chartutils";
 import { dedupeItems } from "aggregate";
-import { useEventBus, type BusEvent } from "eventbus";
 import type { NodeRadiusOptions } from "types";
 
 interface TopologyViewProps extends TopologyViewStyleProps {
@@ -436,106 +435,7 @@ export const TopologyView: React.FC<TopologyViewProps> = ({
 
 	useD3Zoom(svgRef);
 
-	const eventBus = useEventBus();
-	useEffect(() => {
-		if (!svgRef.current) {
-			return;
-		}
-
-		const svg = d3.select<SVGSVGElement, d3.HierarchyNode<TreeNode>>(svgRef.current);
-		const rxColor = "#0f0";
-		const txColor = "#00f";
-		const proxyColor = "#f58d05";
-
-		function animateNode(
-			node: d3.Selection<any, d3.HierarchyNode<TreeNode>, any, any>,
-			color: string
-		) {
-			if (node.empty()) {
-				return;
-			}
-			const data = node.data()[0] as d3.HierarchyNode<TreeNode>;
-			const endRadius = data ? getRadius(data.data.group) : 20;
-			let radiusCurrent = parseFloat(node.attr("r"));
-			let colorCurrent = d3.color(node.attr("stroke"));
-			if (isNaN(radiusCurrent)) {
-				radiusCurrent = 0;
-			}
-			const newRadius = Math.max(Math.min(radiusCurrent + 5, 40), endRadius);
-			const newColor = d3.interpolateRgb(colorCurrent?.formatRgb() ?? "#fff", color)(0.5);
-			node.transition("highlight")
-				.duration(333)
-				.ease(d3.easeCubicOut)
-				.attrTween("stroke", () => d3.interpolateRgb(newColor, "#fff"))
-				.attrTween("stroke-width", () => t => d3.interpolateNumber(4, 1.5)(t).toString())
-				.attrTween(
-					"r",
-					() => t => d3.interpolateNumber(newRadius, endRadius)(t).toString()
-				);
-		}
-
-		function animateLinks(
-			links: d3.Selection<any, d3.HierarchyLink<TreeNode>, any, any>,
-			color: string
-		) {
-			links
-				.transition("highlight")
-				.duration(333)
-				.ease(d3.easeCubicOut)
-				.attrTween("stroke", function () {
-					const link = d3.select<d3.BaseType, d3.HierarchyLink<TreeNode>>(
-						this
-					) as d3.Selection<any, d3.HierarchyLink<TreeNode>, any, unknown>;
-					let colorCurrent = d3.color(link.attr("stroke"));
-					const newColor = d3.interpolateRgb(
-						colorCurrent?.formatRgb() ?? "#fff",
-						color
-					)(0.5);
-
-					return d3.interpolateRgb(newColor, "#fff");
-				})
-				.attrTween("stroke-width", () => t => d3.interpolateNumber(4, 1.5)(t).toString());
-		}
-
-		function getColor(event: BusEvent): string {
-			if (event.event === "ws" || event.event === "broadcast") {
-				return event.direction === "rx" ? rxColor : txColor;
-			} else if (event.event === "proxy") {
-				return proxyColor;
-			} else {
-				return "#f00";
-			}
-		}
-
-		const sub = eventBus.subscribe(event => {
-			const color = getColor(event);
-			const node = svg.select(`[data-nodeid="${event.node_id}"]`);
-			animateNode(node, color);
-			if (!node.empty()) {
-				const parent = svg.select(`[data-nodeid="${node.datum().parent?.data.id}"]`);
-				animateNode(parent, color);
-			}
-
-			const links = svg.selectAll<SVGSVGElement, d3.HierarchyLink<TreeNode>>(
-				`[data-nodeid-target="${event.node_id}"]`
-			);
-			animateLinks(links, color);
-
-			if (event.room) {
-				const room = svg.select(`[data-nodeid="${event.room}"]`);
-				animateNode(room, color);
-
-				const roomLinks = svg.selectAll<SVGSVGElement, d3.HierarchyLink<TreeNode>>(
-					`[data-nodeid-target="${event.room}"]`
-				);
-				animateLinks(roomLinks, color);
-			}
-		});
-
-		return () => {
-			sub.unsubscribe();
-		};
-	}, [svgRef, eventBus, getRadius]);
+	useActivityAnimations(svgRef, getRadius);
 
 	return (
 		<svg
