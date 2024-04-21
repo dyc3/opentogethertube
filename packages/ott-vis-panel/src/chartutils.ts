941dee007785159a7ec93046b25a49e0d3432ebe
Carson McManus <dyc3@users.noreply.github.com>
2024-04-20 20:15:28 -0400

    vis: simplify useD3Zoom (#1709)

--- packages/ott-vis-panel/src/chartutils.ts
+++ packages/ott-vis-panel/src/chartutils.ts
@@ -1,20 +1,9 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import * as d3 from "d3";
 import { useEventBus, type BusEvent } from "eventbus";
 import type { TreeNode } from "treeutils";
 
 export function useD3Zoom(svgRef: React.MutableRefObject<SVGSVGElement | null>) {
-	const [chartTransform, setChartTransform] = useState("translate(0, 0)");
-	// run this only once after the first render
-	useEffect(() => {
-		if (!svgRef.current) {
-			return;
-		}
-		const svg = d3.select<SVGSVGElement, any>(svgRef.current);
-		svg.select("g.chart").attr("transform", chartTransform);
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
-
 	useEffect(() => {
 		if (!svgRef.current) {
 			return;
@@ -23,10 +12,9 @@ export function useD3Zoom(svgRef: React.MutableRefObject<SVGSVGElement | null>) 
 		const zoom = d3.zoom<SVGSVGElement, any>().on("zoom", handleZoom);
 		function handleZoom(e: any) {
 			svg.select("g.chart").attr("transform", e.transform);
-			setChartTransform(e.transform);
 		}
 		svg.call(zoom);
-	}, [svgRef, chartTransform]);
+	}, [svgRef]);
 }
 
 export function useActivityAnimations(
