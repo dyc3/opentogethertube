92c1263ed36fbc0279af482fb22392f7bdf06bfe
Sebasee009 <sebasee009@outlook.com>
2025-08-16 00:02:20 +0200

    update german language

--- client/src/locales/de.ts
+++ client/src/locales/de.ts
@@ -1,18 +1,42 @@
-import { OttWebsocketError, Role } from "ott-common/models/types";
+import { OttWebsocketError, BehaviorOption, Role } from "ott-common/models/types";
 
 export default {
 	"common": {
+		"yes": "Ja",
+		"no": "Nein",
+		"ok": "OK",
 		"cancel": "Abbrechen",
 		"close": "Schließen",
 		"close-all": "Alle schließen",
 		"add": "Hinzufügen",
 		"remove": "Entfernen",
+		"delete": "Löschen",
 		"play": "Play",
 		"pause": "Pause",
 		"save": "Speichern",
 		"search": "Suche",
 		"undo": "Rückgängig",
+		"copy": "Kopieren",
+		"show": "Anzeigen",
+		"hide": "Ausblenden",
+		"discard": "Verwerfen",
+		"loading": "Laden...",
+		"view": "Ansicht",
+		"restore": "Wiederherstellen",
+		"success": "Erfolg",
 		"vote": "Abstimmen",
+		"unvote": "Abwahl",
+		"on": "Ein",
+		"off": "Aus",
+		"dismiss": "Ignorieren",
+		"errors": {
+			"rate-limit": "Rate-Limit überschritten. Bitte versuchen Sie es in {duration} Sekunden erneut.",
+		},
+	},
+	"behavior": {
+		[BehaviorOption.Always]: "Immer",
+		[BehaviorOption.Prompt]: "Nachfragen",
+		[BehaviorOption.Never]: "Niemals",
 	},
 	"landing": {
 		hero: {
@@ -67,8 +91,8 @@ export default {
 			description2:
 				"Beteiligen Sie sich an der Entwicklung, indem Sie Ihre Ideen oder Ihren Code beisteuern, oder zeigen Sie Ihre Unterstützung, indem Sie Spender werden. Alle Spenden werden verwendet, um die Betriebskosten und die Entwicklung von OpenTogetherTube zu finanzieren und um OpenTogetherTube werbefrei zu halten.",
 			how: "Wie kann ich helfen?",
-			sponsor: "@:nav.support",
-			contribute: "Code beisteuern",
+			sponsor: "Sponsor werden",
+			contribute: "Beitragen",
 		},
 	},
 	"footer": {
@@ -77,6 +101,7 @@ export default {
 		"made-in": "Made in Amerika",
 		"thanks-to": "Besonderen Dank an",
 		"privacy-policy": "Datenschutzbestimmungen",
+		"attribution": "Zuschreibung",
 	},
 	"not-found": {
 		title: "Seite nicht gefunden",
@@ -92,8 +117,10 @@ export default {
 	"nav": {
 		"home": "Startseite",
 		"browse": "Durchsuchen",
+		"my-rooms": "Meine Räume",
+		"faq": "FAQ",
 		"bug": "Einen Fehler melden",
-		"support": "Spenden",
+		"support": "Unterstütze mich!",
 		"login": "Anmelden",
 		"link-discord": "Mit Discord verknüpfen",
 		"logout": "Abmelden",
@@ -105,6 +132,9 @@ export default {
 			"perm-desc": "Ideal für häufige Besucher.",
 		},
 	},
+	"my-rooms": {
+		"confirm-delete": 'Raum "{name}" dauerhaft löschen? Dies kann nicht rückgängig gemacht werden.',
+	},
 	"room-list": {
 		"no-rooms": "Kein Raum im Moment...",
 		"create": "@:nav.create.title",
@@ -113,10 +143,12 @@ export default {
 	},
 	"room": {
 		"title-temp": "Temporärer Raum",
+		"kick-me": "Mich rauswerfen",
 		"rewind": "10s Zurückspulen",
 		"skip": "10s Vorspulen",
 		"play-pause": "Wiedergabe/Pause",
 		"next-video": "Nächstes Video",
+		"next-video-vote": "Für Video überspringen abstimmen",
 		"toggle-fullscreen": "Vollbild umschalten",
 		"con-status": {
 			connecting: "Verbinden...",
@@ -133,9 +165,14 @@ export default {
 			you: "Du",
 			demote: "degradieren",
 			promote: "befördern",
+			kick: "Rauswerfen",
 		},
 	},
+	"privacy": {
+		title: "@:footer.privacy-policy",
+	},
 	"chat": {
+		"title": "Chat",
 		"type-here": "Schreibe deine Nachricht hier...",
 	},
 	"share-invite": {
@@ -152,6 +189,7 @@ export default {
 		"no-video-text": 'Klicken Sie unten auf "Hinzufügen", um ein Video hinzuzufügen.',
 	},
 	"add-preview": {
+		"label": "Link oder Suche",
 		"add-all": "Alle hinzufügen",
 		"placeholder":
 			"Geben Sie hier einen Suchbegriff ein um YouTube zu durchsuchen, oder geben Sie eine Video-URL ein, um sie zur Warteschlange hinzuzufügen.",
@@ -169,6 +207,13 @@ export default {
 			"youtube-playlists": "Youtube-Wiedergabelisten: {url}",
 			"youtube-channels": "Youtube-Kanäle: {url}",
 		},
+		"messages": {
+			"unknown-status": "Unbekannter Status für Vorschau-Antwort: {status}.",
+			"unknown-error": "Ein unbekannter Fehler ist beim Laden der Vorschau aufgetreten. Versuchen Sie es später erneut.",
+			"failed-to-get-add-preview":
+				"Fehler beim Laden der Vorschau. Dies ist wahrscheinlich ein Bug, überprüfen Sie die Konsole für Details.",
+			"failed-to-all-videos": "Fehler beim Hinzufügen aller Videos: {message}",
+		},
 	},
 	"processed-text": {
 		"link-hint":
@@ -177,16 +222,24 @@ export default {
 	"video-queue": {
 		"no-videos": "Es sind keine Videos in der Warteschlange.",
 		"add-video": "Ein Video hinzufügen",
+		"export": "Exportieren",
+		"export-diag-title": "Warteschlange exportieren",
+		"export-hint": 'Kopieren Sie diesen Text und fügen Sie ihn in das "Hinzufügen"-Tab ein, um diese Warteschlange wiederherzustellen.',
+		"restore": "Möchten Sie die Videos aus der vorherigen Warteschlange wiederherstellen?",
+		"restore-queue": "Warteschlange wiederherstellen?",
+		"restore-queue-hint":
+			"Das war das letzte Mal in der Warteschlange, als dieser Raum aktiv war. Möchten Sie es wiederherstellen?",
 	},
 	"video-queue-item": {
 		"experimental":
 			"Experimentelle Unterstützung für diesen Dienst! Rechnen Sie damit, dass er oft kaputt geht.",
-		"play-next": "Video als nächstes abspielen",
-		"play-last": "Video zuletzt abspielen",
+		"play-next": "Als nächstes abspielen",
+		"play-last": "Als letztes abspielen",
 		"messages": {
 			"video-added": "Video hinzugefügt",
 			"video-removed": "Video entfernt",
 		},
+		"start-at": "Starten bei {timestamp}",
 	},
 	"room-settings": {
 		"title": "@:create-room-form.title",
@@ -198,14 +251,21 @@ export default {
 		"manual": "@:create-room-form.manual",
 		"manual-hint":
 			"Standardverhalten, funktioniert so, wie man es erwarten würde. Sie können Einträge in der Warteschlange manuell neu anordnen.",
-		"vote": "@:create-room-form.vote",
+		"vote": "@:common.vote",
 		"vote-hint": "Das Video mit den meisten Stimmen wird als nächstes abgespielt.",
 		"loop": "Schleife",
 		"loop-hint": "Wenn ein Video endet, wird es an das Ende der Warteschlange geschoben.",
+		"dj": "DJ",
 		"dj-hint":
 			"Wenn das Video endet, startet das gleiche Video von Anfang an. Gut für das Abspielen von Hintergrundmusik.",
-		"auto-skip-text":
-			"Automatisches Überspringen von gesponserten Segmenten, Intros, Eigenwerbung usw. mit SponsorBlock-Daten.",
+		"auto-skip-text": "Unerwünschte Videosegmente automatisch überspringen mit SponsorBlock-Daten.",
+		"auto-skip-text-sponsor": "Sponsor",
+		"auto-skip-text-intro": "Intro",
+		"auto-skip-text-outro": "Outro",
+		"auto-skip-text-interaction": "Interaktion",
+		"auto-skip-text-selfpromo": "Eigenwerbung",
+		"auto-skip-text-music_offtopic": "Musik abseits des Themas",
+		"auto-skip-text-preview": "Vorschau",
 		"permissions-not-available": "In temporären Räumen sind keine Berechtigungen verfügbar.",
 		"room-needs-owner":
 			"Dieser Raum braucht einen Eigentümer, bevor die Berechtigungen geändert werden können.",
@@ -214,17 +274,26 @@ export default {
 			"Sie können die Berechtigungen in diesem Raum nicht ändern.",
 		"settings-applied": "Einstellungen angewendet",
 		"now-own-the-room": "Der Raum {room} gehört jetzt Ihnen.",
+		"load-failed": "Fehler beim Laden der Raumeinstellungen.",
+		"restore-queue": "Warteschlange beim Laden des Raums wiederherstellen",
+		"enable-vote-skip": "Abstimmung zum Überspringen aktivieren",
 	},
 	"create-room-form": {
 		"card-title": "Permanenten Raum erstellen",
 		"create-room": "Raum erstellen",
+		"name": "Name",
 		"name-hint": "Wird in der Raum-URL verwendet. Kann später nicht mehr geändert werden.",
 		"title": "Titel",
+		"title-hint": "Optional",
 		"description": "Beschreibung",
+		"description-hint": "@:create-room-form.title-hint",
 		"visibility": "Sichtbarkeit",
 		"visibility-hint": "Steuert, ob der Raum in der Raumliste angezeigt wird oder nicht.",
 		"queue-mode": "Warteschlangen-Modus",
 		"manual": "Manuell",
+		"vote": "@:common.vote",
+		"loop": "@:room-settings.loop",
+		"dj": "@:room-settings.dj",
 		"public": "Öffentlich",
 		"unlisted": "Ungelistet",
 		"rules": {
@@ -247,9 +316,12 @@ export default {
 		"register": "Registrieren",
 		"login-discord": "Anmelden mit Discord",
 		"email": "E-Mail",
+		"email-or-username": "E-Mail oder Nutzername",
 		"username": "Nutzername",
 		"password": "Passwort",
 		"retype-password": "Passwort erneut eingeben",
+		"email-optional":
+			"Optional. Die Angabe einer E-Mail macht es möglich, Ihr Konto wiederherzustellen, falls Sie Ihr Passwort vergessen.",
 		"rules": {
 			"email-required": "E-Mail ist erforderlich",
 			"valid-email": "Es muss eine gültige E-Mail sein",
@@ -273,6 +345,15 @@ export default {
 				"Die Registrierung ist fehlgeschlagen, und ich weiß nicht, warum. Prüfen Sie die Konsole und melden Sie dies als Fehler.",
 			"in-use": "Bereits in Verwendung.",
 		},
+		"change-password": {
+			title: "Passwort ändern",
+			success: "Passwort erfolgreich geändert.",
+			forgot: "Passwort vergessen?",
+			prompt: "Geben Sie die E-Mail-Adresse oder den Nutzernamen Ihres Kontos ein.",
+			reset: "Zurücksetzen",
+			sent: "Passwort-Reset-E-Mail gesendet.",
+			failed: "Passwort konnte nicht zurückgesetzt werden.",
+		},
 	},
 	"permissions-editor": {
 		"title": "Berechtigungs-Editor",
@@ -285,11 +366,46 @@ export default {
 	},
 	"client-settings": {
 		"title": "Anpassen",
-		"description": "Diese Einstellungen werden in deinem Browser gespeichert.",
+		"description": "Diese Einstellungen werden in Ihrem Browser gespeichert und betreffen nur Sie.",
 		"activator": "@:client-settings.title",
+		"room-layout": "Raum-Layout",
+		"theme": "Theme",
+		"sfx-enable": "Soundeffekte aktivieren",
+		"sfx-volume": "Soundeffekt-Lautstärke",
 		"room-settings": "Standardraumeinstellungen",
 	},
 	"connect-overlay": {
+		"title": "Verbindung getrennt",
 		"find-another": "Einen anderen Raum finden",
+		"dc-reasons": {
+			[OttWebsocketError.UNKNOWN]: "@:connect-overlay.dc-reasons.unknown",
+			[OttWebsocketError.ROOM_NOT_FOUND]: "Raum nicht gefunden.",
+			[OttWebsocketError.ROOM_UNLOADED]: "Raum wurde entladen.",
+			[OttWebsocketError.MISSING_TOKEN]:
+				"Es wurde kein Token bereitgestellt. Aktualisieren Sie die Seite und versuchen Sie es erneut. Andernfalls öffnen Sie bitte ein Issue auf GitHub.",
+			[OttWebsocketError.KICKED]: "Sie wurden von einem Benutzer aus dem Raum geworfen.",
+			unknown: "Etwas ist passiert, aber wir wissen nicht was. Bitte melden Sie dies als Bug.",
+		},
+	},
+	"vote-skip": {
+		remaining: "{count} weitere Stimmen zum Überspringen",
+	},
+	"roles": {
+		[Role.Administrator]: "Administrator",
+		[Role.Moderator]: "Moderator",
+		[Role.TrustedUser]: "Vertrauensvoller Nutzer",
+		[Role.RegisteredUser]: "Registrierter Nutzer",
+		[Role.UnregisteredUser]: "Nicht registrierter Nutzer",
+		[Role.Owner]: "Besitzer",
+	},
+	"errors": {
+		BadPasswordError:
+			"Passwort erfüllt nicht die Mindestanforderungen. Muss mindestens 8 Zeichen lang sein und 2 der folgenden Kategorien enthalten: Kleinbuchstaben, Großbuchstaben, Zahlen, Sonderzeichen.",
+		BadApiArgumentException: "Schlechtes API-Argument. Dies ist wahrscheinlich ein Bug, bitte melden Sie es.",
+	},
+	"player": {
+		"buffer-warn": {
+			spans: "Sie haben noch nicht genug von dem Video gepuffert. Aktuelle Zeit-Bereiche gepuffert: {ranges}",
+		},
 	},
 };
