d392cffb1198fefb5324ff7fdbe7b12b233cb5b8
Carson McManus <carson.mcmanus1@gmail.com>
2026-03-14 19:09:56 -0400

    fix: strip trimming metadata on video end in looping modes

--- server/tests/unit/room.spec.ts
+++ server/tests/unit/room.spec.ts
@@ -401,35 +401,43 @@ describe("Room", () => {
 			}
 		);
 
-		it.each([QueueMode.Loop])("should requeue the current item when mode is %s", async mode => {
-			room.queueMode = mode;
-			await room.dequeueNext();
-			expect(room.currentSource).toEqual({
-				service: "direct",
-				id: "video2",
-			});
-			expect(room.queue).toHaveLength(1);
-			expect(room.queue.items[0]).toEqual({
-				service: "direct",
-				id: "video",
-			});
-		});
+		it.each([
+			{
+				mode: QueueMode.Loop,
+				expectedCurrentSource: { service: "direct", id: "video2" },
+				expectedQueueItem: { service: "direct", id: "video" },
+				expectedPlaybackPosition: 0,
+			},
+			{
+				mode: QueueMode.Dj,
+				expectedCurrentSource: { service: "direct", id: "video" },
+				expectedQueueItem: { service: "direct", id: "video2" },
+				expectedPlaybackPosition: 0,
+			},
+		])(
+			"should clear trim metadata when mode is $mode",
+			async ({
+				mode,
+				expectedCurrentSource,
+				expectedQueueItem,
+				expectedPlaybackPosition,
+			}) => {
+				room.queueMode = mode;
+				room.currentSource = {
+					service: "direct",
+					id: "video",
+					startAt: 10,
+					endAt: 20,
+				};
+				room.playbackPosition = 10;
+				await room.dequeueNext();
 
-		it.each([QueueMode.Dj])("should restart the current item when mode is %s", async mode => {
-			room.queueMode = mode;
-			room.playbackPosition = 10;
-			await room.dequeueNext();
-			expect(room.currentSource).toEqual({
-				service: "direct",
-				id: "video",
-			});
-			expect(room.playbackPosition).toEqual(0);
-			expect(room.queue).toHaveLength(1);
-			expect(room.queue.items[0]).toEqual({
-				service: "direct",
-				id: "video2",
-			});
-		});
+				expect(room.currentSource).toEqual(expectedCurrentSource);
+				expect(room.playbackPosition).toEqual(expectedPlaybackPosition);
+				expect(room.queue).toHaveLength(1);
+				expect(room.queue.items[0]).toEqual(expectedQueueItem);
+			}
+		);
 
 		it.each([QueueMode.Loop])(
 			"should requeue the current item when mode is %s, with empty queue",
