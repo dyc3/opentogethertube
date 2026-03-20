8348c5128ca84c5fef985243f9be03c4a7752cc0
WuTofu <5987870+WuTofu@users.noreply.github.com>
2026-03-20 20:10:03 +0800

    Define manifest structure for client

--- client/src/models/media-tracks.ts
+++ client/src/models/media-tracks.ts
@@ -1,4 +1,5 @@
 export interface VideoTrack {
+	label?: number;
 	width: number;
 	height: number;
 }
@@ -6,6 +7,30 @@ export interface VideoTrack {
 export interface CaptionTrack {
 	kind?: "subtitles" | "captions";
 	label?: string;
-	srclang?: string;
+	srclang?: string; // If kind is "subtitles", srclang must be defined
 	default?: boolean;
+}
+
+export interface ManifestSource {
+	url: string;
+	contentType: string;
+	quality: number;
+	bitrate?: number;
+}
+
+export interface ManifestTextTrack {
+	url: string;
+	contentType: string;
+	name?: string;
+	srclang: string;
+	default?: boolean;
+}
+
+export interface CustomMediaManifest {
+	title: string;
+	duration: number;
+	live?: boolean;
+	thumbnail?: string;
+	sources: ManifestSource[];
+	textTracks?: ManifestTextTrack[];
 }
