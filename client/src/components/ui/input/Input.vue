910d9810ee4c76d27f02bfb5885529fdc8a7e5c8
Carson McManus <carson.mcmanus1@gmail.com>
2026-05-31 09:20:13 -0400

    refactor(ui): make Input, Textarea use defineModel directly

--- client/src/components/ui/input/Input.vue
+++ client/src/components/ui/input/Input.vue
@@ -1,22 +1,12 @@
 <script setup lang="ts">
 import type { HTMLAttributes } from 'vue'
-import { useVModel } from '@vueuse/core'
 import { cn } from '@/lib/utils'
 
 const props = defineProps<{
-  defaultValue?: string | number
-  modelValue?: string | number
-  class?: HTMLAttributes['class']
+	class?: HTMLAttributes['class']
 }>()
 
-const emits = defineEmits<{
-  (e: 'update:modelValue', payload: string | number): void
-}>()
-
-const modelValue = useVModel(props, 'modelValue', emits, {
-  passive: true,
-  defaultValue: props.defaultValue,
-})
+const modelValue = defineModel<string | number>()
 </script>
 
 <template>
