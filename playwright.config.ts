381471a40bdb3a329506b4683d68ee70e2e54fd5
Carson McManus <carson.mcmanus1@gmail.com>
2026-06-06 08:46:18 -0400

    ci: only one e2e worker

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,6 +3,7 @@ import { defineConfig, devices } from "@playwright/test";
 export default defineConfig({
 	testDir: "tests/e2e/integration",
 	fullyParallel: false,
+	workers: process.env.CI ? 1 : undefined,
 	forbidOnly: !!process.env.CI,
 	retries: process.env.CI ? 3 : 0,
 	reporter: process.env.CI ? "github" : "list",
